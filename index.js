94f8c8ab6a93bfe4e941fc5e9ed87f94cb70dad2
Edmund <edmundcjj@gmail.com>
2018-03-20 19:27:24 +0800

    Completed logic to render content to html

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
+/**
+ * ===================================
+ * Import libraries
+ * ===================================
+ */
+ // Express Library
 const express = require('express');
+
+// Express Handlebar Library
 const handlebars = require('express-handlebars');
 
-// const jsonfile = require('jsonfile');
+// Jsonfile Library
+const jsonfile = require('jsonfile');
 
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
 // Init express app
 const app = express();
+
+// Pokedex Json File
+const POKEDEX_FILE = 'pokedex.json';
 
 // Set handlebars to be the default view engine
 app.engine('handlebars', handlebars.create().engine);
@@ -21,10 +32,25 @@ app.set('view engine', 'handlebars');
  * Routes
  * ===================================
  */
-
 app.get('/names/:pokemon', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.params.pokemon);
+  let context = {
+    pokemon_name: request.params.pokemon,
+    pokemon_weight: "0kg"
+  }
+  // Read the pokedex json file
+	jsonfile.readFile(POKEDEX_FILE, function(err, obj) {
+		// deal with the request
+    console.log("Name of the pokemon is => " + context.pokemon_name);
+
+    for (var i = 0; i < obj.pokemon.length; i++) {
+      if (obj.pokemon[i].name === context.pokemon_name) {
+        context.pokemon_weight = obj.pokemon[i].weight;
+        console.log("Weight of the pokemon is => " + context.pokemon_weight);
+      }
+    }
+	});
+  // Render html based on the keys specified in home.handlebars
+  response.render('home', context);
 });
 
 app.get('/', (request, response) => {
