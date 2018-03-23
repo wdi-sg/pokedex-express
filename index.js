3b49521e192af053c71d1180b207caff3ecda541
enkai87 <tanenkai@firebelly.com.sg>
2018-03-23 14:21:25 +0800

    submission

--- index.js
+++ index.js
@@ -1,40 +1,86 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const jsonfile = require('jsonfile');
 
-// const jsonfile = require('jsonfile');
+const FILE = 'pokedex.json';
+// const pokemonFile = 'pokedex.json';
 
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 
 // Set handlebars to be the default view engine
 app.engine('handlebars', handlebars.create().engine);
 app.set('view engine', 'handlebars');
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+// Qns1: Return a string response "Welcome to the online Pokedex!" when a request for the root route (/) is received
+// app.get(('/'), function(request, response) {
+// 	response.send('Welcome to the online Pokedex!')
+// });
 
-app.get('/names/:pokemon', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.params.pokemon);
-});
+// Qns2: Return a string response with the requested pokemon's information when a request comes with matching the route /names/:name (eg. localhost:3000/names/Bulbasaur should show Bulbasaur's information - for now, show only its weight value)
+// app.get('/names/:name', (request, response) => {
+// 	jsonfile.readFile(FILE, function(err,obj) {
+	
+// 	var PokemonWeight;
+// 	var PokemonName = request.params.name;
+// 	var item = obj["pokemon"];
+// 		for(i=0; i < item.length; i++) {
+// 			if(item[i]["name"] == PokemonName) {
+// 				var PokemonWeight = item[i]["weight"];
+// 				response.send(PokemonWeight);
+// 			}
+// 		}
+// 	});
+// });
 
+// Qns3: Modify your response for /names/:name to return a HTML page (instead of just a string) with a h1 tag that displays the name of the pokemon being requested, and a ul displaying its weight (eg. "Weight: 10 kg")
+// app.get('/names/:name', (request, response) => {
+// 	jsonfile.readFile(FILE, function(err,obj) {
+// 		var PokemonName = request.params.name;
+// 		var item = obj["pokemon"];
+// 			for(i=0; i < item.length; i++) {
+// 				if(item[i]["name"] == PokemonName) {
+// 					var PokemonWeight = item[i]["weight"];
+// 				};
+// 				}
+// 				var context = {
+// 						PokemonName: PokemonName,
+// 						PokemonWeight: PokemonWeight			
+// 	};
+// 	response.render('home', context);
+// });
+// });
+
+// Qns4: 
+// app.get('/names/:name', (request, response) => {
+// 	jsonfile.readFile(FILE, function(err,obj) {
+// 		var PokemonName = request.params.name;
+// 		var item = obj["pokemon"];
+// 			for(i=0; i < item.length; i++) {
+// 				if(item[i]["name"] !== PokemonName) {
+// 					var PokemonWeight = ["Pokemon not found"];
+// 				}
+// 	}
+// 	var context = {
+// 		PokemonName: PokemonName,
+// 		PokemonWeight: PokemonWeight
+// 	};
+// 	response.render('invalidname', context);
+// });
+// });
+
+// Qns5:
 app.get('/', (request, response) => {
-  // send response with some data (a HTML file)
-  response.render('home');
+	jsonfile.readFile(FILE, function(err,obj) {
+		var item = obj.pokemon;
+		var PokemonList = [];
+			for(i=0; i < item.length; i++) {
+				PokemonList.push(item[i]["name"]);
+			}
+			var context = {
+				PokemonList: PokemonList
+			};
+			response.render('list', context);
+	});
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
