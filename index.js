ab492fcb22f022343ffcb735e600da3fbf0b86b2
samywamy <sam.alexis.ng@gmail.com>
2018-06-21 21:28:15 +0800

    find pokemon weight

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const fileName = 'pokedex.json';
+
+var pokedex;
 
 /**
  * ===================================
@@ -19,12 +23,36 @@ const app = express();
 
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+  let pokeName = request.path.substring(1).toLowerCase(); // using substring(1) gets rid of the slash in request.path. index(0) is dropped.
+  let pokemon = findPokemon(pokeName);
+  response.send(pokemon.weight);
 });
 
+var findPokemon = (pokemon) => {
+	let pokeArr = pokedex.pokemon;
+	for (var i = 0; i < pokeArr.length; i++) {
+		let poke = pokeArr[i];
+		if (pokemon == poke.name.toLowerCase()) {
+			return poke;
+		}
+	}
+	return undefined;
+}
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+// Write your solutions below
+
+jsonfile.readFile(fileName, function(err, obj) {                                      
+    if (err == undefined) { 
+    	pokedex = obj;
+    } else {
+        console.error(err);
+    }
+
+});
