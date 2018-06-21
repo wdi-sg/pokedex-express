c944d4a929c7890845af4530be7f6749a5651ad7
Phangster <mynamebryanphang@gmail.com>
2018-06-22 03:21:03 +0800

    completed deliverables

--- index.js
+++ index.js
@@ -7,24 +7,81 @@ const pokedex = 'pokedex.json'; //pokedex library
 const app = express();
 
 jsonfile.readFile(pokedex, function(err, obj) { //READ FILE
-	//items=obj[0].name;
-	// console.log(obj.pokemon[2]); 
-});
+	var object = obj;
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
-var responseCallback = (request, response) => {
-	console.log(request.path);
-// send response with some data (a string) 
-	if ( request.path == "/bulbasaur" ){
-		response.send("<html><body><h1>Bulbasaur</h1><ul>6.9kg</ul></body></html>");
-	}else{
-  		response.send(';)');
+function getPokemonName(name){
+	let pokemon = object.pokemon;
+	for( let i = 0; i<pokemon.length; i++ ){
+		if( pokemon[i].name.toLowerCase() == name.toLowerCase()){
+			return pokemon[i];
+		}
 	}
 };
+
+function html(pokemon){ //pokemon will be in a object
+	let str = '';
+	for(let key in pokemon){
+		str += "<html><body>" + key + ":" + pokemon[key] + "</body></html>";
+	}
+	return str
+};
+
+function error(name){
+	var message = "<p>Could not find information about <b>" + name + "</b> Is that a new pokemon? Gotta catch em' all!</p>";
+	html ="<html><body>" + message + "</body></html>";
+	return html
+};
+
+function mainpage(){
+	let pokemon = object.pokemon;
+	let heading = '<h1>Welcome to the online Pokedex!</h1>'
+	let list = '';
+	for( let i = 0; i<pokemon.length; i++ ){
+		let name = pokemon[i].name;
+		list += "<li>" + name + "</li>";
+	}
+	let data = "<ul>" + list + "</ul>";
+	return heading + data
+}
+function successful(name, pokemon){
+	for( let i = 0; i<pokemon.length; i++ ){
+		let heading = "<h1>" + name + "</h1>";
+		let weight = pokemon[i].weight;
+		let content = "<ul>" + weight + "</ul>";
+	}
+	let html = "<html><body>" + heading + "<br></br>" + weight + "</body></html>";
+	return html
+}
+
+var responseCallback = (request, response) => {
+	var str = request.path;
+	var search = str.slice(1);
+
+	let pokemon = object.pokemon;
+	for( let i = 0; i<pokemon.length; i++ ){
+		name = pokemon[i].name;
+		
+		if (name == search){
+			response.send(successful(name, pokemon));
+
+    	}else if( request.path !== str ){
+			response.send(error(name));
+			response.status(404);
+
+		}else if( request.path == false ){
+			response.send(mainpage());
+		}
+	}
+}
+
+// send response with some data (a string) 
+	// if ( request.path == "/bulbasaur" ){
+	// 	response.send("<html><body><p>Could not find information about <pokemon_name> - Is that a new pokemon? Gotta catch em' all!</p></body></html>");
+	// 	response.status(404);
+	// }else{
+ //  		response.send(';)');
+	// }
+// };
 
 app.get('*', responseCallback);
 /**
@@ -33,3 +90,4 @@ app.get('*', responseCallback);
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+});
