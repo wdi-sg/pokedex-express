63c6113e9dd813b6f8453b1f1bb6c9c3ececdf2f
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-06-22 00:49:08 +0800

    try to do further 1

--- index.js
+++ index.js
@@ -32,24 +32,26 @@ app.get('*', (request, response) => {
 		let search = []
 		
 		for (var i = 0; i < obj.pokemon.length; i ++) {
-    		//check for match
+    		//check for match, added / to pokemon database name
     		if ('/' + obj.pokemon[i].name == searchedPokemon) {  		
-    		var heading = '<h1>' + obj.pokemon[i].name + '</h1>'
-    		var	weight = '<ul>' + 'Weight: '+ obj.pokemon[i].weight + '</ul>'
-    		//if match push into array with relevant html tags
-    		search.push('<html> <body>' + heading + weight + '</body> </html>');
+    			var heading = '<h1>' + obj.pokemon[i].name + '</h1>'
+    			var	weight = '<ul>' + 'Weight: '+ obj.pokemon[i].weight + '</ul>'
+    			//if match push into array with relevant html tags
+    			search.push('<html> <body>' + heading + weight + '</body> </html>');
  
     		}
+
   		}
   		//join the array to return a string
   		if (search.length == 1) {
-  		response.send(search.join());
+  			response.send(search.join());
   		
   		}
   		//as there is no result the array is empty, therefore
   		if (search.length < 1) {
+  			//just to remove /
   			searchedPokemon =searchedPokemon.split('/').join('')
-  		response.status(404).send("<p>Could not find information about </p> " + searchedPokemon + " <p> Is that a new pokemon? Gotta catch em' all! </p>")
+  			response.status(404).send("<html> <body> <p>Could not find information about </p> " + searchedPokemon + " <p> Is that a new pokemon? Gotta catch em' all! </p> </body> </html>")
   		}
   				
 	})
@@ -64,9 +66,16 @@ app.get('*', (request, response) => {
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
+// //further 1 effort
+// let complete = []
 
-
-
+// if (searchedPokemon === '/'){
+// 	var heading = '<h1> Welcome to the Jungle !</h1>'
+    
+//     for (var i = 0; i < obj.pokemon.length; i ++){
+//     	var list ='<li>'+ obj.pokemon[i].name +'</li>'
+// 	}
+// }
 
 
 
