0872c3a5df5adf8b75fd8bc4df61262e6313dfca
samywamy <sam.alexis.ng@gmail.com>
2018-06-21 23:18:32 +0800

    Further1, display all pokemon

--- index.js
+++ index.js
@@ -21,16 +21,24 @@ const app = express();
  * ===================================
  */
 
+ // detect if the user didn't put anthing in the path. 
+ // Return a HTML page with a h1 tag saying "Welcome to the online Pokdex!" 
+ // and a ul displaying all the pokemon that exist in the pokedex
+
 app.get('*', (request, response) => {
 	// send response with some data (a string)
-	let pokeName = request.path.substring(1).toLowerCase(); // using substring(1) gets rid of the slash in request.path. index(0) is dropped.
-	let pokemon = findPokemon(pokeName);
-	if (pokemon == undefined) {
-		response.status(404);
-		response.send('<html><body><p>Could not find information about ' + pokeName + ' - Is that a new pokemon? Gotta catch em\' all!<p/></body></html>');
+	if (request.path == '/') {
+		response.send(listPokemons());
 	} else {
-		response.send(findPokemonNameWeight(pokemon));
-	}
+		let pokeName = request.path.substring(1).toLowerCase(); // using substring(1) gets rid of the slash in request.path. index(0) is dropped.
+		let pokemon = findPokemon(pokeName);
+		if (pokemon == undefined) {
+			response.status(404);
+			response.send('<html><body><p>Could not find information about ' + pokeName + ' - Is that a new pokemon? Gotta catch em\' all!<p/></body></html>');
+		} else {
+			response.send(findPokemonNameWeight(pokemon));
+		}
+	}	
 });
 
 var findPokemon = (pokemon) => {
@@ -48,12 +56,23 @@ var findPokemonNameWeight = (pokemon) => {
 	return ('<html><body><h1>' + pokemon.name + '</h1><ul>' + pokemon.weight + '</ul></body></html>');
 };
 
+var listPokemons = () => {
+	let htmlString = '<html><body><h1>Welcome to the online Pokdex!</h1>';
+	let pokeArr = pokedex.pokemon;
+	for (var i = 0; i < pokeArr.length; i++) {
+		let ul = '<ul>' + pokeArr[i].name + '</ul>';
+		htmlString = htmlString + ul;	
+	}
+	htmlString = htmlString + '</body></html>';
+	return htmlString;
+};
+
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () => console.log('~~~ Tuning in to the pika pika waves of port 3000 ~~~'));
 
 
 // Write your solutions below
