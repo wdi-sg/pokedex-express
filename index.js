5dbcc1462aa601a8b487a2d9959f82936de70e77
Lee Wen Yao <peterleewenyao@gmail.com>
2018-06-21 22:01:50 +0800

    homework

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+let pokemonName;
+let pokemonDetails;
+let pokemonWeight;
+let pokemonHeight;
+var pokemonObj = {};
+ const jsonfile = require('jsonfile');
+ const pokedex = './pokedex.json';
+ jsonfile.readFile(pokedex, function(err, obj) {
+ 	pokemonObj = obj;
 
-// const jsonfile = require('jsonfile');
+ 	 //console.log(pokemonObj.pokemon.length);
+ 	 //console.log(pokemonObj.pokemon.length);
+ 	 //console.log(obj.pokemon[0].weight);
+ 	 //console.log(obj)
+ })
 
 /**
  * ===================================
@@ -17,14 +30,39 @@ const app = express();
  * ===================================
  */
 
+
+// let pokemonSearch = function(pokemons, userSearch){
+	
+// 	for(var i = 0; i < pokemons.pokemon.length; i++){
+// 		if (userSearch == '/'+pokemons.pokemon[i].name){
+// 			let pokemonDetails = pokemons.pokemon[i].name+'s weight is '+pokemons.pokemon[i].weight+' and its height is '+pokemons.pokemon[i].height;
+// 		}
+// 	} return pokemonDetails;
+// }
+
+
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
-});
+  let userPokemonSearch = request.path.split('/');
+  console.log(userPokemonSearch);
+  for(var i = 0; i < pokemonObj.pokemon.length; i++){
+  	if (userPokemonSearch == +pokemonObj.pokemon[i].name){
+  		pokemonName = pokemonObj.pokemon[i].name;
+  		pokemonWeight = pokemons.pokemon[i].weight;
+  		pokemonHeight = pokemons.pokemon[i].height;
+  }
+} response.send(pokemonName+' is '+pokemonWeight+' and '+pokemonHeight);
+ 	
+	//response.send(pokemonSearch(pokemonObj, userPokemonSearch));
+
+	// if(request.path == 'bulbasaur'){
+	// 	response.send('ok')
+	// }
+ });
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 !~~~'));
