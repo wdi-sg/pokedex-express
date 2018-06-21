339f433b701320ea369f4dd92e6b2a1216cbc9ea
mirage1129 <meraj.a.qazi@gmail.com>
2018-06-21 21:12:07 +0800

    Did the first part of extra credit

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const jsonfile = require('jsonfile');
 const pokedex = 'pokedex.json'
-var result = {};	
+var result = {};	 
+var ulList = '';
 
 //read file function
-jsonfile.readFile(pokedex, (err, obj) => {
+jsonfile.readFile(pokedex, (err, obj) => { 
 	
 	// loops through the length of the pokemon file  	
  	for (let i = 0; i<obj.pokemon.length; i++) {
@@ -19,9 +20,12 @@ jsonfile.readFile(pokedex, (err, obj) => {
 		
 		//pushes the name and weight as key and values into results object
 		result[pokeName] = pokeWeight;
+		
+		//adds each pokemon between li tags so it can be plugged into the HTML variable later
+		ulList += '<li>'+pokeName+'</li>'
 
  	}
-	
+
 
 	app.get('*', (request, response) => {
 	
@@ -29,14 +33,23 @@ jsonfile.readFile(pokedex, (err, obj) => {
 		let route = request.path
 		let routeWithoutSlash = route.substring(1);
 		let compare = routeWithoutSlash.charAt(0).toUpperCase() + routeWithoutSlash.slice(1);
+		let bodyTags = '<head><title>Look at all them Pokemans!</title><head><body><h1>Welcome to the online Pokerdex!</h1><h2>Woulda look at that, pokermons. Just look at em. They everywhere.</h2><ul>' + ulList + '</ul></body>'
 
-		//if request path is in the object then send the name of it, if not say 'not here'
+		//if request path is in the object 
 		if (result.hasOwnProperty(compare) === true) {
+
+		//then send the weight of the pokemon
 			response.send(result[compare]);
+
+		//or if the result path is empty
+		} else if (routeWithoutSlash === '') {
+		
+		//show H1 tag and ul tag 
+			response.send(bodyTags);
+
 		} else { 
 			response.send('Not here')
 		}
-
 	});
 })
 
