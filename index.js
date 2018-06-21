6b782f155f575f29ff511caa6ef9600dba3b1d5b
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-06-21 21:40:03 +0800

    basic deliverables done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const file = 'pokedex.json'
+
 
 /**
  * ===================================
@@ -18,8 +21,36 @@ const app = express();
  */
 
 app.get('*', (request, response) => {
+
+	var searchedPokemon = request.path
+
+	var pokemon;
+
+	jsonfile.readFile(file, (err, obj) => {
+
+		let search = []
+		
+		for (var i = 0; i < obj.pokemon.length; i ++) {
+    		if ('/' + obj.pokemon[i].name == searchedPokemon) {  		
+    		var heading = '<h1>' + obj.pokemon[i].name + '</h1>'
+    		var	description = '<ul>' + 'Weight: '+ obj.pokemon[i].weight + '</ul>'
+    		result.push('<html> <body>' + heading + description + '</body> </html>');
+ 
+    		}
+  		}
+
+  		if (search.length == 1) {
+  		response.send(result.join());
+  		
+  		}
+
+  		if (search.length < 1) {
+  		response.status(404).send("<p>Could not find information about </p> " + searchedPokemon + " <p> Is that a new pokemon? Gotta catch em' all! </p>")
+  		}
+  				
+	})
   // send response with some data (a string)
-  response.send(request.path);
+  //response.send(pokemon);
 });
 
 /**
@@ -28,3 +59,12 @@ app.get('*', (request, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
+
+
+
+
+
+
