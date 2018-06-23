cda4e9d29dfab169e421efd83d12992fbfa5049c
delontoh <delontoh89@gmail.com>
2018-06-23 19:24:12 +0800

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const myjsonfile = require('jsonfile');
 
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
+const pokedex = "./pokedex.json";
 
-// Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+app.get('/', (req, res) => {
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+	myjsonfile.readFile(pokedex, (err, obj) => {
+
+			let result = [];
+
+			let header = '<h1>Welcome to the online Pokedex!</h1>';
+			let subheader = '<h2>We have 151 Pokemon in total!</h2>';
+			let openUL = '<ul>';
+			let closeUL = '</ul>';
+
+			result.push(header, subheader, openUL);  // push h1, h2 and openUL into array.
+
+			let pokemonList = obj.pokemon
+
+			for(let i=0; i<pokemonList; i++) {
+				let selectPokemon = pokemonList[i];
+				let pokemonName = selectPokemon.name;
+
+				result.push('<li>' + pokemonName + '</li>') //creates list for every pokemon in pokemonList array.
+			};
+
+			result.push(closeUL);
+			let display = result.join('');
+
+			res.send(display); //joins all elements in array into a string.
+	});
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+app.get('*', (req, res) => {
+
+	const PATH_REQ = req.path
+	let splitPath = PATH_REQ.split('/'); // split string into an array.
+	let lastParam = splitPath[splitPath.length -1]; 
+	
+	myjsonfile.readFile(pokedex, (err, obj) => {
+
+		let pokemonList = obj.pokemon;
+
+		for(let i=0; i<pokemonList.length; i++) {
+
+			if(lastParam.toLowerCase() === pokemonList[i].name.toLowerCase()) {
+
+				let pokemonName = '<h1>' + pokemonList[i].name + '</h1>';	// display pokemone name
+				let propertiesArr = [];
+
+				for(keys in pokemonList[i]) {
+					let properties = '<li>' + pokemonList[i][keys] + '</li>';
+						propertiesArr.push(properties);
+					// if(keys === 'prev_evolution' || keys === 'next_evolution') {
+					// 	for(j=0; j<)
+					// }
+
+					}
+				res.send(pokemonName + '<ul>' + propertiesArr.join('')) + '</ul>'
+			};
+		};
+	})
+});
+
+
+
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+// Note: HTTP comms protocol : 1 request => 1 response
+// app.get("*", (request,response) => {
+// 			response.send(...);
+//			response.send(...); ----> does not work
+//			})
