eac839f6a30e8670612f3b5ed341a1a1836e6959
Lee Wen Yao <peterleewenyao@gmail.com>
2018-06-23 16:49:01 +0800

    Q1

--- index.js
+++ index.js
@@ -1,68 +1,45 @@
 const express = require('express');
-let pokemonName;
-let pokemonDetails;
-let pokemonWeight;
-let pokemonHeight;
-var pokemonObj = {};
- const jsonfile = require('jsonfile');
- const pokedex = './pokedex.json';
- jsonfile.readFile(pokedex, function(err, obj) {
- 	pokemonObj = obj;
-
- 	 //console.log(pokemonObj.pokemon.length);
- 	 //console.log(pokemonObj.pokemon.length);
- 	 //console.log(obj.pokemon[0].weight);
- 	 //console.log(obj)
- })
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
+const jsonfile = require('jsonfile');
+const fileName = 'pokedex.json';
+let weight;
+let height;
+let pokeName;
+// const jsonfile = require('jsonfile');
+let pokedex;
+jsonfile.readFile(fileName, (err, obj) => {
+  //console.log(pokedex.pokemon[0].height);
+  pokedex = obj;
+})
 
 // Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
-
-
-// let pokemonSearch = function(pokemons, userSearch){
-	
-// 	for(var i = 0; i < pokemons.pokemon.length; i++){
-// 		if (userSearch == '/'+pokemons.pokemon[i].name){
-// 			let pokemonDetails = pokemons.pokemon[i].name+'s weight is '+pokemons.pokemon[i].weight+' and its height is '+pokemons.pokemon[i].height;
-// 		}
-// 	} return pokemonDetails;
-// }
-
+//the function to get requested pokemon details
+var getPokemondDetails = function(allPokemonsInfo, requestFromUser){
+  for(let i = 0; i < allPokemonsInfo.pokemon.length; i++){
+  if (requestFromUser == allPokemonsInfo.pokemon[i].name){
+     weight = allPokemonsInfo.pokemon[i].weight;
+     height = allPokemonsInfo.pokemon[i].height;
+     pokeName = allPokemonsInfo.pokemon[i].name;
+    return (pokeName+" "+weight+" "+height);
+}
+}
+}
 
 app.get('*', (request, response) => {
+  //the response is to send pokemon name weight and height
+  let requestedPokemon = request.path.substring(1);
+  
   // send response with some data (a string)
-  let userPokemonSearch = request.path.split('/');
-  console.log(userPokemonSearch);
-  for(var i = 0; i < pokemonObj.pokemon.length; i++){
-  	if (userPokemonSearch == +pokemonObj.pokemon[i].name){
-  		pokemonName = pokemonObj.pokemon[i].name;
-  		pokemonWeight = pokemons.pokemon[i].weight;
-  		pokemonHeight = pokemons.pokemon[i].height;
-  }
-} response.send(pokemonName+' is '+pokemonWeight+' and '+pokemonHeight);
- 	
-	//response.send(pokemonSearch(pokemonObj, userPokemonSearch));
+  console.log(pokedex);
+  response.send(pokemonDetailsWithHtml(pokedex, requestedPokemon));
+});
 
-	// if(request.path == 'bulbasaur'){
-	// 	response.send('ok')
-	// }
- });
+
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 !~~~'));
+app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
