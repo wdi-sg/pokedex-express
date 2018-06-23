7740bd2a23657d6d050e83a69673503445bf12e1
JulianPoh <julianpoh01@gmail.com>
2018-06-23 22:32:27 +0800

    made changes to js file

--- index.js
+++ index.js
@@ -4,25 +4,33 @@ const express = require('express');
 const jsonfile = require('jsonfile');
 const file = 'pokedex.json';
 
+//Function to get Pokemon Name and Weight.
+
 // Init express app
 const app = express();
 
 jsonfile.readFile(file, (err, obj) => {
-    let pokedex = obj.pokemon
-    let result = []
-    for (var i=0; i<obj.pokemon.length; i++) {
-        if (var userSearch == pokedex[i].name) {
-            console.log(pokedex.[i].name + " Weight: " + pokedex[i].weight);
-        }          
-    }
-})
 
-
-
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+//Root Route
+app.get('/', (request, response) => {
+  response.send("Welcome to the Pokedex!");
 });
+
+
+app.get('/*', (request, response) => {
+    jsonfile.readFile(file, (err, obj) => {
+        let pokedex = obj.pokedex;
+        let searchResult = [];
+        for (i=0; i<pokedex.length; i++) {
+            if (request.path == pokedex[i].name) {
+                var pokeName = pokedex[i].name;
+                var pokeWeight = pokedex[i].weight;
+                searchResult.push("<h1>" + pokeName + "</h1><ul>Weight :" + pokeWeight + "</ul>");
+            }
+        }
+    })
+    response.send(searchResult);
+})
 
 /**
  * ===================================
@@ -30,3 +38,4 @@ app.get('*', (request, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+})
