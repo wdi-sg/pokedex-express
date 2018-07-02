4d491234aea3af6d1f348d9a79f775443c7c0e89
hygro5d <faidhiamran@Faidhis-MacBook-Pro.local>
2018-07-02 22:27:27 +0800

    initial

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 // const jsonfile = require('jsonfile');
-
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
 /**
  * ===================================
  * Configurations and set up
@@ -11,16 +12,44 @@ const express = require('express');
 // Init express app
 const app = express();
 
+ jsonfile.readFile(file, (err, obj) => {
+    let pokedex = obj.pokemon
+    let result = []
+    for (var i=0; i<obj.pokemon.length; i++) {
+        if (var userSearch == pokedex[i].name) {
+            console.log(pokedex.[i].name + " Weight: " + pokedex[i].weight);
+        }
+    }
+})
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
+ app.get('/', (request, response) => {
+   response.send("Welcome to the Pokedex!");
+ });
+
 
 app.get('*', (request, response) => {
-  // send response with some data (a string)
   response.send(request.path);
 });
+
+app.get('/*', (request, response) => {
+    jsonfile.readFile(file, (err, obj) => {
+        let pokedex = obj.pokedex;
+        let searchResult = [];
+        for (i=0; i<pokedex.length; i++) {
+            if (request.path == pokedex[i].name) {
+                var pokeName = pokedex[i].name;
+                var pokeWeight = pokedex[i].weight;
+                searchResult.push("<h1>" + pokeName + "</h1><ul>Weight :" + pokeWeight + "</ul>");
+            }
+        }
+    })
+    response.send(searchResult);
+})
 
 /**
  * ===================================
