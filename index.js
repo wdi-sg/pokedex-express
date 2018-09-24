a7c2ffbf9aebe890dd3b53c36e95b54844157850
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-24 22:34:22 +0800

    Complete till further 2.4

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ const jsonfile = require('jsonfile');
 const app = express();
 
 // FUNCTIONS
+const checkType = (param, p) => {
+  const payload = [];
+  Object.keys(p).forEach((key) => {
+    const pokemon = p[key];
+    Object.keys(pokemon.type).forEach((x) => {
+      if (pokemon.type[x] === param) {
+        payload.push(pokemon.name);
+      }
+    });
+  });
+  return payload;
+};
+
+const checkWeakness = (param, p) => {
+  const payload = [];
+  Object.keys(p).forEach((key) => {
+    const pokemon = p[key];
+    Object.keys(pokemon.weaknesses).forEach((x) => {
+      if (pokemon.weaknesses[x] === param) {
+        payload.push(pokemon.name);
+      }
+    });
+  });
+  return payload;
+};
+
 
 // MAGIC HAPPENS BELOW
 jsonfile.readFile('pokedex.json', (err, obj) => {
@@ -13,6 +39,13 @@ jsonfile.readFile('pokedex.json', (err, obj) => {
     const p = obj.pokemon;
 
     app.get('*', (req, res) => {
+      // FURTHER 2.3
+      if (req.path.slice(0, 6) === '/type/') {
+        return res.send(checkType(req.path.substring(6), p));
+      }
+      // FURTHER 2.4
+      if (req.path.slice(0, 12) === '/weaknesses/') return res.send(checkWeakness(req.path.substring(12), p));
+
       const param = req.path.substring(1); // Remove '/' from req
       let found = false;
       Object.keys(p).forEach((key) => {
@@ -23,17 +56,16 @@ jsonfile.readFile('pokedex.json', (err, obj) => {
           // payload.push(pokedex[key].weight);
           // FURTHER 2.2
           const types = p[key].type.toString().replace(',', ' & ');
-          res.send(`This is ${p[key].name}, it weighs ${p[key].weight}. It is ${p[key].height} tall. It is of ${types} type.`);
           found = true;
+          return res.send(`This is ${p[key].name}, it weighs ${p[key].weight}. It is ${p[key].height} tall. It is of ${types} type.`);
         }
       });
       // FURTHER 1
       if (found === false && param.length > 0) {
-        res.status(404).send(`Could not find information about ${param}- Is that a new pokemon? Gotta catch em' all!`);
-      } else {
-        // FURTHER 2.1
-        res.send('Welcome to the online pokedex.');
+        return res.status(404).send(`Could not find information about ${param}- Is that a new pokemon? Gotta catch em' all!`);
       }
+      // FURTHER 2.1
+      return res.send('Welcome to the online pokedex.');
     });
   }
 });
