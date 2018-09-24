f62640ecb35104a8f5d0e6c786a2c799378425ef
lyvzm <chanleyou@gmail.com>
2018-09-24 23:30:32 +0800

    completed assignment

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express();
 
 app.get('*', (request, response) => {
 
-  let output = [];
-
   let searchTerm = request.path.toLowerCase();
 
   while (searchTerm.charAt(0) === '/') {
@@ -17,29 +15,77 @@ app.get('*', (request, response) => {
 
   if (searchTerm === "") {
     response.send("Welcome to the online Pokedex!");
-    return;
   }
 
   json.readFile(pokedex, function(err, obj) {
 
     const pokemon = obj.pokemon;
+    let output = [];
 
-    for (let i in pokemon) {
-      if (pokemon[i].name.toLowerCase().includes(searchTerm)) {
-        output.push(pokemon[i]);
+    if (searchTerm.startsWith("type/")) {
+
+      let searchType = searchTerm.substr(5);
+
+      for (let i in pokemon) {
+        for (let y in pokemon[i].type) {
+          if (pokemon[i].type[y].toLowerCase() === searchType) {
+            output.push(pokemon[i].name);
+          }
+        }
+      }
+    } else if (searchTerm.startsWith("weakness/")) {
+
+      let searchWeakness = searchTerm.substr(9);
+
+      for (let i in pokemon) {
+        for (let y in pokemon[i].weaknesses) {
+          if (pokemon[i].weaknesses[y].toLowerCase() === searchWeakness) {
+            output.push(pokemon[i].name);
+          }
+        }
+      }
+    } else if (searchTerm.startsWith("nextevolution/")) {
+
+      let searchEvo = searchTerm.substr(14);
+
+      for (let i in pokemon) {
+        if (pokemon[i].next_evolution) {
+          for (let y in pokemon[i].next_evolution) {
+            if (pokemon[i].next_evolution[y].name.toLowerCase() === searchEvo) {
+              output.push(pokemon[i].name);
+            }
+          }
+        }
+      }
+    } else {
+      for (let i in pokemon) {
+        if (pokemon[i].name.toLowerCase().includes(searchTerm)) {
+
+          let name = pokemon[i].name;
+          let type = "";
+          let weight = pokemon[i].weight;
+
+          if (pokemon[i].type.length === 2) {
+            type = `${pokemon[i].type[0]}/${pokemon[i].type[1]}`;
+          } else {
+            type = pokemon[i].type[0];
+          }
+
+          output.push(`${name} is a ${type}-type Pokemon! It weighs ${weight}.`);
+        }
       }
     }
 
-    if (output.length > 0) {
-      response.send(output);
+    if (output.length === 0) {
+      response.send(`Search for '${searchTerm}' returned no results.`);
     } else {
-      response.send (`Search for '${searchTerm}' returned no results.`);
+      response.send(output);
     }
 
     if (err) {
-      output = err;
       console.log(err);
     }
+
   })
 })
 
