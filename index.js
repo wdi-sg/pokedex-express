53288eba34b50c07ae931636ffdd5373393b496e
saufm68 <saufm68@gmail.com>
2018-09-24 23:42:11 +0800

    Did all the way till the end

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const pokedex = 'pokedex.json';
+
+let listOfPokemon;
+
+jsonfile.readFile(pokedex, (err,obj) => {
+
+    listOfPokemon = obj;
+
+});
 
 /**
  * ===================================
@@ -19,8 +29,69 @@ const app = express();
 
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+
+  let pathRoute = request.path.split("/");
+  console.log(pathRoute);
+
+  if (pathRoute[1] === "") {
+
+    response.send("Welcome to the online Pokdex!");
+
+  } else if (pathRoute[1] === "type") {
+
+    var path = pathRoute[2][0].toUpperCase() + pathRoute[2].substring(1);
+
+    iterateArray("type", path, response);
+
+  } else if (pathRoute[1] === "weaknesses"){
+
+    var path = pathRoute[2][0].toUpperCase() + pathRoute[2].substring(1);
+
+    iterateArray("weaknesses", path, response);
+
+  } else {
+
+    var path = pathRoute[1][0].toUpperCase() + pathRoute[1].substring(1);
+    console.log(path);
+
+    for (let i = 0; i < listOfPokemon['pokemon'].length; i++) {
+
+        if (listOfPokemon['pokemon'][i]['name'] === path) {
+
+            response.send(listOfPokemon['pokemon'][i]);
+
+        }
+
+    }
+
+    response.status(404);
+    response.send("Could not find information about " + path);
+
+  }
+
+
 });
+
+
+function iterateArray(key, searchFor, response) {
+
+    const list = [];
+
+    for (let i = 0; i < listOfPokemon['pokemon'].length; i++) {
+
+        if (listOfPokemon['pokemon'][i][key].includes(searchFor)) {
+
+            list.push(listOfPokemon['pokemon'][i]);
+            check = true;
+
+        }
+
+    }
+
+    response.send(list);
+
+
+}
 
 /**
  * ===================================
