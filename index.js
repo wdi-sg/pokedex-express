0cbcad8d880f1443f97bd21073dc6af6c1ee85e3
wang0805 <wh_wang@live.com>
2018-09-24 18:42:55 +0800

    up till and before type

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
-
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
+var pokemon;
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
+jsonfile.readFile(file, function (err,obj){
+	if(err){console.error(err)}
+		//console.dir(obj);
+		pokemon = obj["pokemon"];
+		console.log(pokemon[0].name);
+})
+
 
 // Init express app
 const app = express();
@@ -19,8 +27,22 @@ const app = express();
 
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
-});
+	let exist = false;
+
+	for(var i=0;i<pokemon.length;i++){
+	  	if("/"+pokemon[i].name.toLowerCase() == request.path){
+	  		response.status(200).send("This is "+pokemon[i].name+ ", he is "+pokemon[i].weight+" in weight!");
+	  		exist = true;
+	  	}
+  	}
+	if(exist==false&&request.path!="/"){
+	  	var name = request.path.split("/")[1];
+	  	response.status(404).send("Could not find "+name+". Is that a new pokemon? Gotta catch em' all!");
+	}
+	if(request.path=="/"){
+	  	response.status(200).send("Welcome to Pokedex!");
+	}
+ })
 
 /**
  * ===================================
