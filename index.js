5832df6d3fdb3f3fbfb6cb6d6c5db61944816726
ChloeLiang <nliangxin@gmail.com>
2018-09-25 21:25:37 +0800

    Refactor

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const express = require('express');
 const app = express();
 const file = 'pokedex.json';
 
+const capitalizeFirstLetter = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
 const getHtmlPage = content => {
   let html = '';
   html += '<html>';
   html += '<head>';
   html += '<title>Pokemon</title>';
-  html += '<style>* {padding: 0; margin: 0; box-sizing: border-box;} body {padding: 2rem; text-align: center; font-family: sans-serif;} h1 {padding-bottom: 1rem;} h2 {padding: 1rem;} ul {display: inline-block; color: grey;} p {color: grey;} a {text-decoration: none; color: grey; display: inline-block; margin-bottom: 1rem;}</style>';
+  html += '<style>* {padding: 0; margin: 0; box-sizing: border-box;} body {padding: 2rem; text-align: center; font-family: sans-serif;} h1 {padding-bottom: 1rem;} h3 {padding: .5rem;} ul {display: inline-block; color: grey;} p {color: grey;} a {text-decoration: none; color: grey; display: inline-block; margin-bottom: .5rem;}</style>';
   html += '</head>';
   html += '<body>';
   html += content;
@@ -32,7 +36,7 @@ const getHtmlList = arr => {
   return html;
 };
 
-const getLinkOfPokemons = arrOfPokemons => {
+const getPokemonLinks = arrOfPokemons => {
   content = '<ul>';
   arrOfPokemons.forEach(pokemon => {
     content += '<li><a href="/' + pokemon.name + '">' +
@@ -48,54 +52,19 @@ const getAllInfo = pokemon => {
 
   content += '<h1>' + pokemon.name + '</h1>';
   content += '<img src="' + pokemon.img + '">';
-  content += '<h2>Type</h2>';
-  content += '<ul>';
-  pokemon.type.forEach(type => {
-    content += '<li>' + type + '</li>';
+  Object.entries(pokemon).forEach(([key, value]) => {
+    if (key !== 'name' && key !== 'img') {
+      let title = capitalizeFirstLetter(key).replace('_', ' ');
+      content += '<h3>' + title + '</h3>';
+      if (key === 'type' || key === 'weaknesses' || key === 'multipliers') {
+        content += getHtmlList(value);
+      } else if (key === 'prev_evolution' || key === 'next_evolution') {
+        content += getPokemonLinks(value);
+      } else {
+        content += '<p>' + value + '</p>';
+      }
+    }
   });
-
-  content += '</ul>';
-  content += '<h2>Height</h2>';
-  content += '<p>' + pokemon.height + '</p>';
-  content += '<h2>Weight</h2>';
-  content += '<p>' + pokemon.weight + '</p>';
-  content += '<h2>Candy</h2>';
-  content += '<p>' + pokemon.candy + '</p>';
-  content += '<h2>Candy Count</h2>';
-  content += '<p>' + pokemon.candy_count + '</p>';
-  content += '<h2>Egg</h2>';
-  content += '<p>' + pokemon.egg + '</p>';
-  content += '<h2>Spawn Chance</h2>';
-  content += '<p>' + pokemon.spawn_chance + '</p>';
-  content += '<h2>Average Spawns</h2>';
-  content += '<p>' + pokemon.avg_spawns + '</p>';
-  content += '<h2>Spawn Time</h2>';
-  content += '<p>' + pokemon.spawn_time + '</p>';
-  content += '<h2>Multippliers</h2>';
-  content += getHtmlList(pokemon.multipliers);
-  content += '<h2>Weaknesses</h2>';
-  content += getHtmlList(pokemon.weaknesses);
-  content += '<h2>Previous Evolution</h2>';
-  if (pokemon.prev_evolution) {
-    content += '<ul>';
-    pokemon.prev_evolution.forEach(prev => {
-      content += '<li>' + prev.name + '</li>';
-    });
-    content += '</ul>';
-  } else {
-    content += '<p>Null</p>';
-  }
-
-  content += '<h2>Next Evolution</h2>';
-  if (pokemon.next_evolution) {
-    content += '<ul>';
-    pokemon.next_evolution.forEach(next => {
-      content += '<li>' + next.name + '</li>';
-    });
-    content += '</ul>';
-  } else {
-    content += '<p>Null</p>';
-  }
 
   return content;
 };
@@ -108,57 +77,45 @@ app.get('/', (request, response) => {
       return;
     }
 
-    content += getLinkOfPokemons(obj.pokemon);
+    content += getPokemonLinks(obj.pokemon);
     response.send(getHtmlPage(content));
   });
 });
 
 app.get('/:name', (request, response) => {
-  let nameRequested = request.params.name.toLowerCase();
-
   jsonfile.readFile(file, (err, obj) => {
     if (err) {
       response.send(getHtmlPage('<p>' + err + '</p>'));
       return;
     }
 
-    let i;
-    for (i = 0; i < obj.pokemon.length; i++) {
-      let pokemon = obj.pokemon[i];
-      if (pokemon.name.toLowerCase() === nameRequested) {
-        let content = getAllInfo(pokemon);
-        response.send(getHtmlPage(content));
-        break;
-      }
-    }
+    const nameRequested = request.params.name.toLowerCase();
+    const result = obj.pokemon.filter(pokemon => {
+      return pokemon.name.toLowerCase() === nameRequested;
+    });
 
-    if (i === obj.pokemon.length) {
-      response.status(303);
-      response.redirect('/');
+    if (result.length > 0) {
+      response.send(getHtmlPage(getAllInfo(result[0])));
+    } else {
+      response.status(303).redirect('/');
     }
   });
 });
 
 app.get('/type/:type', (request, response) => {
-  let typeRequested = request.params.type.charAt(0).toUpperCase() +
-    request.params.type.slice(1).toLowerCase();
-
   jsonfile.readFile(file, (err, obj) => {
     if (err) {
       response.send(getHtmlPage('<p>' + err + '</p>'));
       return;
     }
 
-    let names = [];
-    for (let i = 0; i < obj.pokemon.length; i++) {
-      let pokemon = obj.pokemon[i];
-      if (pokemon.type.includes(typeRequested)) {
-        names.push(pokemon.name);
-      }
-    }
+    const typeRequested = capitalizeFirstLetter(request.params.type);
+    const result = obj.pokemon.filter(pokemon => {
+      return pokemon.type.includes(typeRequested);
+    });
 
-    if (names.length > 0) {
-      response.send(getHtmlPage(getHtmlList(names)));
+    if (result.length > 0) {
+      response.send(getHtmlPage(getPokemonLinks(result)));
     } else {
       response.status(303).redirect('/');
     }
@@ -166,25 +123,19 @@ app.get('/type/:type', (request, response) => {
 });
 
 app.get('/weaknesses/:weakness', (request, response) => {
-  let weakness = request.params.weakness.charAt(0).toUpperCase() +
-    request.params.weakness.slice(1).toLowerCase();
-
   jsonfile.readFile(file, (err, obj) => {
     if (err) {
       response.send(getHtmlPage('<p>' + err + '</p>'));
       return;
     }
 
-    let names = [];
-    for (let i = 0; i < obj.pokemon.length; i++) {
-      let pokemon = obj.pokemon[i];
-      if (pokemon.weaknesses.includes(weakness)) {
-        names.push(pokemon.name);
-      }
-    }
+    const weakness = capitalizeFirstLetter(request.params.weakness);
+    const result = obj.pokemon.filter(pokemon => {
+      return pokemon.weaknesses.includes(weakness);
+    });
 
-    if (names.length > 0) {
-      response.send(getHtmlPage(getHtmlList(names)));
+    if (result.length > 0) {
+      response.send(getHtmlPage(getPokemonLinks(result)));
     } else {
       response.status(303).redirect('/');
     }
@@ -192,33 +143,32 @@ app.get('/weaknesses/:weakness', (request, response) => {
 });
 
 app.get('/nextevolution/:name', (request, response) => {
-  let name = request.params.name.toLowerCase();
-
   jsonfile.readFile(file, (err, obj) => {
     if (err) {
       response.send(getHtmlPage('<p>' + err + '</p>'));
       return;
     }
 
-    for (let i = 0; i < obj.pokemon.length; i++) {
-      let pokemon = obj.pokemon[i];
-      if (name === pokemon.name.toLowerCase() && pokemon.prev_evolution) {
-        let names = [];
-        pokemon.prev_evolution.forEach(pokemonObj => {
-          names.push(pokemonObj.name);
-        });
+    const name = request.params.name.toLowerCase();
+    const result = obj.pokemon.filter(pokemon => {
+      return pokemon.name.toLowerCase() === name && pokemon.prev_evolution;
+    });
 
-        if (names.length > 0) {
-          response.send(getHtmlPage(getHtmlList(names)));
-        } else {
-          response.status(303).redirect('/');
-        }
-      }
+    if (result.length > 0) {
+      response.send(getHtmlPage(getPokemonLinks(result[0].prev_evolution)));
+    } else {
+      response.status(303).redirect('/');
     }
   });
 });
 
-// /search/spawn_chance?amount=1&compare=less
+/**
+ * /search/spawn_chance?amount=1&compare=less
+ * /search/avg_spawns?amount=1&compare=more
+ * /search/spawn_time?amount=11:00&compare=less
+ * /search/height?amount=3&compare=more
+ * /search/weight?amount=2&compare=less
+ */
 app.get('/search/:searchBy', (request, response) => {
   jsonfile.readFile(file, (err, obj) => {
     if (err) {
@@ -226,21 +176,30 @@ app.get('/search/:searchBy', (request, response) => {
       return;
     }
 
-    let searchBy = request.params.searchBy.toLowerCase();
-    let amount = request.query.amount;
-    let compare = request.query.compare.toLowerCase();
-    let pokemons = obj.pokemon.filter(pokemon => {
+    const searchBy = request.params.searchBy.toLowerCase();
+    const compare = request.query.compare.toLowerCase();
+    const pokemons = obj.pokemon.filter(pokemon => {
+      let amount;
+      let value;
+      if (searchBy === 'spawn_time') {
+        amount = request.query.amount;
+        value = pokemon[searchBy];
+      } else {
+        amount = parseFloat(request.query.amount);
+        value = parseFloat(pokemon[searchBy]);
+      }
+
       if (compare === 'less') {
-        return pokemon[searchBy] < amount;
+        return value < amount;
       } else if (compare === 'more') {
-        return pokemon[searchBy] > amount;
+        return value > amount;
       } else {
         response.status(303).redirect('/');
       }
     });
 
-    response.send(getHtmlPage(getLinkOfPokemons(pokemons)));
+    response.send(getHtmlPage(getPokemonLinks(pokemons)));
   });
 });
 
-app.listen(3001, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
