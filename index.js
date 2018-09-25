c0120bd479748b606f9692685ec5b421313f1cf8
wandsen <dsenwan@gmail.com>
2018-09-26 02:33:23 +0800

    some weird interaction going on sometimes can load sometimes cant

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const file = "pokedex.json"
 
 
 
-var respondToRequest = function (request, response){
+var getPokemon = function (request, response){
   // send response with some data (a string)
-    var userInputPath = request.path.split("/")[1]
-
+    var userInputPath = request.path.split("/")[2].toLowerCase()
     //read pokedex.json file
     jsonfile.readFile (file, function(err, obj){
 
@@ -30,8 +29,20 @@ var respondToRequest = function (request, response){
                 //comparing what the user enter in url and the pokedex.json to get correct pokemon
 
                 if( userInputPath === element.name.toLowerCase() ){
-                    output = element.weight
-                    response.send(output)
+                    let html = "";
+
+                    html += "<html>"
+                    html += "<head>"
+                    html += "</head>"
+                    html += "<body>"
+                    html += "<img src=" + element.img + ">" + element.name
+
+
+                    html += "</body>"
+                    html += "</html>"
+
+                    console.log(html)
+                    response.send(html)
                 };
 
             });
@@ -60,9 +71,9 @@ jsonfile.readFile( file, function(err, obj){
 
 
 
-    var arrayListOfPokemon = obj.pokemon
+var arrayListOfPokemon = obj.pokemon
 
-    var outputList = []
+var outputList = []
 
     if (userInputPath[1] === "type"){
 
@@ -115,9 +126,8 @@ jsonfile.readFile( file, function(err, obj){
 }
 
 
-app.get('*', respondToType)
+//app.get('*', respondToType)
 
-//app.get('*', respondToRequest)
 
 
 /**
@@ -125,4 +135,159 @@ app.get('*', respondToType)
  * Listen to requests on port 3000
  * ===================================
  */
+
+
+
+/**
+ * ===================================
+ * Part 2 of exercise
+ * ===================================
+ */
+
+
+
+
+var respondToTypeHtml = function(request, response){
+
+
+
+var userInputPath = request.path.split("/")
+console.log(userInputPath[1])
+console.log(userInputPath[2])
+
+jsonfile.readFile( file, function(err, obj){
+
+
+
+var arrayListOfPokemon = obj.pokemon
+
+var outputList = []
+
+    if (userInputPath[1] === "type"){
+
+        //loop through each pokemon
+        arrayListOfPokemon.forEach(function(element){
+
+            //loop through each type
+            element.type.forEach(function(someType){
+                if(userInputPath[2] === someType.toLowerCase()){
+                    console.log("userInput2")
+                    outputList.push(element.name)
+                }
+            })
+        })
+    }
+
+    else if (userInputPath[1] === "weaknesses"){
+
+        //looping through each pokemon
+        arrayListOfPokemon.forEach(function(element){
+
+            //loop through array of weakness(there is more that one weakness)
+            element.weaknesses.forEach(function(someWeakness){
+                if(userInputPath[2] === someWeakness.toLowerCase()){
+                    outputList.push(element.name)
+                }
+            })
+        })
+    }
+
+    else if (userInputPath[1] === "nextevolution"){
+
+        //looping each pokemon
+        arrayListOfPokemon.forEach(function(element){
+            if(userInputPath[2] === element.name.toLowerCase()){
+
+                //loop through each stage of evolution
+                element.next_evolution.forEach(function(evolution){
+                   outputList.push(evolution.name)
+                })
+
+            }
+
+        })
+    }
+
+    let html = "";
+
+    html += "<html>"
+    html += "<head>"
+    html += "</head>"
+    html += "<body>"
+        html += "<ul>"
+
+        outputList.forEach(function(element){
+            html += "<li>" + element + "</li>"
+        })
+
+        html += "</ul>"
+    html += "</body>"
+    html += "</html>"
+
+    console.log(html)
+    response.send(html)
+})
+
+}
+
+
+// app.get('*', respondToTypeHtml)
+
+
+
+var rootPage = function(request, response){
+    var userInputPath = request.path.split("/")
+    console.log(request.path)
+    console.log(userInputPath[1])
+
+    jsonfile.readFile(file, function(err, obj){
+
+        var arrayListOfPokemon = obj.pokemon
+
+            let html = "";
+
+            html += "<html>"
+            html += "<head>"
+            html += "</head>"
+            html += "<body>"
+                html += "<ul>"
+
+            if (userInputPath[1] === "pokemon"){
+                arrayListOfPokemon.forEach(function(element){
+                    html += "<li>" + "<img src=" + element.img + ">" + element.name + "</li>"
+                    html += '<a href="/pokemon/'+element.name+'">Link to pokemon</a>'
+                })
+
+            }
+
+                html += "</ul>"
+            html += "</body>"
+            html += "</html>"
+
+
+            console.log(html)
+    response.send(html)
+
+    })
+
+}
+
+app.get("/pokemon", rootPage);
+
+
+app.get('/pokemon/*', getPokemon)
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
+
+
+
+
+
+
+
+
+
+
