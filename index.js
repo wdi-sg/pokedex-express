a55de3058f19776035bbe3a13217ef0512542fcf
mervyns <mervyns@gmail.com>
2018-09-25 17:21:18 +0800

    Completed all further parts of Day 1 of the Assignment

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const jsonfile = require("jsonfile");
 
 const file = "./pokedex.json";
-var pokedex;
+
 // Getting the Data from JSON Object
-jsonfile.readFile(file, function(err, obj) {
+jsonfile.readFile(file, (err, obj) => {
   if (err) {
     console.error(err);
   } else {
@@ -27,10 +27,16 @@ const app = express();
  * ===================================
  */
 
+// Further Part 2. Just check whether the request path is empty or `not. If empty, welcome user to the Pokedex
+app.get("/", (request, response) => {
+  response.send("Welcome to the online pokedex.");
+});
+
 app.get("*", (request, response) => {
   // send response with some data (a string)
   let pokemonName = request.path.substring(1).toLowerCase(); // using substring(1) gets rid of the slash in request.path.
   let pokemonResult = findPokemon(pokemonName);
+  let requestPath = request.path.split("/"); // Breaking the request path into different parts
   // Further Part 1
   // Just needed to add a if/else conditional for Further Part 1.
   if (pokemonResult) {
@@ -41,9 +47,18 @@ app.get("*", (request, response) => {
         pokemonResult.weight
       }. His height is : ${pokemonResult.height}.`
     );
-    // Further Part 2. Just check whether the request path is empty or `not. If empty, welcome user to the Pokedex
-  } else if (pokemonName === "") {
-    response.send("Welcome to the online pokedex.");
+  } else if (requestPath[1] == "type") {
+    // Matching the request path to type
+    let typeResult = findPokemonByType(requestPath[2]);
+    response.send(typeResult);
+  } else if (requestPath[1] == "weakness") {
+    // Matching the request path to weaknesses
+    let weaknessResult = findPokemonByWeakness(requestPath[2]);
+    response.send(weaknessResult);
+  } else if (requestPath[1] == "nextevolution") {
+    // Matching the request path to type
+    let evolutionResult = findPokemonByNextEvo(requestPath[2]);
+    response.send(evolutionResult);
   } else {
     response.send(
       `Could not find information about ${pokemonName} - Is that a new pokemon? Gotta catch em' all!`
@@ -52,14 +67,56 @@ app.get("*", (request, response) => {
 });
 
 var findPokemon = pokemonResult => {
-  let pokeArr = pokedex.pokemon;
-  for (var i = 0; i < pokeArr.length; i++) {
-    let pokemon = pokeArr[i];
+  const pokeArray = pokedex.pokemon;
+  for (var i = 0; i < pokeArray.length; i++) {
+    let pokemon = pokeArray[i];
     if (pokemonResult == pokemon.name.toLowerCase()) {
       return pokemon;
     }
   }
   return undefined;
+};
+
+const findPokemonByType = pokemonType => {
+  const pokeArray = pokedex.pokemon;
+  let arr = [];
+  for (var i = 0; i < pokeArray.length; i++) {
+    for (var j = 0; j < pokeArray[i].type.length; j++) {
+      if (pokemonType === pokeArray[i].type[j].toLowerCase()) {
+        arr.push(pokeArray[i].name);
+      }
+    }
+  }
+  return arr;
+};
+
+const findPokemonByWeakness = pokemonWeakness => {
+  const pokeArray = pokedex.pokemon;
+  let arr = [];
+  for (var i = 0; i < pokeArray.length; i++) {
+    for (var j = 0; j < pokeArray[i].weaknesses.length; j++) {
+      if (pokemonWeakness === pokeArray[i].weaknesses[j].toLowerCase()) {
+        arr.push(pokeArray[i].name);
+      }
+    }
+  }
+  return arr;
+};
+
+const findPokemonByNextEvo = pokemonEvo => {
+  const pokeArray = pokedex.pokemon;
+  let arr = [];
+  for (var i = 0; i < pokeArray.length; i++) {
+    if (
+      pokeArray[i].prev_evolution &&
+      pokeArray[i].name.toLowerCase() === pokemonEvo
+    ) {
+      for (var j = 0; j < pokeArray[i].prev_evolution.length; j++) {
+        arr.push(pokeArray[i].prev_evolution[j].name);
+      }
+    }
+  }
+  return arr;
 };
 
 /**
