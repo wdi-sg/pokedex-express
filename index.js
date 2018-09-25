b2c0ce9e478887d7439289562aec6b2aac9253d4
benghui <benghui@gmail.com>
2018-09-25 10:51:38 +0800

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const pokedex = 'pokedex.json';
+
+let pokeobj;
+let resultObj = {};
 
 /**
  * ===================================
@@ -19,7 +24,27 @@ const app = express();
 
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+  jsonfile.readFile(pokedex, function(err, obj) {
+    pokeobj = obj.pokemon;
+    // console.log(pokeobj)
+    /*for (let i = 0; i < pokeobj.length; i++){
+      let pokename = pokeobj[i].name.toLowerCase();
+      console.log(pokename);
+    }*/
+    obj.pokemon.forEach((pokemon,i) =>{resultObj[pokeobj[i].name.toLowerCase()] = pokemon})
+    //console.log(resultObj);
+      let pokename = Object.keys(resultObj);
+      // console.log(pokename);
+       let myParams = request.path.split("/")
+       console.log(myParams);
+
+       for(let i = 0; i < pokeobj.length; i++){
+         if(myParams.toLowerCase() == pokeobj[i].name.toLowerCase()){
+           response.send(request.path);
+         }
+       }
+  });
+// response.send(request.path);
 });
 
 /**
@@ -27,4 +52,4 @@ app.get('*', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3001, () => console.log('~~~ Tuning in to the waves of port 3001 ~~~'));
