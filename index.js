53ac19cf3e5472d607313c350cb9dcdf6af98980
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-25 20:01:09 +0800

    Complete all routes

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ const capitalize = (word) => {
   return capitalized;
 };
 
-const checkName = (param, p) => {
+const checkName = (param, pokedex) => {
   let status = false;
   let payload;
 
-  Object.keys(p).forEach((key) => {
-    if (p[key].name === param) {
-      payload = p[key];
+  Object.keys(pokedex).forEach((key) => {
+    if (pokedex[key].name === param) {
+      payload = pokedex[key];
       status = true;
     }
   });
   return [status, payload];
 };
 
-const checkType = (param, p) => {
+const checkType = (param, pokedex) => {
   const payload = [];
-  Object.keys(p).forEach((key) => {
-    const pokemon = p[key];
+  Object.keys(pokedex).forEach((key) => {
+    const pokemon = pokedex[key];
     Object.keys(pokemon.type).forEach((x) => {
       if (pokemon.type[x] === param) {
         payload.push(pokemon.name);
@@ -38,10 +38,10 @@ const checkType = (param, p) => {
   return payload;
 };
 
-const checkWeakness = (param, p) => {
+const checkWeakness = (param, pokedex) => {
   const payload = [];
-  Object.keys(p).forEach((key) => {
-    const pokemon = p[key];
+  Object.keys(pokedex).forEach((key) => {
+    const pokemon = pokedex[key];
     Object.keys(pokemon.weaknesses).forEach((x) => {
       if (pokemon.weaknesses[x] === param) {
         payload.push(pokemon.name);
@@ -51,10 +51,10 @@ const checkWeakness = (param, p) => {
   return payload;
 };
 
-const checkEvo = (param, p) => {
+const checkEvo = (param, pokedex) => {
   const payload = [];
-  Object.keys(p).forEach((key) => {
-    const pokemon = p[key];
+  Object.keys(pokedex).forEach((key) => {
+    const pokemon = pokedex[key];
     if (pokemon.name === param) {
       Object.keys(pokemon.prev_evolution).forEach((x) => {
         payload.push(pokemon.prev_evolution[x].name);
@@ -64,13 +64,31 @@ const checkEvo = (param, p) => {
   return payload;
 };
 
-const generateHtml = (title, content) => {
+const searchPokemon = (param, amt, cmp, pokedex) => {
+  if (amt !== undefined && cmp === 'more') {
+    const payload = [];
+    Object.keys(pokedex).forEach((key) => {
+      if (pokedex[key][param] > amt) payload.push(pokedex[key].name);
+    });
+    return payload;
+  }
+  if (amt !== undefined && cmp === 'less') {
+    const payload = [];
+    Object.keys(pokedex).forEach((key) => {
+      if (pokedex[key][param] < amt) payload.push(pokedex[key].name);
+    });
+    return payload;
+  }
+  return 'This should not happen.';
+};
+
+const generateHtml = (title, pokedex) => {
   let body = '<html><body style="font-family:monospace;">';
   body += `<h1>${title}</h1>`;
-  if (typeof content === 'object') {
+  if (typeof pokedex === 'object') {
     body += '<ul style="list-style-type:none; padding:0; margin:0;">';
 
-    Object.keys(content).forEach((key) => {
+    Object.keys(pokedex).forEach((key) => {
       // CHANGE INDEX TO START FROM 1
       let index = key;
       if (index.match(/^[0-9]+$/) !== null) {
@@ -78,15 +96,15 @@ const generateHtml = (title, content) => {
       }
       // CHECK FOR IMG ELEMENT
       if (index === 'img') {
-        body += `<img src="${content[key]}">`;
+        body += `<img src="${pokedex[key]}">`;
       } else if (key === 'next_evolution' || key === 'prev_evolution') { // CHECK FOR NEXT/PREV EVO OBJECT
-        content[key].forEach((item) => {
+        pokedex[key].forEach((item) => {
           body += `<li><strong>${capitalize(index.toString())}</strong>: ${item.name}</li>`;
         });
-      } else body += `<li><strong>${capitalize(index.toString())}</strong>: ${content[key]}</li>`;
+      } else body += `<li><strong>${capitalize(index.toString())}</strong>: ${pokedex[key]}</li>`;
     });
     body += '</ul>';
-  } else body += content;
+  } else body += pokedex;
   body += '</body></html';
   return body;
 };
@@ -94,9 +112,9 @@ const generateHtml = (title, content) => {
 const generatePokemonList = (title, content) => {
   let body = '<html><body style="font-family:monospace;">';
   body += `<h1>${title}</h1><ol>`;
-  for (let i = 0; i < content.length; i++) {
-    body += `<li><a href="/${content[i].name}">${content[i].name}</li>`;
-  }
+  Object.keys(content).forEach((key) => {
+    body += `<li><a href="/${content[key]}">${content[key]}</li>`;
+  });
   body += '</ol></body></html>';
   return body;
 };
@@ -105,36 +123,47 @@ const generatePokemonList = (title, content) => {
 jsonfile.readFile('pokedex.json', (err, obj) => {
   if (err) console.error(err);
   else {
-    const p = obj.pokemon;
+    const pokedex = obj.pokemon;
 
     app.get('/', (req, res) => {
-      res.send(generatePokemonList('Pokedex', p));
+      const list = [];
+      Object.keys(pokedex).forEach((key) => {
+        list.push(pokedex[key].name);
+      });
+      res.send(generatePokemonList('Pokedex', list));
     });
 
     app.get('/:name', (req, res) => {
       const param = capitalize(req.params.name);
-      const result = checkName(param, p);
+      const result = checkName(param, pokedex);
       if (result[0] === true) res.send(generateHtml(param, result[1]));
       res.status(404).send(generateHtml('404', `Could not find information about ${req.params.name}- Is that a new pokemon? Gotta catch em' all!`));
     });
 
+    app.get('/search/:parameter', (req, res) => {
+      const param = req.params.parameter;
+      const amt = req.query.amount;
+      const cmp = req.query.compare;
+      res.send(generatePokemonList(`${capitalize(param).replace('_', ' ')} ${cmp} than ${amt}`, searchPokemon(param, amt, cmp, pokedex)));
+    });
+
     app.get('/type/:type', (req, res) => {
       const param = capitalize(req.params.type);
-      const result = checkType(param, p);
+      const result = checkType(param, pokedex);
       const title = `Pokemon of type: ${param}`;
       res.send(generateHtml(title, result));
     });
 
     app.get('/weaknesses/:weakness', (req, res) => {
       const param = capitalize(req.params.weakness);
-      const result = checkWeakness(param, p);
+      const result = checkWeakness(param, pokedex);
       const title = `Pokemon weak against: ${param}`;
       res.send(generateHtml(title, result));
     });
 
     app.get('/nextevolution/:name', (req, res) => {
       const param = capitalize(req.params.name);
-      const result = checkEvo(param, p);
+      const result = checkEvo(param, pokedex);
       const title = `Pokemon evolving to ${param}`;
       res.send(generateHtml(title, result));
     });
