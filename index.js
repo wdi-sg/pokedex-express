c07420bddfefe8d5954f7191b30099881a5673ea
shroomcrumpet <kencheng3@gmail.com>
2018-09-26 02:07:00 +0800

    All further sections completed

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ var handleRequestRoot = (request, response) => {
 
         html += `<html><body style="margin:5vw;"><h1>Welcome to the online Pokedex!</h1><h3 style="color:red;">Pokemon:</h3>`;
 
-        for (i = 1; i <= obj.pokemon.length; i++) {
+        for (i in obj.pokemon) {
 
-            html += `<a href = "/${obj.pokemon[i-1].name}">${i}. ${obj.pokemon[i-1].name}</a><br>`;
-        }
+            html += `<a href = "/${obj.pokemon[i].name}">${parseInt(i)+1}. ${obj.pokemon[i].name}</a><br>`;
+        };
 
         html += `</body></html>`;
 
@@ -207,12 +207,48 @@ var handleRequestEvo = (request, response) => {
 };
 
 
+var handleRequestSearch = (request, response) => {
+
+    console.log("Handling response now...");
+    console.log("Request path: " + request.path);
+    console.log(`Requesting Pokemon with ${request.params.searchattribute} ${request.query.compare} than ${request.query.amount}...`);
+    console.log(request.query);
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        if (err) {console.log(err)};
+
+        const pokes = obj.pokemon;
+        const attr = request.params.searchattribute;
+
+        var operators = {
+            'more': function(a, b) { return a > b },
+            'less': function(a, b) { return a < b },
+        };
+
+        let html = "";
+        html += `<html><body style="margin:5vw;"><h1>Pokedex Query</h1><h3 style="color:red;">Pokemon with ${request.params.searchattribute} ${request.query.compare} than ${request.query.amount}:</h3>`;
+
+        for (i in pokes) {
+
+            if (operators[request.query.compare] (parseFloat(pokes[i][request.params.searchattribute]), (request.query.amount))) {
+
+                html += `<a href = "/${pokes[i].name}">${parseInt(i)+1}. ${pokes[i].name}</a><br>`;
+            };
+        };
+
+        html += `</body></html>`;
+        return response.send (html);
+    });
+};
+
+
+app.get('/search/:searchattribute', handleRequestSearch);
 app.get('/type/:type', handleRequestType );
 app.get('/weaknesses/:weaknesses', handleRequestWeak );
 app.get('/prevevolution/:prevevos', handleRequestEvo );
 app.get('/:name', handleRequestName );
 app.get('/', handleRequestRoot );
-// app.get('/', (request, response) => {response.send("Welcome to the online Pokdex!");} );
 
 app.listen(PORT_NUMBER, () => console.log('~~~ Tuning in to the waves of port 3001 ~~~'));
 
@@ -235,12 +271,11 @@ app.listen(PORT_NUMBER, () => console.log('~~~ Tuning in to the waves of port 30
 
 // Further
 // Create a route for each of these: spawn_chance, avg_spawns that will show each pokemon
-// that is more or less than the given number. Example: /search/spawn_chance?amount=1&compare=less will send back a formatted HTML page with a list of every pokemon with a spawn change less than 1. Example 2: /search/avg_spawn?amount=0.8&compare=more
+// that is more or less than the given number. Example: /search/spawn_chance?amount=1&compare=less
+// will send back a formatted HTML page with a list of every pokemon with a spawn chance less than 1.
+// Example 2: /search/avg_spawns?amount=0.8&compare=more
 
 // Further
 // Create the same routes for: height, weight and spawn_time.
-
-
-
 
 
