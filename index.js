7ae9915fb1ad5f504d878397e9ca2f8d5e858d39
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-25 10:50:34 +0800

    incomplete work

--- index.js
+++ index.js
@@ -18,36 +18,48 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
+app.get('/:name', (request, response) => {
   // send response with some data (a string)
-  var requestPath = request.path;
-  var nameSearch = requestPath.replace('/','');
+  // var requestPath = request.path;
+  // var nameSearch = requestPath.replace('/','');
 
   jsonfile.readFile(file, (err,obj)=>{
-      const pokemon = obj.pokemon;
+
+      const pokemonObj = obj.pokemon;
       let resultName = [];
       let resultWeight = [];
 
-
-      var pokemonWeight = () => {
-        for (let i = 0; i < pokemon.length; i++){
-            let pokemonName = pokemon[i].name;
-            let pokemonWeight = pokemon[i].weight;
-            resultName.push('<li>' + pokemonName + ': ' + pokemonWeight + '</li>');
+    for (let i = 0; i < pokemonObj.length; i++){
+        let pokemonName = pokemonObj[i];
+        if (pokemonName.name === request.params.name){
+            response.send(pokemonName.weight);
         }
-        return resultName;
-      }
-
-      var makeHtmlPage = (weight) => {
-        let weightPage = '<ul>' + 'Weight of pokemon is: ' + weight + '</ul>';
-        let htmlPage = '<html><head></head><body>' + weightPage + '</body></html>';
-        return htmlPage;
-      }
-
-      response.send(makeHtmlPage(pokemonWeight()));
-  })
-
+       // resultName.push('<li>' + pokemonName + ': ' + pokemonWeight + '</li>');
+    }
+  }
+  )
 });
+
+
+  //     var pokemonWeightFunc = () => {
+  //       for (let i = 0; i < pokemon.length; i++){
+  //           let pokemonWeight = pokemon[0].weight;
+  //           resultWeight.push(pokemonWeight);
+
+  //       }
+  //       return resultWeight;
+  //     }
+
+  //     var makeHtmlPage = (name, weight) => {
+  //       let weightPage = '<ul>' +  name + ": " + weight + '</ul>';
+  //       let htmlPage = '<html><head></head><body><h1>' + 'Weight of pokemon is: ' + '</h1>' + weightPage + '</body></html>';
+  //       return htmlPage;
+  //     }
+
+  //     response.send(makeHtmlPage(pokemonNameFunc(), pokemonWeightFunc()));
+  // })
+
+
 
 /**
  * ===================================
@@ -55,3 +67,7 @@ app.get('*', (request, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
+
