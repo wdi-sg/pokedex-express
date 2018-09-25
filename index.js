9a76c78f6227f6f4fa376d1dd1ffd401020aa497
xnithunx <nithun.nanda@gmail.com>
2018-09-25 10:51:51 +0800

    did first part, working on other parts

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const file = "./pokedex.json";
+
+const PORT_NUMBER = 3002;
 
 /**
  * ===================================
@@ -8,8 +12,42 @@ const express = require('express');
  * ===================================
  */
 
-// Init express app
-const app = express();
+// // Init express app
+// const app = express();
+
+
+// Function to read and go through the json file and return weight
+
+
+var searchPokemon = (object, pokeName) => {
+
+     for (i in object.pokemon) {
+
+         if (object.pokemon[i].name.toLowerCase() === pokeName.toLowerCase()) {
+
+             return object.pokemon[i].weight;
+         };
+     };
+ };
+
+// Function for handle request
+
+var handleRequest = (request, response) => {
+
+     console.log("Loading. . .");
+     console.log("Request path: " + request.path);
+
+     let result;
+
+     jsonfile.readFile(file, (err, obj) => {
+
+         if (err) {console.log(err)};
+
+         result = searchByName(obj);
+         response.send(result);
+
+         });
+ };
 
 /**
  * ===================================
@@ -17,14 +55,21 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+app.get('*', handleRequest);
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3004, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
