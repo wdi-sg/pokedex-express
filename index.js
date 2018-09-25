91386a358ef8acb08bea4f7e7fab45c63613b520
weiwenl <weiwenl.justin@gmail.com>
2018-09-25 09:34:26 +0800

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
+const jsonfile = require('jsonfile');
 
 // Init express app
 const app = express();
+const file = 'pokedex.json';
+// var searchByName = (object, )
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
 
-app.get('*', (request, response) => {
+
+jsonfile.readFile(file, function(err, obj){
+  if(err){
+    console.log('error is: ', err);
+  }
+  for(let i = 0; i < obj.pokemon.length; i++){
+    let pokemon = obj.pokemon[i];
+    console.log(pokemon);
+  }
+
+  var searchArray = (object, pokeName) => {
+    if(object.name.toLowerCase() === pokeName){
+      ob
+    }
+  };
+});
+
+app.get('/:name', (request, response) => {
   // send response with some data (a string)
+  console.log('request path: ', request.path);
+  console.log('request params: ', request.params);
   response.send(request.path);
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+// let name = 0;
+// let i = 0;
+// while(request.path[i] !== undefined){
+//   name += request.path[i];
+//   i++;
+//   console.log(name);
+// }
