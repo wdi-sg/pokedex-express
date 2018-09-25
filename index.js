f00095fb2d9ba1b14400482cfbaae80a666035a5
Rstinkho <t.turchinskiy@bk.ru>
2018-09-25 19:26:45 +0800

    access to object with node

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const express = require("express");
+const jsonfile = require("jsonfile");
+const pokeFile = "./pokedex.json";
+
+// Get the object with pokemon
+
+jsonfile.readFile(pokeFile, (err, obj) => {
+  if (err) {
+    console.error(err);
+  } else {
+    pokedex = obj;
+ //    console.log(pokedex)
+  }
+});
 
 /**
  * ===================================
@@ -9,7 +21,8 @@ const express = require('express');
  */
 
 // Init express app
-const app = express();
+//const app = express();
+
 
 /**
  * ===================================
@@ -17,22 +30,26 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  //send response
-    let exist = false;
-app.get('*', (request, response) => {
-send response with some data (a string)
-let exist = false;
-for(var i=0;i<pokemon.length;i++){
-if("/"+pokemon[i].name.toLowerCase() == request.path){
-response.status(200).send("This is "+pokemon[i].name+ ", he is "+pokemon[i].weight+" in weight!");
-exist = true;
-}
-}
+//  app.get('*', (request, response) => {
+//    response.send("HI");
+//
+//      let exist = false;
+//
+//  app.get('*', (request, response) => {
+//  send response with some data (a string)
+//
+//  let exist = false;
+//
+//  for(var i=0;i<pokemon.length;i++){
+//  if("/"+pokemon[i].name.toLowerCase() == request.path){
+//  response.status(200).send("This is "+pokemon[i].name+ ", he is "+pokemon[i].weight+" in weight!");
+//  exist = true;
+//  }
+//  }
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+// app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
