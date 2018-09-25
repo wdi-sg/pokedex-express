b13114f9a71ad65189c6cbd0de1ad38665cb9349
audthenerd <aud.kow88@gmail.com>
2018-09-26 00:12:27 +0800

    Completed until root pages

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let pokeObj;
+let result;
+var typeArr =[];
+
 const express = require('express');
 
 const jsonfile = require('jsonfile');
@@ -10,33 +14,6 @@ const jsonfile = require('jsonfile');
 
 const file = 'pokedex.json';
 
-var pokeObj;
-var result;
-
-jsonfile.readFile (file, function(err, obj) {
-
-    console.log(file);
-
-    pokeObj = obj.pokemon;
-
-    // var resultFile = 'result.json';
-
-//     for (i in pokeObj) {
-//     console.log(pokeObj[i].name.toLowerCase());
-//     }
-
-
-//     if (process.argv[2] === pokeObj[i].name.toLowerCase())
-//     {
-//         result = (`${pokeObj[i].name} is ${pokeObj[i].weight}!`);
-//     }
-
-
-// jsonfile.writeFile(resultFile, result, function (err) {
-
-
-// });
-});
 
 // Init express app
 const app = express();
@@ -46,45 +23,243 @@ const app = express();
  * Routes
  * ===================================
  */
+app.get('/', (request, response) => {
 
-app.get('*', (request, response) => {
+    jsonfile.readFile (file, function(err, obj) {
+
+        console.log(file);
+
+        pokeObj = obj.pokemon;
+//--------------------------------------------------------------------
+            //types of pokemon
+           var typesOfPoke = [];
+
+                for(i in pokeObj) {
+                    for(j in pokeObj[i].type) {
+                        typesOfPoke.push(pokeObj[i].type[j]);
+                    }
+                };
 
 
 
+                typesOfPoke.forEach(function(item) {
+                     if(typeArr.indexOf(item) < 0) {
+                         typeArr.push(item);
+                     }
+                });
 
-    var req = request.path.substr(1).toLowerCase();
+                console.log(typeArr);
+//---------------------------------------------------------------------
 
-    for (i in pokeObj) {
+    var title = `Welcome to Pokedex!`;
+    var html = '';
 
-        if (req.includes(pokeObj[i].name.toLowerCase())) {
-
-                var pokeWeight = pokeObj[i].weight;
-
-                response.status(200);
-
-                response.send(`${pokeObj[i].name} is ${pokeWeight}!`);
+    html += "<html>";
+    html += "<body>";
+    html += "<h1>"+title+"</h1>";
+    html += '<h2 class="type">Choose by type:</h2>';
 
 
-        } else if (request.path == "/") {
-            response.status(200);
+    for (i in typeArr) {
+        html += '<div><a href="type/'+typeArr[i]+' ">'+typeArr[i].charAt(0).toUpperCase() + typeArr[i].substr(1) +'</a></div>';
+    };
 
-            response.send(`Welcome to Pokedex!`);
+    html += '<br />';
+    html += '<h2 class="weakness">Choose by weakness:</h2>';
 
-        } else {
-            response.status(404);
-
-            response.send(`Could not find information about ${request.path.substr(1)} - Is that a new pokemon? Gotta catch em' all!`);
-        }
+    for (i in typeArr) {
+        html += '<div><a href="weakness/'+typeArr[i]+' ">'+typeArr[i].charAt(0).toUpperCase() + typeArr[i].substr(1) +'</a></div>';
     };
 
 
 
-  // send response with some data (a string)
-  // response.send(request.path);
-  console.log(request.path);
+
+    response.status(200);
+    response.send(html);
+
+    });
 });
 
 
+
+app.get('/:name', (request, response) => {
+
+    jsonfile.readFile (file, function(err, obj) {
+
+        console.log(file);
+
+        pokeObj = obj.pokemon;
+
+        var req = request.params.name.toLowerCase();
+
+        console.log(req);
+
+
+
+            var html = '';
+
+            html += "<html>";
+            html += "<body>";
+
+
+            for (i in pokeObj) {
+
+                if (req === pokeObj[i].name.toLowerCase()) {
+
+                    html += "<h1>"+ req.charAt(0).toUpperCase() + req.substr(1) +"</h1>";
+
+                    var pokeWeight = pokeObj[i].weight;
+
+                    response.status(200);
+
+            html += '<div class=image"><img src=" '+ pokeObj[i].img + ' "></div>';
+
+            html += '<div class="weight-txt">Weight: '+ pokeWeight + '</div>';
+
+            html += '<div class="type">Pokemon Type: ';
+            pokeObj[i].type.forEach(function(elem) {
+                html += `<a href="/type/${elem.toLowerCase()}">${elem}</a>&nbsp`
+            });
+            html += '</div>';
+
+
+            html += '<div class="weaknesses">Weaknesses: ';
+            pokeObj[i].weaknesses.forEach(function(elem) {
+                html += `<a href="/weakness/${elem.toLowerCase()}">${elem}</a>&nbsp`
+                });
+            html += '</div>';
+
+
+            html += '<div class="next-evo"> Next Evolution(s): ';
+            if(pokeObj[i].next_evolution !== undefined) {
+                pokeObj[i].next_evolution.forEach(function(elem) {
+                    html += `<a href="/${elem.name.toLowerCase()}">${elem.name}</a>&nbsp`;
+                });
+                } else {
+                    html += `None`;
+                };
+            html +='</div>';
+
+
+            html += '<div class="prev-evo"> Previous Evolution(s): ';
+            if(pokeObj[i].prev_evolution !== undefined) {
+                pokeObj[i].prev_evolution.forEach(function(elem) {
+                    html += `<a href="/${elem.name.toLowerCase()}">${elem.name}</a>&nbsp`;
+                });
+                } else {
+                    html += `None`;
+                };
+            html +='</div>';
+
+            }
+        };
+
+        html += "</body>";
+        html += "</html>";
+        response.send(html);
+
+
+        //No such pokemon:
+        response.status(404);
+
+        var html = '';
+        html += '<html>'
+        html += '<body>'
+        html += "<h1>Woops!</h1>";
+        html += `<p class="error-msg">Could not find information about ${request.path.substr(1)} - Is that a new pokemon? Gotta catch em' all! </p>`;
+        html += "</body>";
+        html += "</html>";
+        response.send(html);
+        return response.redirect('/');
+
+
+      // send response with some data (a string)
+      // response.send(request.path);
+      console.log(request.path);
+
+  });
+
+});
+
+
+
+app.get('/type/:typename', (request, response) => {
+
+    jsonfile.readFile (file, function(err, obj) {
+
+        console.log(file);
+
+        pokeObj = obj.pokemon;
+
+
+
+            var type = request.params.typename.toLowerCase();
+            var title = type.charAt(0).toUpperCase() + type.substr(1);
+            var html = "";
+
+            html += "<html>";
+            html += "<body>";
+            html += "<h1>"+title+"</h1>";
+            html += "<ul>";
+
+            for (i in pokeObj) {
+                for (j in pokeObj[i].type) {
+                    if (type === pokeObj[i].type[j].toLowerCase()) {
+
+                    response.status(200);
+
+                    html += '<li><img src="'+ pokeObj[i].img +'"></div>';
+                    html += '<a href="/' + pokeObj[i].name.toLowerCase() + ' ">' + pokeObj[i].name + '</a></li>';
+                    html += '<br />';
+                    }
+                }
+            };
+
+            html += "</ul>"
+            html += "</body>";
+            html += "</html>";
+
+            response.send(html);
+
+    });
+});
+
+app.get('/weakness/:weaknessname', (request, response) => {
+
+    jsonfile.readFile (file, function(err, obj) {
+
+        console.log(file);
+
+        pokeObj = obj.pokemon;
+
+            var weaknessName = request.params.weaknessname.toLowerCase();
+            var title = weaknessName.charAt(0).toUpperCase() + weaknessName.substr(1);
+            var html = '';
+
+            html += "<html>";
+            html += "<body>";
+            html += "<h1>"+title+"</h1>";
+
+            for (i in pokeObj) {
+                for (j in pokeObj[i].weaknesses) {
+                    if (weaknessName === pokeObj[i].weaknesses[j].toLowerCase()) {
+
+                    response.status(200);
+
+                    html += '<li><img src="'+ pokeObj[i].img +'"></div>';
+                    html += '<a href="/' + pokeObj[i].name.toLowerCase() + ' ">' + pokeObj[i].name + '</a></li>';
+                    html += '<br />';
+                    }
+                }
+            };
+
+            // html += "</ul>"
+            html += "</body>";
+            html += "</html>";
+
+            response.send(html);
+    });
+});
 
 
 /**
