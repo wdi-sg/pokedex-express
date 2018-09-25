3f9594f7f9a597bc0ce587dd3f3591272aa5c3b3
Pablo101 <yaguelopezpablo@gmail.com>
2018-09-25 22:19:37 +0800

    basic not working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const file = "pokedex.json"
 
 /**
  * ===================================
@@ -11,16 +13,39 @@ const express = require('express');
 // Init express app
 const app = express();
 
+  var doWhenFileIsRead = function(err, obj) {
+      jsonfile.readFile(file, doWhenFileIsRead);
+
+
+        if (err) {
+            console.log(err)
+        }  else {
+            pokedex = obj;
+        }
+  };
+
+
+app.get('*', (request, response) => {
+    console.log('hello');
+  // send response with some data (a string)
+  response.send(response.path)
+
+    for (var i = 0; i < obj.poke.length; i++) {
+        let pokemon = obj.pokemon[i];
+        let pokemonRequested = request.params.name.toLowerCase();
+            if (pokemon.name.toLowerCase() === namequested) {
+                response.send("your pokemon's weight is: " + pokemon.weight);
+
+    }
+ }
+});
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+
 
 /**
  * ===================================
