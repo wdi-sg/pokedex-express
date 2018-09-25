cd74efa6ca034d574d3ccc20599ba5172e2c35ea
wandsen <dsenwan@gmail.com>
2018-09-25 17:55:36 +0800

    mostly completed

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const file = "pokedex.json"
 var respondToRequest = function (request, response){
   // send response with some data (a string)
     var userInputPath = request.path.split("/")[1]
-    console.log(userInputPath)
 
     //read pokedex.json file
     jsonfile.readFile (file, function(err, obj){
@@ -19,29 +18,106 @@ var respondToRequest = function (request, response){
 
         var output;
 
+        if (userInputPath === ""){
+            response.send("Welcome to the online pokedex")
+        }
 
-        //loop through each member of the pokemon array
-        arrayListOfPokemon.forEach(function(element){
+        else{
 
-            //comparing what the user enter in url and the pokedex.json to get correct pokemon
+            //loop through each member of the pokemon array
+            arrayListOfPokemon.forEach(function(element){
 
-            if( userInputPath === element.name.toLowerCase() ){
-                output = element.weight
-                response.send(output)
+                //comparing what the user enter in url and the pokedex.json to get correct pokemon
+
+                if( userInputPath === element.name.toLowerCase() ){
+                    output = element.weight
+                    response.send(output)
+                };
+
+            });
+
+            if(output === undefined){
+            response.send("Could not information about " + userInputPath + "\nIs that a new pokemon? Gotta catch em' all!")
             };
-
-        });
-
-        if(output === undefined){
-        response.send("Could not information about " + userInputPath)
         };
     })
 };
 
 
+//type could be type, weakness, nextevolution
+//sometype must be in accordance to the type
 
 
-app.get('*', respondToRequest)
+var respondToType = function(request, response){
+
+
+
+var userInputPath = request.path.split("/")
+console.log(userInputPath[1])
+console.log(userInputPath[2])
+
+jsonfile.readFile( file, function(err, obj){
+
+
+
+    var arrayListOfPokemon = obj.pokemon
+
+    var outputList = []
+
+    if (userInputPath[1] === "type"){
+
+        //loop through each pokemon
+        arrayListOfPokemon.forEach(function(element){
+
+            //loop through each type
+            element.type.forEach(function(someType){
+                if(userInputPath[2] === someType.toLowerCase()){
+                    console.log("userInput2")
+                    outputList.push(element.name)
+                }
+            })
+        })
+    }
+
+    else if (userInputPath[1] === "weaknesses"){
+
+        //looping through each pokemon
+        arrayListOfPokemon.forEach(function(element){
+
+            //loop through array of weakness(there is more that one weakness)
+            element.weaknesses.forEach(function(someWeakness){
+                if(userInputPath[2] === someWeakness.toLowerCase()){
+                    outputList.push(element.name)
+                }
+            })
+        })
+    }
+
+    else if (userInputPath[1] === "nextevolution"){
+
+        //looping each pokemon
+        arrayListOfPokemon.forEach(function(element){
+            if(userInputPath[2] === element.name.toLowerCase()){
+
+                //loop through each stage of evolution
+                element.next_evolution.forEach(function(evolution){
+                   outputList.push(evolution.name)
+                })
+
+            }
+
+        })
+    }
+
+    response.send(outputList)
+})
+
+}
+
+
+app.get('*', respondToType)
+
+//app.get('*', respondToRequest)
 
 
 /**
