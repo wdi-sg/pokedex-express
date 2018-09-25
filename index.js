098bce29c2c944887241a9a17707b49172d828da
saufm68 <saufm68@gmail.com>
2018-09-25 17:44:17 +0800

    part 2

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const jsonfile = require('jsonfile');
 
 const pokedex = 'pokedex.json';
 
-let listOfPokemon;
-
-
-jsonfile.readFile(pokedex, (err,obj) => {
-
-    listOfPokemon = obj;
-
-});
-
-
 /**
  * ===================================
  * Configurations and set up
@@ -29,94 +19,125 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  var check = false;
+app.get('/type/:type', (request, response) => {
 
-  let pathRoute = request.path.split("/");
-  // console.log(pathRoute);
+    let userRequest = request.params.type[0].toUpperCase() + request.params.type.substring(1).toLowerCase();
 
-  if (pathRoute[1] === "") {
+    jsonfile.readFile(pokedex, (err,obj) => {
 
-    response.send("Welcome to the online Pokdex!");
+        let list = ""
 
-  } else if (pathRoute[1] === "type") {
+        for (let i = 0; i < obj['pokemon'].length; i++) {
 
-    if (pathRoute[2] === undefined) {
+            if (obj['pokemon'][i]['type'].includes(userRequest)) {
 
-        response.send("Enter a type to search");
+                list += `<li>${obj['pokemon'][i]['name']}</li>`;
 
-    } else {
-
-        var path = pathRoute[2][0].toUpperCase() + pathRoute[2].toLowerCase().substring(1);
-        iterateArray("type", path, response);
-
-    }
-
-    check = true;
-
-  } else if (pathRoute[1] === "weaknesses"){
-
-    if (pathRoute[2] === undefined) {
-
-        response.send("Enter a type to search");
-
-
-    } else {
-
-    var path = pathRoute[2][0].toUpperCase() + pathRoute[2].toLowerCase().substring(1);
-    iterateArray("weaknesses", path, response);
-
-    }
-
-    check = true;
-
-  } else {
-
-    var path = pathRoute[1][0].toUpperCase() + pathRoute[1].toLowerCase().substring(1);
-    // console.log(path);
-
-    for (let i = 0; i < listOfPokemon['pokemon'].length; i++) {
-
-        if (listOfPokemon['pokemon'][i]['name'] === path) {
-
-            response.send(listOfPokemon['pokemon'][i]);
-            check = true;
+            }
 
         }
 
-    }
+        let html = `<html><head><title>All ${userRequest}</title><body><h1>All ${userRequest} Pokemons</h1><ul>${list}</ul></body></html>`
 
-  }
+        response.send(html)
 
-  if (check === false ) {
-
-    response.status(404);
-    response.send("Could not find information about " + path);
-
-  }
+    });
 
 
 });
 
 
-function iterateArray(key, searchFor, response) {
+app.get('/weakness/:type', (request, response) => {
 
-    const list = [];
 
-    for (let i = 0; i < listOfPokemon['pokemon'].length; i++) {
+    let userRequest = request.params.type[0].toUpperCase() + request.params.type.substring(1).toLowerCase();
 
-        if (listOfPokemon['pokemon'][i][key].includes(searchFor)) {
+    jsonfile.readFile(pokedex, (err,obj) => {
 
-            list.push(listOfPokemon['pokemon'][i]);
+        let list = ""
+
+        for (let i = 0; i < obj['pokemon'].length; i++) {
+
+            if (obj['pokemon'][i]['weaknesses'].includes(userRequest)) {
+
+                list += `<li>${obj['pokemon'][i]['name']}</li>`;
+
+            }
 
         }
 
-    }
+        let html = `<html><head><title>Weakness To ${userRequest}</title><body><h1>All Pokemons With Weakness To ${userRequest} </h1><ul>${list}</ul></body></html>`
 
-    response.send(list);
+        response.send(html)
 
-}
+    });
+
+
+
+})
+
+app.get('/nextevolution/:pokemon', (request, response) => {
+
+    let userRequest = request.params.pokemon[0].toUpperCase() + request.params.pokemon.substring(1).toLowerCase();
+
+    jsonfile.readFile(pokedex, (err, obj) => {
+
+        let html = `<html><head><title>All Evolutions</title><body>`
+
+        for (let i = 0; i < obj['pokemon'].length; i++) {
+
+            if (obj['pokemon'][i]['name'] === userRequest) {
+
+                html += `<h1>${obj['pokemon'][i]['name']}</h1><img src=${obj['pokemon'][i]["img"]}>`;
+
+                if (obj['pokemon'][i]['prev_evolution'] !== undefined ) {
+
+                    html += `<h2>Previous Evolutions</h2><ul>`;
+
+                    for (let j = 0; j < obj['pokemon'][i]['prev_evolution'].length; j++) {
+
+                        html += `<li>${obj['pokemon'][i]['prev_evolution'][j]['name']}</li>`;
+                    }
+
+                    html += `</ul>`;
+                }
+
+                if (obj['pokemon'][i]['next_evolution'] !== undefined ) {
+
+                    html += `<h2>Next Evolutions</h2><ul>`;
+
+                    for (let j = 0; j < obj['pokemon'][i]['next_evolution'].length; j++) {
+
+                        html += `<li>${obj['pokemon'][i]['next_evolution'][j]['name']}</li>`;
+                    }
+
+                    html += `<ul>`;
+                }
+            }
+        }
+
+        html += `</body></html>`;
+
+        response.send(html);
+
+    })
+
+})
+
+app.get('*', (err, response) => {
+
+
+
+
+
+
+
+
+
+
+
+})
+
 
 /**
  * ===================================
@@ -124,8 +145,6 @@ function iterateArray(key, searchFor, response) {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
-
-
 
 
 
