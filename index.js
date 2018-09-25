78e70581e802614f92625205b06cf03d1a957f3a
weiwenl <weiwenl.justin@gmail.com>
2018-09-25 19:16:20 +0800

    Add #2.1 further

--- index.js
+++ index.js
@@ -12,33 +12,46 @@ var handleRequest = (request, response) => {
 
   console.log("request.path: ", request.path.split('/')[1]);
   let requestedPokemon = request.path.split('/')[1];
-  //call the readFile method that gets: the name of the file and a function.
-  //This function is called when the file-reading operation has finished.
-  //The function will get two parameters.
-  //The first is the information about any error conditions, the second is the
-  //actual content of the file. In this case, an object.
-  jsonfile.readFile('pokedex.json', function(err, obj){
-    //console.log(contents);
-    let i;
-    for(i = 0; i < obj.pokemon.length; i++){
-      console.log("requestedPokemon: ", requestedPokemon);
-      let pokemon = obj.pokemon[i];
-      if(requestedPokemon === pokemon.name.toLowerCase()){
-        response.send(pokemon.weight);
-        break;
+
+  if(request.path === "/"){
+    console.log(request.path);
+    response.status(200);
+    response.send("Welcome to the online Pokedex!");
+  }
+  else{
+    //call the readFile method that gets: the name of the file and a function.
+    //This function is called when the file-reading operation has finished.
+    //The function will get two parameters.
+    //The first is the information about any error conditions, the second is the
+    //actual content of the file. In this case, an object.
+    jsonfile.readFile('pokedex.json', function(err, obj){
+      //console.log(contents);
+      let i;
+      found = false;
+      for(i = 0; i < obj.pokemon.length; i++){
+        let pokemon = obj.pokemon[i];
+        if(requestedPokemon === pokemon.name.toLowerCase()){
+          response.status(200);
+          response.send(pokemon.weight);
+          found = true;
+          break;
+        }
       }
-    }
-    if(i === obj.pokemon.length){
-      response.status(404);
-      response.send("Could not find information about " + requestedPokemon +
-      " - Is that a new pokemon? Gotta catch em' all!");
-    }
-  });
+
+      if(i === obj.pokemon.length && found === false){
+        response.status(404);
+        response.send("Could not find information about " + requestedPokemon +
+        " - Is that a new pokemon? Gotta catch em' all!");
+      }
+    });
+  }
+
 
 }
 
 //.get is saying that when it gets that path it should give the response that is specified in the function.
 app.get('*', handleRequest);
+
 const PORTNUMBER = 3000;
 
 //.listen is going to bind the application to the port on our machine.
