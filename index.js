dbdd1cd5f49537280340f9c6d9e657e2b7881d95
wenosaurus <wen.mcjawn@gmail.com>
2018-09-26 13:14:30 +0800

    Last night's homework

--- index.js
+++ index.js
@@ -5,31 +5,43 @@ const jsonfile = require('jsonfile');
 // Init express app
 const app = express();
 
-// const pokedex = jsonfile.readFile('pokedex.json', (err, obj) => {
-//     console.log(obj.pokemon[0])
-//     return obj.pokemon
-// })
-
-// console.log(pokedex)
-
-// app.get('*', (req, res) => {
-//     res.send('it works!')
-// })
-
 jsonfile.readFile('./pokedex.json', (err, obj) => {
+
 const pokedex = obj.pokemon;
+
   if (err) {
+
     console.log(err)
+
 } else {
+
     app.get('*', (request, response) => {
+
       const param = request.path.substring(1); // Remove '/' from request
-      const payload = [];
+
       Object.keys(pokedex).forEach((key) => {
+
         // DELIVERABLE
+
         if (pokedex[key].name.toLowerCase() === param) {
-          payload.push(pokedex[key].name);
-          payload.push(pokedex[key].weight);
-          response.send(payload);
+
+          var html = `<html>`;
+          html += `<body>`;
+          html += `<h1>${pokedex[key].name}</h1><br>`;
+          html += `<img src='${pokedex[key].img}'><br>`;
+          html += `Type: ${pokedex[key].type}<br>`;
+          html += `Height: ${pokedex[key].height}<br>`;
+          html += `Weight: ${pokedex[key].weight}<br>`;
+          html += `Candy: ${pokedex[key].candy}<br>`;
+          html += `Candy Count: ${pokedex[key].candy_count}<br>`;
+          html += `Egg: ${pokedex[key].egg}<br>`;
+          html += `Spawn Chance: ${pokedex[key].spawn_chance}<br>`;
+          html += `Avg Spawns: ${pokedex[key].avg_spawns}<br>`;
+          html += `Spawn Time: ${pokedex[key].spawn_time}<br>`;
+          html += `Multipliers: ${pokedex[key].multipliers}<br>`;
+          html += `Weaknesses: ${pokedex[key].weaknesses}<br>`;
+
+          response.send(html);
         }
       });
     });
