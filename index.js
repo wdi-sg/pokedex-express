10413b58d10339e75de655687c4d557766b8cdc9
rchuaml <ronchuaml@gmail.com>
2018-12-17 23:18:11 +0800

    mvp and 3 furthers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+const pokedex = "pokedex.json";
 
 /**
  * ===================================
@@ -16,12 +16,50 @@ const app = express();
  * Routes
  * ===================================
  */
+jsonfile.readFile(pokedex, (err, obj) => {
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+    //further 3
+    app.get("/type/:thetype", (request, response) => {
+        const fakeArr = [];
+        // var searchKing = obj.pokemon[i].type[j];
+        var searchType = request.params.thetype;
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            for (let j = 0; j < 2; j++) {
+                if (obj.pokemon[i].type[j] !== undefined) {
+                    var newStr = obj.pokemon[i].type[j];
+                    newStr = newStr.toLowerCase();
+                }
+                if (searchType == newStr) {
+                    fakeArr.push(obj.pokemon[i].name);
+                }
+            }
+        }
+        response.send("The pokemons are " + fakeArr.join(", ") + ".");
+    });
+
+    //further 2
+    app.get('/', (request, response) => {
+        response.send('Welcome to the online Pokedex!')
+    });
+
+    //MVP and further 1
+    app.get("/:name", (request, response) => {
+        let x = 1;
+        var searchPoke = request.params.name.toLowerCase();
+        for (var i = 0; i < obj.pokemon.length; i++) {
+            var searchInput = obj.pokemon[i].name.toLowerCase();
+            if (searchInput === searchPoke) {
+                x = 0;
+                response.send("It's weight is: " + obj.pokemon[i].weight);
+                console.log(obj.pokemon[i].weight);
+            }
+        }
+        if (x == 1) {
+            response.status(404).send("Could not find information about " + request.params.name + " - Is that a new pokemon? Gotta catch em' all!")
+        }
+
+    });
 });
-
 /**
  * ===================================
  * Listen to requests on port 3000
