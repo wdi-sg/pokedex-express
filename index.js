a0dfd0f08bc6cee0a49b38994b1ac7a7a7375d5d
jumpingrock <kennethyeongky@gmail.com>
2018-12-18 00:39:55 +0800

    mvp, problem with asynchronous

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
+const jsonfile = require('jsonfile');
+const file = "pokedex.json";
 const app = express();
+var pokeObject = "", numDex;
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
-
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+var nameChecker = function (keyWord) {
+    jsonfile.readFile(file, (err, obj) => {
+        console.log(err);
+        for (var i in obj.pokemon){
+            console.log(i);
+            console.log(keyWord, obj.pokemon[i])
+            if (keyWord == obj.pokemon[i].name){
+                console.log(keyWord+" weight is "+obj.pokemon[i].weight                );
+                numDex = i;
+                return obj.pokemon[i].weight
+            }
+        }
+        return ("Could not find information about" + keyWord);
+    })
+};
+app.get('/', (request, response) => {
+  response.send('Welcome to Online Pokdex')
 });
+
+app.get("/pokemon/*", function(req, response) {
+    let myParams = req.params[0].split("/")
+    // let result = nameChecker(myParams);
+    // console.log(numDex)
+    response.send(myParams+ "Weight is  " + nameChecker(myParams))
+})
+
+
 
 /**
  * ===================================
