c40c05328a3ce85f23fe8b850c3bc76b45d38add
SiewLeng <siewleng.lim@gmail.com>
2018-12-17 23:37:54 +0800

    created pokedex-express

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
+const express = require('express');
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
+
 
 // Init express app
 const app = express();
@@ -17,10 +17,55 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+app.get("/name/:x", (request, response) => {
+    var name  = request.params.x;
+    var weight;
+    var nameFound = false;
+    jsonfile.readFile(file, function (err, obj) {
+        for(var i = 0; i < obj.pokemon.length; i++ ) {
+            if(obj.pokemon[i].name.toLowerCase() === name.toLowerCase()) {
+                weight = obj.pokemon[i].weight;
+                nameFound = true;
+                break;
+            }
+        }
+
+        if (nameFound) {
+        response.send("The weight of pokemon with name " + obj.pokemon[i].name + " is " + weight);
+        }
+
+        else {
+        response.send("No such pokemon with name " + name);
+        }
+    })
+})
+
+app.get("/type/:x", (request, response) => {
+    var type  = request.params.x;
+    var list = [];
+    var message = "";
+    jsonfile.readFile(file, function (err, obj) {
+        for(var i = 0; i < obj.pokemon.length; i++ ) {
+            for (var j = 0; j < obj.pokemon[i].type.length; j++) {
+                if (obj.pokemon[i].type[j].toLowerCase() === type.toLowerCase()){
+                    list.push(obj.pokemon[i].name);
+                    break;
+                }
+            }
+        }
+        if (list.length > 0) {
+            message = "The list of pokemon with type " + type + ": ";
+            for (var k = 0; k < list.length; k++) {
+                message = message + list[k] + ", ";
+            }
+            response.send(message);
+        }
+
+        else {
+            response.send("There is no pokemon with type " + type + ".");
+        }
+    })
+})
 
 /**
  * ===================================
