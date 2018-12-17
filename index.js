a4488b0f95baa45ebe19175c867d6b464e718603
“the574th” <sufyazman@gmail.com>
2018-12-18 07:56:15 +0800

    Completed deliverables and some furthers part

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
 
 /**
  * ===================================
@@ -17,10 +18,61 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+jsonfile.readFile(file, (err, obj) => {
+    app.get('/:name', (request, response) => {
+        for (var i = 0; i < obj.pokemon.length; i++) {
+            if (obj.pokemon[i].name.toLowerCase() === request.params.name) {
+                response.send(obj.pokemon[i].weight)
+            }
+        }
+        response.status(404).send("Could not find information about " + request.params.name + " - Is that a new pokemon? Gotta catch em' all!");
+    });
+
+    app.get('/type/:sometype',(request, response) => {
+        var typeList = '';
+        for (var i = 0; i < obj.pokemon.length; i++) {
+            for (var r = 0; r < obj.pokemon[i].type.length; r++) {
+                if (obj.pokemon[i].type[r].toLowerCase() === request.params.sometype) {
+                    typeList += (obj.pokemon[i].name + "<br>");
+                    // response.send(obj.pokemon[i].name);
+                }
+            }
+        }
+        response.send("List of " + request.params.sometype + " Pokemon Type: <br>" + typeList)
+    });
+
+    app.get('/weaknesses/:someweakness',(request, response) => {
+        var typeList = '';
+        for (var i = 0; i < obj.pokemon.length; i++) {
+            for (var r = 0; r < obj.pokemon[i].weaknesses.length; r++) {
+                if (obj.pokemon[i].weaknesses[r].toLowerCase() === request.params.someweakness) {
+                    typeList += (obj.pokemon[i].name + "<br>");
+                    // response.send(obj.pokemon[i].name);
+                }
+            }
+        }
+        response.send("List of Pokemons with " + request.params.someweakness + " type weakness: <br>" + typeList)
+    });
+
+
+
+
+
+        app.get('/nextevolution/:somename',(request, response) => {
+            for (var i = 0; i < obj.pokemon.length; i++) {
+                if (obj.pokemon[i].include()) {
+
+                }
+            }
+            response.send("List of Pokemons with " + request.params.someweakness + " type weakness: <br>" + typeList)
+        });
+
 });
+
+app.get('/', (request, response) => {
+  response.send('"Welcome to the online Pokdex!"')
+});
+
 
 /**
  * ===================================
