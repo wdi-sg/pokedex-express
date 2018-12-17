54e4865adf958eff5c584be031014e5388b9685e
root <root@LAPTOP-V1DATB7R.localdomain>
2018-12-18 01:21:24 +0800

    pokedex

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
+const jsonfile = require('jsonfile');
+const pokedex = "pokedex.json";
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
 // Init express app
 const app = express();
 
@@ -17,14 +15,34 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+app.get("/:name", (request, response) => {
+    let isFound = false;
+    // reads pokemon.json
+    jsonfile.readFile(pokedex, (err, obj) => {
+        for (let i = 0; i < obj.pokemon.length; i++){
+            Object.keys(obj.pokemon[i].find((key) => {
+            if(obj.pokemon[i].name === name)
+            {
+                isFound = true;
+                let weight = obj.pokemon[i].weight;
+                response.status(200).send("Return result: " + request.params.name + "Weight: " + obj.pokemon[i].weight);
+            };
 
+
+    });
+            if (isFound ==- false) {
+                response.status(404).send("No information found" + request.params.name);
+            };
+
+        };
+    });
+
+});
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
