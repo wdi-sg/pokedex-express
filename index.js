c280211df30b27c82ef41017c60e6df1eeac996a
Andrea <andrealmj@live.com>
2018-12-17 21:19:00 +0800

    deliverables completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const pokedex = "pokedex.json";
 
 /**
  * ===================================
@@ -17,10 +19,22 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+// app.get('*', (request, response) => {
+//   // send response with some data (a string)
+//   response.send("<html><body><strong>Hi</strong></body></html>");
+// });
+
+app.get('/:name', (request, response) => {
+    jsonfile.readFile(pokedex, (err, obj) => { //iterate thru the location of where all the pokemon data is stored - the pokedex.json file
+        for (let i=0; i < obj.pokemon.length; i++) { //iterate thru the pokemon object
+            Object.keys(obj.pokemon[i]).find( (key) => { //Object.keys() returns an array of the object's PROPERTY NAMES, find() returns the VALUE of the FIRST element in the array which satisfies the testing function
+                if ( request.params.name == obj.pokemon[i][key] ) {
+                    response.send("Weight of " + request.params.name + ": " + obj.pokemon[i].weight);
+                }
+            })
+        }
+    })
+})
 
 /**
  * ===================================
