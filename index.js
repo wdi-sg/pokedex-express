ffcc99e45d2dbd2efa58bbc0607257257a2d98b6
hongchiong <hongchiong@gmail.com>
2018-12-17 17:54:37 +0800

    pokedex with bad request 404

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+const pokedex = "pokedex.json"
 
 /**
  * ===================================
@@ -11,15 +12,32 @@ const express = require('express');
 // Init express app
 const app = express();
 
+    // Object.keys(object).find(key => object[key] === value);
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('*', (request, response) => {
+app.get('/:pokename', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+    let badReq = true;
+    jsonfile.readFile(pokedex, (err, obj) => {
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            Object.keys(obj.pokemon[i]).find(key =>
+            {
+                if (obj.pokemon[i][key] == request.params.pokename)
+                {
+                    response.status(200).send(obj.pokemon[i].weight);
+                    badReq = false;
+                }
+            });
+        }
+        if (badReq) {
+            response.status(404).send(`Could not find information about ${request.params.pokename} - Is that a new pokemon? Gotta catch em' all!`);
+        }
+        console.log()
+    });
 });
 
 /**
