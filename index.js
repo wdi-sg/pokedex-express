b144bc95e1b2a56361b7ec9f02274f16b1bf912c
sean chan <sean@seans-MacBook-Air.local>
2018-12-17 19:51:47 +0800

    Further 1-3

--- index.js
+++ index.js
@@ -17,18 +17,56 @@ const app = express();
  * ===================================
  */
 
+app.get("/", (request, response) => {
+    response.send("Welcome to the online Pokedex!");
+});
+
 app.get("/:name", (request, response) => {
+    let x = true;
     var searchPoke = request.params.name.toLowerCase();
       jsonfile.readFile(pokedex, (err, obj) => {
         for(var i = 0; i < obj.pokemon.length; i++){
             var searchInput = obj.pokemon[i].name.toLowerCase();
                 if(searchInput === searchPoke){
-                    response.send("Its weight is: " + obj.pokemon[i].weight);
-                    console.log(obj.pokemon[i].weight);
+                    x = false;
+                    var infoPoke = JSON.stringify(obj.pokemon[i]);
+                    response.send("Its full information is: " + infoPoke);
                 }
         }
+        if(x === true){
+        response.status(404).send("Could not find information about " + searchPoke + " Is that a new pokemon? Gotta catch em' all!");
+      }
     });
 });
+
+// var invalidPoke = function(poke){
+//     jsonfile.readFile(pokedex, (err, obj) => {
+//     for(var i = 0; i < obj.pokemon.length; i++){
+//         var searchInput1 = obj.pokemon[i].name.toLowerCase();
+//             if(searchInput1 === poke){
+//                 console.log("hi");
+//             // console.log("Could not find information about " + poke + " Is that a new pokemon? Gotta catch em' all!");
+//             }
+//             else{
+//                 console.log(poke);
+//             }
+//         }
+//     });
+// }
+
+// app.get("/:name", (request, response) => {
+//     var searchPoke = request.params.name.toLowerCase();
+//       jsonfile.readFile(pokedex, (err, obj) => {
+//         for(var i = 0; i < obj.pokemon.length; i++){
+//             var searchInput = obj.pokemon[i].name.toLowerCase();
+//                 if(searchInput === searchPoke){
+//                     else if(searchPoke !== searchInput){
+                //     console.log("Could not find information about " + searchPoke + " Is that a new pokemon? Gotta catch em' all!");
+                // }
+//                 }
+//         }
+//     });
+// });
 
 // app.get("/search", (request, response) => {
 //     var searchPoke = request.query.q.toLowerCase();
