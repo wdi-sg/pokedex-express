ba5376561afdc6621e5ec116029b3df2d0247431
Your Name <you@example.com>
2018-12-18 21:08:22 +0800

    initial commit. not yet completion

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
 // Init express app
 const app = express();
+
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json'
+
+// const jsonfile = require('jsonfile');
 
 /**
  * ===================================
@@ -17,9 +13,32 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+//http://localhost:3000/search?q=bananas => You are search for bananas
+
+
+
+// app.get('*', (request, response) => {
+//   // send response with some data (a string)
+//   response.send(request.path);
+// });
+
+jsonfile.readFile(file, (err, obj) => {
+
+    let pokedex = obj.pokemon;
+
+     app.get("/search/:name", (request, response) => {
+        response.send("Hello, You are searching for: " + request.query.q);
+        response.send("You have found" + pokedex[0].name);
+
+        let search = request.params.name;
+
+        // response.send("You have found" + pokedex[0].name);
+
+        for (var i = 0; i < pokedex.length; i++) {
+            if (search == pokedex[i].name)
+            response.send("You have found" + pokedex[i].name);
+        }
+    });
 });
 
 /**
