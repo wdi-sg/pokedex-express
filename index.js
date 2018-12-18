4c15c5ac5c4e041833b612b0c31e2832df5e2edc
ishakbhn <ishakbhn@gmail.com>
2018-12-18 14:07:00 +0800

    bulbasaur weight

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
 
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
+const pokedex = 'pokedex.json'
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+jsonfile.readFile(pokedex,(err,obj) => {
+const objPokemon = obj.pokemon;
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+    app.get('/bulbasaur', (request, response) => {
+      //length of pokemon object
+      let length = obj.pokemon.length;
+
+
+      for (let i=0; i < length; i++) {
+        if (objPokemon[i].name === "Bulbasaur") {
+            response.send("Bulbasaur weight is  " + objPokemon[i].weight);
+        }
+
+      }
+
+    });
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
+
+app.listen(3000, () => console.log('~~~ Pokedex Tuning in to the waves of port 3000 ~~~'));
