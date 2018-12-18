0255bcc46523449d13bdea3cd22c0dc90c451cd2
tengchoonhong90 <tengchoonhong_90@outlook.com>
2018-12-18 14:35:56 +0800

    Complete furthers

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.get('/', (request, response) => {
 app.get("/:pokemon", (request, response) => {
   jsonfile.readFile(file, (err, obj) => {
     for (let i = 0; i < obj.pokemon.length; ++i) {
-      if (obj.pokemon[i].name.toLowerCase() != request.params.pokemon.toLowerCase()) {
-        response.status(404);
-        response.send(`Could not find information about ${request.params.pokemon} - Is that a new pokemon? Gotta catch em' all!`);
-      } else if (obj.pokemon[i].name.toLowerCase() == request.params.pokemon.toLowerCase()) {
+      if (obj.pokemon[i].name.toLowerCase() == request.params.pokemon.toLowerCase()) {
         response.send(`<img src="${obj.pokemon[i].img}"><br>
         Name: ${obj.pokemon[i].name}<br>
         No: ${obj.pokemon[i].num}<br>
@@ -22,6 +19,12 @@ app.get("/:pokemon", (request, response) => {
         Height: ${obj.pokemon[i].height}<br>
         Weight: ${obj.pokemon[i].weight}<br>
         Weakness: ${obj.pokemon[i].weaknesses}<br>`);
+      };
+    };
+    for (let i = 0; i < obj.pokemon.length; ++i) {
+      if (obj.pokemon[i].name.toLowerCase() != request.params.pokemon.toLowerCase()) {
+        response.status(404);
+        response.send(`Could not find information about ${request.params.pokemon} - Is that a new pokemon? Gotta catch em' all!`);
       };
     };
   });
@@ -37,8 +40,8 @@ app.get('/type/:type', (request, response) => {
         }
       }
     }
-    response.send(`Pokemon that is ${request.params.type}-type:<br>
-    ${placeholder.join("<br>")}`);
+    let nameholder = request.params.type.charAt(0).toUpperCase() + request.params.type.slice(1);
+    response.send(`Pokemon that is ${nameholder}-type:<br>${placeholder.join("<br>")}`);
   })
 })
 
@@ -47,29 +50,30 @@ app.get('/weaknesses/:weakness', (request, response) => {
     let placeholder = [];
     for (let i = 0; i < obj.pokemon.length; ++i) {
       for (let j = 0; j < obj.pokemon[i].weaknesses.length; ++j) {
-        if (obj.pokemon[i].weaknesses[j].toLowerCase() == request.params.weaknesses.toLowerCase()) {
+        if (obj.pokemon[i].weaknesses[j].toLowerCase() == request.params.weakness.toLowerCase()) {
           placeholder.push(`${obj.pokemon[i].name}`);
         }
       }
     }
-    response.send(`Pokemon weak to ${request.params.weakness}-type:<br>
-    ${placeholder.join("<br>")}`);
+    let nameholder = request.params.weakness.charAt(0).toUpperCase() + request.params.weakness.slice(1);
+    response.send(`Pokemon weak to ${nameholder}-type:<br>${placeholder.join("<br>")}`);
   })
 })
 
-// app.get('/nextevolution/:evolve', (request, response) => {
-//   jsonfile.readFile(file, (err, obj) => {
-//     let placeholder = [];
-//     for (let i = 0; i < obj.pokemon.length; ++i) {
-//       for (let j = 0; j < obj.pokemon[i].next_evolution.length; ++j) {
-//         if (obj.pokemon[i].next_evolution[j].name.toLowerCase() == request.params.evolve.toLowerCase()) {
-//           placeholder.push(`${obj.pokemon[i].next_evolution[j].name}`);
-//         }
-//       }
-//     }
-//     response.send(`Pokemon evolve to ${request.params.evolve}`);
-//   })
-// })
+app.get("/nextevolution/:evolve", (request, response) => {
+  jsonfile.readFile(file, (err, obj) => {
+    let placeholder = [];
+    for (let i = 0; i < obj.pokemon.length; ++i) {
+      if (obj.pokemon[i].name.toLowerCase() == request.params.evolve.toLowerCase()) {
+        for (let j = 0; j< obj.pokemon[i].prev_evolution.length; ++j) {
+          placeholder.push(`${obj.pokemon[i].prev_evolution[j].name}`);
+        }
+      };
+    };
+    let nameholder = request.params.evolve.charAt(0).toUpperCase() + request.params.evolve.slice(1);
+    response.send(`${nameholder} evolves from ${placeholder.join(" and ")}`);
+  });
+});
 
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
