4e3d1946e469cf7f0b39395f293cc74a163d45b6
root <root@Sathvik.localdomain>
2018-12-18 14:22:11 +0800

    adding the files

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
+
+
 
 // Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+
+app.get('/', (request, response) => {
+  response.send(request.path)
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.get("/:name", (request, response) => {
+  
+    //let status = false;
+    jsonfile.readFile(file, (err, obj) => {
+      var pokemon = obj.pokemon;
+      for (var i=0;i<pokemon.length;i++)
+        {
+          if (pokemon[i].name === request.params.name)
+          {
+            //status = true;
+            //console.log(request.params);
+             
+            response.send("Pokemon name is " + request.params.name + "weight is "+ pokemon[i].weight);
+          }
+          else
+          {
+            response.send("Failed to find the pokemon " + request.params.name);
+          }
+            
+        }  
+       });
+});
+
+
+
+app.listen(3000);
+
+
+
+
