bc594244271a46c5709f16e5901785b33ef64adf
jumpingrock <kennethyeongky@gmail.com>
2018-12-18 13:10:29 +0800

    for git pull

--- index.js
+++ index.js
@@ -4,34 +4,25 @@ const file = "pokedex.json";
 const app = express();
 var pokeObject = "", numDex;
 
-var nameChecker = function (keyWord) {
+app.get('/', (request, response) => {
+  response.send('Welcome to Online Pokdex')
+});
+
+app.get("/pokemon/*", function(req, response) {
+    let keyWord = req.params[0].split("/")
     jsonfile.readFile(file, (err, obj) => {
         console.log(err);
         for (var i in obj.pokemon){
             console.log(i);
             console.log(keyWord, obj.pokemon[i])
             if (keyWord == obj.pokemon[i].name){
-                console.log(keyWord+" weight is "+obj.pokemon[i].weight                );
-                numDex = i;
-                return obj.pokemon[i].weight
-            }
-        }
-        return ("Could not find information about" + keyWord);
-    })
-};
-app.get('/', (request, response) => {
-  response.send('Welcome to Online Pokdex')
+                console.log(keyWord+" weight is "+obj.pokemon[i].weight);
+                response.send(keyWord+ "Weight is  " + obj.pokemon[i].weight);
+                break;
+            };
+        };
+    });
 });
-
-app.get("/pokemon/*", function(req, response) {
-    let myParams = req.params[0].split("/")
-    // let result = nameChecker(myParams);
-    // console.log(numDex)
-    response.send(myParams+ "Weight is  " + nameChecker(myParams))
-})
-
-
-
 /**
  * ===================================
  * Listen to requests on port 3000
