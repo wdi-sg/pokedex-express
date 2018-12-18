49a3a3f21b8c1c6683a99b4db722efc4127ec523
Your Name <you@example.com>
2018-12-18 23:14:25 +0800

    complete

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const app = express();
 //   response.send(request.path);
 // });
 
+app.get("/", (request, response) => {
+    response.send("Welcome to the online Pokedex!");
+});
+
 app.get("/:name", (request, response) => {
 
         let search = request.params.name;
@@ -33,6 +37,7 @@ app.get("/:name", (request, response) => {
                         <body>
                             <p>This is <img src = ` +pokedex[i].img+ `></p>
                             <p>He weighs ` +pokedex[i].weight+ `.</p>
+                            <p>He is a `+pokedex[i].type+` type pokemon.</p>
                         </body>
                         </html>`
                     );
@@ -43,6 +48,81 @@ app.get("/:name", (request, response) => {
             }
     });
 });
+
+app.get("/type/:some_type", (request, response) => {
+
+        let search = request.params.some_type;
+
+        let sameType = [];
+
+        jsonfile.readFile(file, (err, obj) => {
+
+            let pokedex = obj.pokemon;
+
+            for (i = 0; i < pokedex.length; i++) {
+                for (j = 0; j < pokedex[i].type.length; j++) {
+                    if (pokedex[i].type[j].toLowerCase() == search) {
+                    sameType.push(pokedex[i].name);
+                    } else if (err) {
+                        response.sendStatus(404);
+                        response.send("Could not find information about " + search + " - Is that a new pokemon? Gotta catch em' all!");
+                    }
+                }
+            }response.send(sameType.join(','));
+    });
+});
+
+app.get("/weakness/:some_weakness", (request, response) => {
+
+        let search = request.params.some_weakness;
+
+        let sameWeakness = [];
+
+        jsonfile.readFile(file, (err, obj) => {
+
+            let pokedex = obj.pokemon;
+
+            for (i = 0; i < pokedex.length; i++) {
+                for (j = 0; j < pokedex[i].weaknesses.length; j++) {
+                    if (pokedex[i].weaknesses[j].toLowerCase() == search) {
+                    sameWeakness.push(pokedex[i].name);
+                    } else if (err) {
+                        response.sendStatus(404);
+                        response.send("Could not find information about " + search + " - Is that a new pokemon? Gotta catch em' all!");
+                    }
+                }
+            }response.send(sameWeakness.join(','));
+    });
+});
+
+app.get("/nextevolution/:preform", (request, response) => {
+
+        let search = request.params.preform;
+
+        let final = [];
+
+        jsonfile.readFile(file, (err, obj) => {
+
+            let pokedex = obj.pokemon;
+
+            for (i = 0; i < pokedex.length; i++) {
+                if (pokedex[i].next_evolution !== undefined) {
+                    for (j = 0; j < pokedex[i].next_evolution.length; j++) {
+                        if (pokedex[i].next_evolution[j].name.toLowerCase() == search) {
+                            final.push(pokedex[i].name);
+                        } else if (err) {
+                        response.sendStatus(404);
+                        response.send("Could not find information about " + search + " - Is that a new pokemon? Gotta catch em' all!");
+                        } else {
+                            response.send("No pokemons evolve from " + search + ".")
+                        }
+                    }
+                }
+            } response.send(final.join(','));
+        })
+    });
+
+
 /**
  * ===================================
  * Listen to requests on port 3000
