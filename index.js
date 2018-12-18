bd59a8f97114d0ee0fa5fdb8267df24c1f79e412
ishakbhn <ishakbhn@gmail.com>
2018-12-18 22:03:08 +0800

    done the first part of further status 404

--- index.js
+++ index.js
@@ -2,23 +2,44 @@ const express = require('express');
 const jsonfile = require('jsonfile');
 
 const app = express();
-const pokedex = 'pokedex.json'
+const pokedex = 'pokedex.json';
+let objOfPokemon = [];
 
 jsonfile.readFile(pokedex,(err,obj) => {
-const objPokemon = obj.pokemon;
-let length = obj.pokemon.length; //length of pokemon object
+objOfPokemon = obj.pokemon;
+
     //if name cannot be found display status code 404
-    app.get('/:name',(request, response) => {
+    app.get("/:name",(request, response) => {
+        let exist = false;
         //loop in the pokemon objects
-        for(let b=0; b < length ; b++) {
-            if (request.params.name === objPokemon[b].name) {
-                response.send(objPokemon[b].name);
-            } else {
-                response.send("Could not find information about <span style ='font-weight:bold;''>" + request.params.name +"</span> Is that a new pokemon? Gotta catch em' all! Status code: 404");
+            console.log("value of exist before the loop: " + exist);
+        for (let b=0; b < objOfPokemon.length ; b++) {
+            //if name in path is equal as curent pokmon name
+            //name exist in the pokemen object, exist is TRUE
+            //response send the weight of the pokemon
+            //change exist back to false then exit
+
+            if (request.params.name === objOfPokemon[b].name) {
+            exist = true;
+            response.send(objOfPokemon[b].name + ' weight is ' + objOfPokemon[b].weight);
+            console.log('It is ' + objOfPokemon[b].name);
+            console.log("value of exist after the loop: " + exist);
+            exist= false;
+            console.log("value of exist after print weight and set exist to false :" + exist);
+            return;
             }
         }
-    });
-
+            //if pokemon name did not exist in pokemon obj
+            // exist is still false, display response send status 404
+        if (exist === false) {
+            response.send("Could not find information about " + request.params.name + " Is that a new pokomen? Gotta catch em' all! STATUS CODE: 404");
+            console.log("no such name");
+            return;
+            }
+     });
 });
 
 app.listen(3000, () => console.log('~~~ Pokedex Tuning in to the waves of port 3000 ~~~'));
+
+
+
