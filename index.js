d4a43a2677d650e5d7305cc55c8c698df11f4890
ishakbhn <ishakbhn@gmail.com>
2018-12-18 15:15:46 +0800

    comple first further status 404

--- index.js
+++ index.js
@@ -6,24 +6,19 @@ const pokedex = 'pokedex.json'
 
 jsonfile.readFile(pokedex,(err,obj) => {
 const objPokemon = obj.pokemon;
-
-    app.get('/bulbasaur', (request, response) => {
-      //length of pokemon object
-      let length = obj.pokemon.length;
-
-
-      for (let i=0; i < length; i++) {
-        if (objPokemon[i].name === "Bulbasaur") {
-            response.send("Bulbasaur weight is  " + objPokemon[i].weight);
+let length = obj.pokemon.length; //length of pokemon object
+    //if name cannot be found display status code 404
+    app.get('/:name',(request, response) => {
+        //loop in the pokemon objects
+        for(let b=0; b < length ; b++) {
+            if (request.params.name === objPokemon[b].name) {
+                response.send(objPokemon[b].name);
+            } else {
+                response.send("Could not find information about <span style ='font-weight:bold;''>" + request.params.name +"</span> Is that a new pokemon? Gotta catch em' all! Status code: 404");
+            }
         }
-
-      }
-
     });
+
 });
-
-
-
-
 
 app.listen(3000, () => console.log('~~~ Pokedex Tuning in to the waves of port 3000 ~~~'));
