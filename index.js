7d0c55fdcdb086a706565b85b11886a192ebb7f3
valenlyn <valenlynchua@gmail.com>
2019-04-01 18:36:42 +0800

    Completed up to first further

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
 
 /**
  * ===================================
@@ -17,10 +18,42 @@ const app = express();
  * ===================================
  */
 
+
+app.get("/:pokemonName", (request, response) => {
+
+    let pokemonName = request.params.pokemonName;
+//   // response.send(request.params.number);
+
+  jsonfile.readFile(file, (err, obj) => {
+
+    let pokemonFound = false;
+
+
+    console.log(obj["pokemon"].length);
+    for (var i = 0; i < obj["pokemon"].length; i++) {
+        if (pokemonName.toLowerCase() === obj["pokemon"][i]["name"].toLowerCase()) {
+            response.send(obj["pokemon"][i]["name"]+ "'s weight is " +obj["pokemon"][i]["weight"] + ". It is a " + obj["pokemon"][i]["type"][0] + " Pokémon.");
+            pokemonFound = true;
+        }
+    }
+
+    if (!pokemonFound) {
+        response.send(pokemonName + " does not exist!");
+    }
+
+
+  // jsonfile.writeFile(file, obj, (err) => {
+  //   console.log(err)
+  // });
+});
+});
+
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+  response.send("Welcome to the online Pokédex!");
 });
+
+
 
 /**
  * ===================================
