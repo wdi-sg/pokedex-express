f24113af64e437ecfb4a21a8f5b5921a72d4cf5c
claucanchin <claucanchin@gmail.com>
2019-04-01 21:16:14 +0800

    completed deliverables

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json'
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+//Deliverables
+app.get("/:pokemonName", (request, response) => {
+
+    let pokemonName = (request.params.pokemonName).toLowerCase();
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        let isFound = false;
+
+        for (i=0; i<obj.pokemon.length; i++) {
+            if (pokemonName === (obj.pokemon[i].name).toLowerCase()) {
+                response.send("Pokemon: " + obj.pokemon[i].name + ", Weight: " + obj.pokemon[i].weight);
+                isFound = true;
+            }
+        };
+
+            if (!isFound) {
+                response.send("Pokemon not found");
+            }
+    });
 });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
+// app.get("/pokemon/:number", (request, response) => {
+
+//   let pokemonNumber = request.params.number;
+
+
+  //jsonfile.readFile()
+  // when you read the file, get the specific pokemon that is being requested
+
+  //response.send(pokemon that was requested);
+// });
