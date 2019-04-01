4e3f457c5ad24bac0347c10b98e3609da74922b3
moses <moseslee90@gmail.com>
2019-04-01 23:39:22 +0800

    jobs done

--- index.js
+++ index.js
@@ -21,26 +21,87 @@ const app = express();
  */
 
 jsonfile.readFile(pokedex, (err, obj) => {
-  app.get("/*", (request, response) => {
+  app.get("/nextevolution/:pokemon", (request, response) => {
+    let pokemonArray = obj["pokemon"];
+    let pokemonNEXT = request.params.pokemon;
+    let pokemonArrayByNEXT = [];
+    for (let index = 0; index < pokemonArray.length; index++) {
+      const pokemon = pokemonArray[index];
+      if (pokemon.next_evolution === undefined) {
+      } else {
+        const pokemonNEXTArrayLength = pokemon.next_evolution.length;
+        for (let i = 0; i < pokemonNEXTArrayLength; i++) {
+          const nextEvolution = pokemon.next_evolution[i].name.toLowerCase();
+          if (pokemonNEXT === nextEvolution) {
+            pokemonArrayByNEXT.push(pokemon.name);
+          }
+        }
+      }
+    }
+    response.send(pokemonArrayByNEXT);
+  });
+  app.get("/weakness/:type", (request, response) => {
+    let pokemonArray = obj["pokemon"];
+    let pokemonType = request.params.type;
+    let pokemonArrayByWeakness = [];
+    for (let index = 0; index < pokemonArray.length; index++) {
+      const pokemon = pokemonArray[index];
+      const pokemonTypeArrayLength = pokemon.type.length;
+      for (let i = 0; i < pokemonTypeArrayLength; i++) {
+        const element = pokemon.weaknesses[i].toLowerCase();
+        if (pokemonType === element) {
+          pokemonArrayByWeakness.push(pokemon.name);
+        }
+      }
+    }
+    response.send(pokemonArrayByWeakness);
+  });
+  app.get("/type/:type", (request, response) => {
+    let pokemonArray = obj["pokemon"];
+    let pokemonType = request.params.type;
+    let pokemonArrayByType = [];
+    for (let index = 0; index < pokemonArray.length; index++) {
+      const pokemon = pokemonArray[index];
+      const pokemonTypeArrayLength = pokemon.type.length;
+      for (let i = 0; i < pokemonTypeArrayLength; i++) {
+        const element = pokemon.type[i].toLowerCase();
+        if (pokemonType === element) {
+          pokemonArrayByType.push(pokemon.name);
+        }
+      }
+    }
+    response.send(pokemonArrayByType);
+  });
+
+  app.get("/:pokemon", (request, response) => {
     // send response with some data (a string)
     let pokemonArray = obj["pokemon"];
-    let pokemonName = request.path;
-    let pokemonNameWithoutSlash = pokemonName.split("/")[1];
+    let pokemonName = request.params.pokemon;
     // console.log(pokemonName);
     // console.log(pokemonArray[0].name);
     let pokemonFound = false;
-    if (request.path === "/") {
-      response.send("Welcome to the online Pokdex!")
-    } else{
+    if (pokemonName === "") {
+      response.send("Welcome to the online Pokdex!");
+    } else {
       for (let index = 0; index < pokemonArray.length; index++) {
         const pokemon = pokemonArray[index];
-        if (pokemonName === "/" + pokemon.name.toLowerCase()) {
+        if (pokemonName === pokemon.name.toLowerCase()) {
           pokemonFound = true;
-          response.send( "This is " + pokemon.name + ", he is " + pokemon.weight + " in weight! He is also " + pokemon.height + " tall.");
+          response.send(
+            "This is " +
+              pokemon.name +
+              ", he is " +
+              pokemon.weight +
+              " in weight! He is also " +
+              pokemon.height +
+              " tall."
+          );
         }
       }
       if (pokemonFound === false) {
-        response.status(404).send("Could not find information about " + pokemonNameWithoutSlash);
+        response
+          .status(404)
+          .send("Could not find information about " + pokemonName);
       }
     }
   });
