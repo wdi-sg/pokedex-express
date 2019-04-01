b10fc45a757df99a91b63284444fc253403a361e
claucanchin <claucanchin@gmail.com>
2019-04-01 23:19:01 +0800

    completed till further 5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const jsonfile = require('jsonfile');
 const file = 'pokedex.json'
 
-//Deliverables
+//showing Pokemon's information
 app.get("/:pokemonName", (request, response) => {
 
     let pokemonName = (request.params.pokemonName).toLowerCase();
@@ -13,31 +13,79 @@ app.get("/:pokemonName", (request, response) => {
 
         let isFound = false;
 
-        for (i=0; i<obj.pokemon.length; i++) {
+        for (let i=0; i<obj.pokemon.length; i++) {
             if (pokemonName === (obj.pokemon[i].name).toLowerCase()) {
-                response.send("Pokemon: " + obj.pokemon[i].name + ", Weight: " + obj.pokemon[i].weight);
+                response.send("This is " + obj.pokemon[i].name + ", he is " + obj.pokemon[i].weight + " in weight! He also has a height of " + obj.pokemon[i].height +".");
                 isFound = true;
             }
         };
-
-            if (!isFound) {
-                response.send("Pokemon not found");
-            }
+        if (!isFound) {
+            response.status(404).send("Could not find information about " + pokemonName + " - Is that a new Pokémon? Gotta catch em' all!");
+        }
     });
 });
 
+//default response when nothing in path
+app.get("", (request, response) => {
+  response.send("Welcome to the online Pokédex!");
+});
+
+//getting list of specific pokemon type
+app.get("/type/:someType", (request, response) => {
+
+    let someType = (request.params.someType).toLowerCase();
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        let pokeArr = [];
+        let isFound = false;
+
+        for (let i=0; i<obj.pokemon.length; i++) {
+
+            let pokeType = (obj.pokemon[i].type).toString().toLowerCase();
+
+            if (pokeType.includes(someType)) {
+                pokeArr.push(obj.pokemon[i].name)
+                isFound = true;
+            }
+        }
+        if (isFound === true) {
+            response.send("Pokémon type " + someType + ": " + pokeArr.join(', ') + ".")
+        } else {
+            response.status(404).send("Could not find Pokémon type " + someType + ".");
+        }
+    });
+});
+
+//getting list of weakness
+app.get("/weakness/:someWeakness", (request, response) => {
+
+    let someWeakness = (request.params.someWeakness).toLowerCase();
+
+    jsonfile.readFile(file, (err, obj) => {
+
+        let pokeArr = [];
+        let isFound = false;
+
+        for (let i=0; i<obj.pokemon.length; i++) {
+
+            let pokeWeakness = (obj.pokemon[i].weaknesses).toString().toLowerCase();
+
+            if (pokeWeakness.includes(someWeakness)) {
+                pokeArr.push(obj.pokemon[i].name)
+                isFound = true;
+            }
+        }
+        if (isFound === true) {
+            response.send("Pokémon weakness " + someWeakness + ": " + pokeArr.join(', ') + ".")
+        } else {
+            response.status(404).send("Could not find Pokémon weakness " + someWeakness + ".");
+        }
+    });
+});
+
+//getting list evolves from (not done)
+//console log below shows example of evolved pokemon
+// console.log(obj.pokemon[0].next_evolution[0].name);
 
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
-
-
-
-// app.get("/pokemon/:number", (request, response) => {
-
-//   let pokemonNumber = request.params.number;
-
-
-  //jsonfile.readFile()
-  // when you read the file, get the specific pokemon that is being requested
-
-  //response.send(pokemon that was requested);
-// });
