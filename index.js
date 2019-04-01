d33f54fd55a31f5e766aef04163304a1a8f21c9f
khyreerusydi <nicoraventbh@gmail.com>
2019-04-01 19:18:34 +0800

    added route for pokemon type

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const file = 'pokedex.json';
 
 const app = express();
 
-// find pokemon's weight by name input
+// find pokemon's details by name input
 app.get("/:pokemon", (request, response) => {
     let pokemonName = request.params.pokemon;
     jsonfile.readFile(file, (err, obj) => {
@@ -17,26 +17,44 @@ app.get("/:pokemon", (request, response) => {
                 let pokemonHeight = obj.pokemon[i].height;
                 let candy = obj.pokemon[i].candy;
                 let id = obj.pokemon[i].id;
-                response.send("This is " + pokemonName + ", it weighs " + pokemonWeight + "! It's height is " + pokemonHeight + " and it loves " + candy + ". " + pokemonName + "'s Pokedex number is " + id + ".");
-                response.send(pokemonName + "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.")
+                response.send("This is " + pokemonName + ", it weighs " + pokemonWeight + "! It's height is " + pokemonHeight + " and it uses " + candy + " to get stronger. " + pokemonName + "'s Pokedex number is " + id + ".");
                 pokemonFound = true;
             }
         }
         if (!pokemonFound) {
             response.send(404, "Could not find information about " + pokemonName + "- is that a new pokemon? Gotta catch em' all!");
+            // res.status(404).send("Could not find information about " + pokemonName + "- is that a new pokemon? Gotta catch em' all!")
         }
     });
-    // when you read the file, get the specific pokemon that is being requested
 
-    // response.send(pokemonName + "'s weight is: " + pokemonWeight);
 });
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+// list the names of all pokemon that have the specified type
+app.get("/type/:type", (request, response) => {
+    let inputType = request.params.type;
+    jsonfile.readFile(file, (err, obj) => {
+        let typeFound = false;
+        let pokemonList = [];
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            let pokemonType = obj.pokemon[i].type.toString().toLowerCase();
+            let typeInput = inputType.toLowerCase();
+            if (pokemonType.includes(typeInput)) {
+                pokemonList.push(obj.pokemon[i].name)
+                typeFound = true;
+            }
+        }
+        console.log(typeFound)
+        if (typeFound === true) {
+            response.send("Here is a list of pokemon that are of the " + inputType + "type: " + pokemonList.join(', ') + ".");
+        } else {
+            response.send(404, "Could not find information about " + inputType + "- is that a new pokemon type? Gotta catch em' all!");
+        }
+    });
 
+});
+
+
+// default response
 app.get('*', (request, response) => {
     // send response with some data (a string)
     // response.send(request.path);
