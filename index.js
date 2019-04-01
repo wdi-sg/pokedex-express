167143cd83bf8499907d6ab6a2738ec366ecc6b9
Yui Terai <yuiterai88@gmail.com>
2019-04-01 21:01:45 +0800

    Done further 1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 // Init express app
 const app = express();
@@ -7,29 +6,26 @@ const app = express();
 const jsonfile = require('jsonfile');
 const file = "pokedex.json"
 
+
+
+
+//Send pokemon's weight based on user input
 app.get("/pokemon/:name", (request, response) => {
     jsonfile.readFile(file, (err, obj) => {
-        for (i = 0; i < obj.pokemon.length; i++) {
-            if (request.params.name === obj.pokemon[i].name) {
-                response.send(`${obj.pokemon[i].name}'s weight is ${obj.pokemon[i].weight}`)
+        let nameExist = false;
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            if (request.params.name.toLowerCase() === obj.pokemon[i].name.toLowerCase()) {
+                response.send(`${obj.pokemon[i].name}'s weight is ${obj.pokemon[i].weight}`);
+                nameExist = true;
             }
+        }
+        if (!nameExist) {
+            response.send(404, "Could not find information about " + request.params.name + " - Is that a new pokemon? Gotta catch em all!");
         }
     });
 });
 
-// app.get('/pokemon/:name', (request, response) => {
-//     jsonfile.readFile(file, (err, obj) => {
-//         for (i = 0; i < obj.pokemon.length; i++) {
-//             if (request.params.name === obj.pokemon[i].name) {
-//             response.send(`${obj.pokemon[i].name}'s weight is ${obj.pokemon[i].weight}`)
-//         }
-//         }
-// });
-// });
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+
+ //Listen to requests on port 3000
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
