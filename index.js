95ac35c98577d5e194277b5a434cf83429514c80
helloimhd <herda58@gmail.com>
2019-04-03 09:14:44 +0800

    first commit

--- index.js
+++ index.js
@@ -5,43 +5,37 @@ const express = require('express');
 const app = express();
 
 //pokemonObj["pokemon"][1]["name"]
-jsonfile.readFile(file, (err, pokemonObj) => {
-/*     for (let i = 0; i < pokemonObj["pokemon"].length; i++) {
-                console.log(pokemonObj["pokemon"][i]["name"].toLowerCase());
-}*/
-   // console.log(pokemonObj["pokemon"][4]["name"])
-    const doWhenRequest = (request, response) => {
-        response.send("Welcome to the online Pokdex!");
-    };
 
-    app.get('/', doWhenRequest );
+const doWhenRequest = (request, response) => {
+    response.send("Welcome to the online Pokdex!");
+};
 
-    app.get("/:pokemonName", (request, response) => {
-        console.log( "request params", request.params);
-            let pokemon = request.params.pokemonName;
-            response.send(pokemon);
+app.get('/', doWhenRequest );
 
+app.get("/:pokemonName", (request, response) => {
+    let pokemon = request.params.pokemonName;
 
-            // length is 151
-            for (let i = 0; i < pokemonObj["pokemon"].length; i++) {
-                //console.log(pokemonObj["pokemon"][i]["name"]);
-                //response.send(pokemon)
+    jsonfile.readFile(file, (err, pokemonObj) => {
+        response.send(pokemonObj["pokemon"].length);
 
-                if (pokemon === pokemonObj["pokemon"][i]["name"].toLowerCase()) {
-                    let weight = pokemonObj["pokemon"][i]["weight"];
+        // length is 151
+        for (let i = 0; i < pokemonObj["pokemon"].length; i++) {
+            //console.log(pokemonObj["pokemon"][i]["name"]);
+            //response.send(pokemon)
 
-                    response.send(`Pokemon: ${pokemon} \nWeight: ${weight}`)
+            if (pokemon === pokemonObj["pokemon"][i]["name"].toLowerCase()) {
+                console.log("yes")
+                let weight = pokemonObj["pokemon"][i]["weight"];
 
-                } else {    // end of if else statement
-                    response.send(404, `Could not find information about ${pokemon} - Is that a new pokemon? Gotta catch em' all!`);
-                        //response.send(404, '<html><body><h1>BANANANSSSSS'+num+'</h1></body></html>');
-                }
-            }  // end of for loop
+                response.send(`Pokemon: ${pokemon} \nWeight: ${weight}`)
 
-     //response.send(pokemon)
-    });
+            } else {    // end of if else statement
+                response.send(404, `Could not find information about ${pokemon} - Is that a new pokemon? Gotta catch em' all!`);
+                    //response.send(404, '<html><body><h1>BANANANSSSSS'+num+'</h1></body></html>');
+            }
+        }  // end of for loop
+    })    // end of read file
+});   // end of get pokemon name
 
 
-    app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
-
-})    // end of read file
+app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
