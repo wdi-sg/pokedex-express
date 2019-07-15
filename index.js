cf58810cbf47d6d8eb58061433b43bc886f7d65a
gitwenlei <ngwenlei@gmail.com>
2019-07-15 17:55:16 +0800

    Added return string response with matching route e.g. localhost:3000/pokemon/bulbasaur

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 /**
  * ===================================
@@ -10,6 +9,9 @@ const express = require('express');
 
 // Init express app
 const app = express();
+const file = 'pokedex.json'
+
+
 
 /**
  * ===================================
@@ -17,10 +19,44 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+// app.get('*', (request, response) => {
+//   // send response with some data (a string)
+//   response.send(request.path);
+// });
+
+
+
+app.get('/pokemon/:name', (request, response) => {
+    var pokemonWeight;
+    let pokemonFound = false;
+
+    jsonfile.readFile(file, function (err, obj) {
+        if (err) {
+        console.error(err)
+        response.send('Pokedex 404!');
+
+        } else {
+            for (let i = 0; i < obj.pokemon.length; i++) {
+                let pokemonName = obj.pokemon[i].name;
+                if (pokemonName.toLowerCase() == request.params.name) {
+                    console.log(request.params.name + ' weighs' + obj.pokemon[i].weight);
+                    pokemonWeight = obj.pokemon[i].weight;
+                    pokemonFound = true;
+
+                    response.send(request.params.name + ' weighs ' + pokemonWeight);
+                }
+            }
+            if (pokemonFound == false ) {
+                response.send('No pokemon found');
+            }
+        }
+    })
 });
+
+app.get('/', (request, response) => {
+    response.send('welcome to pokemon world!');
+});
+
 
 /**
  * ===================================
