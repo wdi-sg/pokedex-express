b4bd377788b98095d7095938f5ebd7d3465330ad
Yi Xin Ang <angyixin@gmail.com>
2019-07-15 20:45:27 +0800

    Able to show pokemon and his weight

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
 
-// const jsonfile = require('jsonfile');
+const app = express();
+const file = 'pokedex.json';
+
+
 
 /**
  * ===================================
@@ -8,23 +12,38 @@ const express = require('express');
  * ===================================
  */
 
-// Init express app
-const app = express();
 
 /**
  * ===================================
  * Routes
  * ===================================
  */
+ app.get('/', (request, response) => {
+   response.send('Welcome to the online Pokedex!');
+ });
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+ app.get("/pokemon/:name", (request, response) => {
+   // response.send("The pokemon is: " + request.params.name);
+
+   jsonfile.readFile(file, function (err, data) {
+      if (err){
+        console.error(err);
+      } else {
+        for( var i = 0 ; i < data.pokemon.length; i++){
+          var pokemonName = (data.pokemon[i].name).toLowerCase();
+          if( request.params.name === pokemonName){
+            response.send(`The pokemon is ${data.pokemon[i].name}. His weight is ${data.pokemon[i].weight}.`);
+          }
+        }
+      }
+    })
+ });
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
+
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
