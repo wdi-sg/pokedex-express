58c14da93ad70e82914111ef8f402d65abc93ea8
emijay <hilmi.johari@yahoo.com.sg>
2019-07-15 21:28:36 +0800

    Added prelim code to read pokedex.json file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const file = 'pokedex.json';
 
 /**
  * ===================================
@@ -11,16 +13,61 @@ const express = require('express');
 // Init express app
 const app = express();
 
+//----- Function for capitalizing first letter of Pokemon being searched -----//
+
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+//----- Function for reading data.json -----//
+
+const readDataFile = (pokemonName) => {
+
+    let capPokemonName = capitalizeFirstLetter(pokemonName);
+
+    console.log(capPokemonName);
+
+    jsonfile.readFile(file, (err,obj) => {
+
+        if(err) {
+            console.log('there is an error')
+
+        } else {
+
+            console.log('running array check')
+
+            for (let i = 0; i < obj['pokemon'].length; i ++) {
+
+                if (obj['pokemon'][i].name === capPokemonName) {
+                    console.log(obj['pokemon'][i].weight)
+                }
+            }
+        }
+    });
+}
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+// app.get('*', (request, response) => {
+//   // send response with some data (a string)
+//   response.send(request.path);
+// });
+
+app.get("/pokedex/:name/", (request, response) => {
+
+    let pokemonSearched = request.params.name;
+
+    let output = readDataFile(pokemonSearched);
+
+    response.send(output);
+
 });
+
 
 /**
  * ===================================
