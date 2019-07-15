973af9fa76d6822e6e1554406aa2db21f822e095
EliseChia <swedesurrender@gmail.com>
2019-07-15 22:36:17 +0800

    finished basic requirements

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
+// const pokedex = require('pokedex.json');
 
-// const jsonfile = require('jsonfile');
 
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+//allows pokedex file to be read
+jsonfile.readFile(file, (err, obj) => {
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+    //can move the below into a callback function?
+    app.get('/pokemon/:name', (request, response) => {
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+        //if path exists, do this
+            console.log('Searching for Pokemon!')
+
+            let pokeToFind = request.params.name;
+            console.log(pokeToFind);
+
+            //find pokeToFind in obj and return its name and weight
+            for (let i = 0; i < obj.pokemon.length; i++) {
+                if (obj.pokemon[i].name === pokeToFind) {
+                    console.log(obj.pokemon[i].name);
+                    console.log(obj.pokemon[i].weight);
+                    // get data from the file
+                    let pokeName = obj.pokemon[i].name;
+                    let pokeWeight = obj.pokemon[i].weight;
+                    response.send(`<html><body><h1>${pokeName}</h1><br><p>Weight: ${pokeWeight}</p></body></html>`);
+                }
+            }
+    });
+})
+
+//setting port as 8080
+app.listen(8080, () => console.log('~~~ Tuning in to the waves of port 8080 ~~~'));
