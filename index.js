d7469e6e617b889a81b8bf4ca4456a33698b458b
kinskin <md.asshikin@gmail.com>
2019-07-15 22:55:48 +0800

    some tweeking problem.

--- index.js
+++ index.js
@@ -40,8 +40,29 @@ var pokemon = (request, response) => {
     });
 };
 
+// var nextEvolution = (request, response) => {
+//     var pokemonName = request.params.name
+//     // var evolution = false;
+//     jsonfile.readFile(file,(err,obj) =>{
+//         // console.log("BOOYEAH");
+//         for (let i = 0; i < obj.pokemon.length; i++){
+//             if(pokemonName === obj.pokemon[i].name.toLowerCase()) {
+//                 // console.log("The name of the pokemon: " + obj.pokemon[i].name);
+//                 for (let j = 0; j < obj.pokemon[i].next_evolution.length; j++){
+//                     console.log(obj.pokemon[i].next_evolution[j].name);
+//                     break;
+//                 }
+//             }
+//             else {
+//                 response.status(404)
+//             };
+//         };
+//         // response.send(display);
+//     });
+// };
+
 var type = (request, response) => {
-    var pokemonType = request.params.colorType;
+    var pokemonType = request.params.pokemonsType;
     console.log(pokemonType);
     //console.log(request.params);
     var pokemon = [];
@@ -62,21 +83,52 @@ var type = (request, response) => {
                 }
             };
         };
-        var pokemonJoin = pokemon.join(", ");
-        response.send(pokemonJoin);
+        var pokemonTypeJoin = pokemon.join(", ");
+        response.send(pokemonTypeJoin);
     });
 };
+
+var weakness = (request,response)=>{
+    var pokemonWeakness = request.params.pokemonWeakness;
+    var pokemon = [];
+    // console.log("HELLO");
+    jsonfile.readFile(file,(err,obj) =>{
+        // console.log("BOOOOO");
+        for (let i = 0; i < obj.pokemon.length; i++){
+            // console.log(obj.pokemon[i].name);
+            for (let j = 0; j < obj.pokemon[i].weaknesses.length; j++){
+                if(pokemonWeakness === obj.pokemon[i].weaknesses[j].toLowerCase()){
+                    // console.log("The pokemon that is weak against " + pokemonWeakness + " is " + obj.pokemon[i].name);
+                    pokemon.push(obj.pokemon[i].name);
+                    // console.log("Is it this :" + pokemon);
+                }
+                // else{
+                //     response.status(404)
+                //     display = "No such weaknesses";
+                //     response.send(display);
+                // };
+            };
+        };
+        console.log("Is it this :",pokemon)
+        var pokemonWeaknessJoin = pokemon.join(", ");
+        response.send(pokemonWeaknessJoin);
+    });
+};
+
+
 
 var showHome = (request, response) =>{
     // console.log("Welcome to the online Pokdex!");
     display = "Welcome to the online Pokedex!";
     response.send(display);
-}
+};
 
 
-app.get('/pokemon/:name', pokemon);
-app.get("/type/:colorType", type)
-app.get('/', showHome);
+app.get("/pokemon/:name", pokemon);
+// app.get("/next_evolution/:name", nextEvolution);
+app.get("/type/:pokemonType", type);
+app.get("/weakness/:pokemonWeakness",weakness);
+app.get("/", showHome);
 
 
   //   console.log("test ", pokemon("Bulbasaur"));
