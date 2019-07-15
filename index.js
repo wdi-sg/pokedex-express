79d7c381d4c200a286baad3e3811a56597c313d1
gitwenlei <ngwenlei@gmail.com>
2019-07-15 18:41:21 +0800

    Added show pokemon by type search

--- index.js
+++ index.js
@@ -34,12 +34,13 @@ app.get('/pokemon/:name', (request, response) => {
         if (err) {
         console.error(err)
         response.send('Pokedex 404!');
+        response.status(404);
 
         } else {
             for (let i = 0; i < obj.pokemon.length; i++) {
                 let pokemonName = obj.pokemon[i].name;
                 if (pokemonName.toLowerCase() == request.params.name) {
-                    console.log(request.params.name + ' weighs' + obj.pokemon[i].weight);
+                    console.log(request.params.name + ' weighs ' + obj.pokemon[i].weight);
                     pokemonWeight = obj.pokemon[i].weight;
                     pokemonFound = true;
 
@@ -47,14 +48,53 @@ app.get('/pokemon/:name', (request, response) => {
                 }
             }
             if (pokemonFound == false ) {
-                response.send('No pokemon found');
+                response.send('Could not find information about ' + request.params.name + '... Is that a new pokemon? Gotta catch em all!');
+                response.status(404);
             }
         }
     })
 });
 
+
+app.get('/type/:requestType', (request, response) => {
+    var pokemonType;
+    let typeArr = [];
+    let pokemonFound = false;
+
+    jsonfile.readFile(file, function (err, obj) {
+        if (err) {
+        console.error(err)
+        response.send('Pokedex 404!');
+        response.status(404);
+
+        } else {
+            for (let i = 0; i < obj.pokemon.length; i++) {
+                let pokemonType = obj.pokemon[i].type[0];
+                if (pokemonType.toLowerCase() == request.params.requestType) {
+                    pokemonFound = true;
+                    // console.log('type: ' + obj.pokemon[i].type[0] + ' name: ' + obj.pokemon[i].name);
+                    typeArr.push('Type: ' + obj.pokemon[i].type[0] + ' ; Name: ' + obj.pokemon[i].name);
+                }
+            }
+            if (pokemonFound == false ) {
+                response.send('Could not find information about ' + request.params.requestType + '... Is that a new type?');
+                response.status(404);
+            } else {
+                response.send(typeArr);
+            }
+        }
+    })
+});
+
+
+
+
+app.get('/pokemon', (request, response) => {
+    response.send('Which pokemon are you looking for?');
+});
+
 app.get('/', (request, response) => {
-    response.send('welcome to pokemon world!');
+    response.send('Welcome to the online Pokdex!');
 });
 
 
