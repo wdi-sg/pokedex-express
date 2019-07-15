316d56d5a76ac35b55bd4875b7d99794fc43962a
EliseChia <swedesurrender@gmail.com>
2019-07-15 23:10:04 +0800

    am i on crack? why is the 404 not working

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 const express = require('express');
 const jsonfile = require('jsonfile');
 const file = 'pokedex.json';
-// const pokedex = require('pokedex.json');
 
 
 const app = express();
 
-//allows pokedex file to be read
-jsonfile.readFile(file, (err, obj) => {
 
-    //can move the below into a callback function?
-    app.get('/pokemon/:name', (request, response) => {
+app.get('/pokemon/:name', (request, response) => {
 
-        //if path exists, do this
-            console.log('Searching for Pokemon!')
+    jsonfile.readFile(file, (err, obj) => {
 
-            let pokeToFind = request.params.name;
-            console.log(pokeToFind);
+        let pokeName = "";
+        let pokeWeight = "";
+        let pokeFound = false;
+        let pokeToFind = request.params.name;
 
-            //find pokeToFind in obj and return its name and weight
-            for (let i = 0; i < obj.pokemon.length; i++) {
-                if (obj.pokemon[i].name === pokeToFind) {
-                    console.log(obj.pokemon[i].name);
-                    console.log(obj.pokemon[i].weight);
-                    // get data from the file
-                    let pokeName = obj.pokemon[i].name;
-                    let pokeWeight = obj.pokemon[i].weight;
-                    response.send(`<html><body><h1>${pokeName}</h1><br><p>Weight: ${pokeWeight}</p></body></html>`);
-                }
+        console.log(pokeToFind);
+
+        //find pokeToFind in obj and return its name and weight
+        for (let i = 0; i < obj.pokemon.length-1; i++) {
+            if (obj.pokemon[i].name === pokeToFind) {
+                pokeName = obj.pokemon[i].name;
+                pokeWeight = obj.pokemon[i].weight;
+                pokemonFound = true;
+
+                console.log(obj.pokemon[i].name);
+                console.log(obj.pokemon[i].weight);
             }
+        }
+
+        if (!pokeFound) {
+            response.status(404).send(`<html><body><h1>Pikaboo:(</h1><br><p>Could not find information on ${pokeToFind} - Is that a new pokemon? Gotta catch em all!</p></body></html>`);
+        } else if (pokeFound) {
+            response.send(`<html><body><h1>${pokeName}</h1><br><p>Weight: ${pokeWeight}</p></body></html>`);
+        }
     });
-})
+});
+
 
 //setting port as 8080
 app.listen(8080, () => console.log('~~~ Tuning in to the waves of port 8080 ~~~'));
