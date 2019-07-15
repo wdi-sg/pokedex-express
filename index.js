2a372d4c0d53128149891c7cb41794a125bae31a
Janet <cashhpvn@yahoo.com>
2019-07-15 21:59:30 +0800

    done basic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
-
+const jsonfile = require('jsonfile');
+const file = "./pokedex.json";
 /**
  * ===================================
  * Configurations and set up
@@ -17,9 +17,44 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+app.get('/pokemon/:name', (request, response) => {
+
+  // get json from specified file
+  jsonfile.readFile(file, (err, obj) => {
+   
+    // extract input data from request
+    var name =  request.params.name ;
+
+    var pokemon
+    var pokemon_weight;
+
+    // find pokemon from the pokedex json file
+    for( let i=0; i<obj.pokemon.length; i++ ){
+
+      let matchedPokemon = obj.pokemon[i];
+
+      if( matchedPokemon.name === name){
+      	pokemon = matchedPokemon;
+        pokemon_weight = matchedPokemon.weight;
+      }
+    }
+
+    if (pokemon===undefined) {
+
+      // send 404 back
+      response.status(404);
+      response.send(`Could not find information about ${name}- Is that a new pokemon? Gotta catch em' all!`);
+    } else {
+
+    var msg = `${name} weighs ${pokemon_weight}`
+      response.send(msg);
+    }
+  });
+});
+
+
+app.get('/', (request, response) => {
+  response.send("Which pokemon are you looking at?");
 });
 
 /**
