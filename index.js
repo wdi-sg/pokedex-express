dece21ca15fc7598a7ebf51e8134078fd2b74b96
aliciawongg <w.alicia@gmail.com>
2019-07-16 17:52:16 +0800

    fix code for names not found

--- index.js
+++ index.js
@@ -12,31 +12,38 @@ const app = express();
 
 //listen to request for pokemon name to get weight
 app.get("/pokemon/:name", (request, response) => {
-    const name = request.params.name
+    console.log('haveeeee');
+    const name = request.params.name.toLowerCase();
     let pokemonWeight = "";
-    let noMatch = false;
+    let pokemonFound = false;
 //read pokedex json
     jsonfile.readFile(file, (err, obj) => {
+        console.log('or nottttttt');
          //read obj
          // response.send(obj);
 //check through array and retrieve pokemon weight
-        for (i = 0; i < obj.pokemon.length; i++) {
-            if (obj.pokemon[i].name === name) {
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            if (obj.pokemon[i].name.toLowerCase() === name) {
                 console.log(name);
+                pokemonFound = true;
                 pokemonWeight = obj.pokemon[i].weight;
                 response.send(name + "'s weight is " + pokemonWeight);
+                break;
             }
-            if (obj.pokemon[i].name !== name) {
-                noMatch === true;
-                console.log('not found');
-                response.send("Could not find information about " + name + " - Is that a new pokemon? Gotta catch em' all!");
-            }
+        };
+//**note - this should be outside for-loop, otherwise there will be multiple responses triggered.//
+        if(pokemonFound === false) {
+            console.log(pokemonFound);
+            response.send("Could not find information about " + name + " - Is that a new pokemon? Gotta catch em' all!");
         }
     });
 });
 
 
-
+//if nothing in the path//
+app.get("/pokemon", (request, response) => {
+    response.send("Welcome to the online Pokedex!");
+});
 /**
  * ===================================
  * Listen to requests on port 3000
