bbf48e3fff03ea35c65018dafef54e2875093a3f
jasminesis <jasmine-aips@hotmail.com>
2019-10-07 18:18:10 +0800

    gives error when pokemon not found

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
 
 // const jsonfile = require('jsonfile');
 
@@ -7,6 +9,9 @@ const express = require('express');
  * Configurations and set up
  * ===================================
  */
+
+
+
 
 // Init express app
 const app = express();
@@ -17,10 +22,35 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+
+app.get('/pokemon/:pokename', (request, response) => {
+
+    let i = 0;
+    let foundPokemon = null;
+    let pokemonName = request.params.pokename;
+    console.log(`Now searching for ${pokemonName}`);
+
+    jsonfile.readFile(file, (err, obj) => {
+        if (err) {
+            console.log(`Aww shucks there's an error: ${err}`);
+        }
+        for (i = 0; i < obj["pokemon"].length; i++) {
+            console.log("looping i:" + i);
+            if (obj["pokemon"][i].name.toLowerCase() === pokemonName.toLowerCase()) {
+                console.log('OMG')
+                foundPokemon = obj["pokemon"][i];
+                console.log(foundPokemon)
+                console.log(`i is ${i}`);
+                response.send(foundPokemon);
+                break;
+            }
+        }
+        if (foundPokemon === null) {
+            response.status(404).send(`<html><body style='text-align:center'><h1>Error 404</h1><h2>Could not find ${pokemonName}.</h2><img src='https://static.pokemonpets.com/images/monsters-images-300-300/404-Luxio.png'></body></html>`)
+        }
+    })
+})
+
 
 /**
  * ===================================
