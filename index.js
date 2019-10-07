abcf2da1eeeb43d712896ef8bb4992282b6f7431
dannyozh <danielongzhihao@gmail.com>
2019-10-07 21:07:59 +0800

    MVP done

--- index.js
+++ index.js
@@ -19,16 +19,31 @@ const doPokemon = (request, response)=>{
 
   jsonfile.readFile(file, (err, obj) => {
     let pokeList = obj["pokemon"];
+    // console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+    // console.log(pokeList[0])
+    // console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+    // console.log(pokeList[0].name)
+
 
     if( err ){
       console.log("ERR", err );
+      response.send('404 @@@@@ error!!!!!')
     } else {
+        // for (let i = 0; i < pokeList.length; i++) {
+        //     if (i+1 === parseInt(request.params.name)) {
+        //         pokeList[i] = "You chose " + pokeList[i].name;
+        //         response.send(pokeList[i]);
+        //     }
+        // }
+        let message;
         for (let i = 0; i < pokeList.length; i++) {
-            if (i+1 === parseInt(request.params.name)) {
-                pokeList[i] = "You chose " + pokeList[i].name;
-                response.send(pokeList[i]);
+            if (pokeList[i].name === request.params.name) {
+                message = "The weight of " +pokeList[i].name + " is " + pokeList[i].weight;
             }
         }
+
+    response.send(message);
+
 
     // console.log("WOW FINISHED READING",obj.pokemon[1])
     // const firstPoke = obj.pokemon[1];
