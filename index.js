e280b5fc68d3ec2fc922028c09c37e90307e490c
jasminesis <jasmine-aips@hotmail.com>
2019-10-07 22:18:17 +0800

    Working on adding type searches

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/pokemon/:pokename', (request, response) => {
                 foundPokemon = obj["pokemon"][i];
                 console.log(foundPokemon)
                 console.log(`i is ${i}`);
-                response.send(foundPokemon);
+                response.send(`${foundPokemon.name} is a ${foundPokemon.type} type Pokemon. It is weak to ${foundPokemon.weaknesses.join(", ")} attacks.`);
                 break;
             }
         }
@@ -51,6 +51,47 @@ app.get('/pokemon/:pokename', (request, response) => {
     })
 })
 
+app.get('/', (request, response) => {
+    response.send(`<html><body style='text-align:center'><h1>Welcome to the online pokedex!</h1></body><html>`)
+});
+
+// for type -- use RegExp for case insensitive search
+app.get('/type/:ttype', (request, response) => {
+
+    let i = 0;
+    let foundPokemon = [];
+    let pokemonType = request.params.ttype;
+    console.log(`Now searching for Pokemon of ${pokemonType} type`);
+
+    jsonfile.readFile(file, (err, obj) => {
+        console.log("jenny")
+        if (err) {
+            console.log(`Aww jeez there's an error: ${err}`);
+        }
+        for (i = 0; i < obj["pokemon"].length; i++) {
+            console.log("looping i:" + i);
+
+// use RegExp here
+            if (obj["pokemon"][i]["type"].includes(pokemonType)) {
+                foundPokemon.push(obj["pokemon"][i]["name"]);
+                console.log(foundPokemon)
+                console.log(`i is ${i}`);
+                response.send(`${foundPokemon}`);
+            }
+        }
+        if (foundPokemon === null) {
+            response.status(404).send(`<html><body style='text-align:center'><h1>Error 404</h1><h2>Could not find ${pokemonName}.</h2><img src='https://static.pokemonpets.com/images/monsters-images-300-300/404-Luxio.png'></body></html>`)
+        }
+    })
+});
+
+// app.get('/weaknesses/:some-weakness', (request, response) => {
+//     response.send(`<html><body style='text-align:center'><h1>Welcome to the online pokedex!</h1></body><html>`)
+// });
+
+// app.get('/nextevolution/:some-name', (request, response) => {
+//     response.send(`<html><body style='text-align:center'><h1>Welcome to the online pokedex!</h1></body><html>`)
+// });
 
 /**
  * ===================================
