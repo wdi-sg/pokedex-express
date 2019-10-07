2199cb234957d6cb088a1b4b5b689dd7a3e9fd47
lhjd <hongjinlau@gmail.com>
2019-10-07 22:29:11 +0800

    Respond with previous evolution Pokemon

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const jsonfile = require('jsonfile');
  * ===================================
  */
 
-
-
 // Init express app
 const app = express();
 
@@ -19,7 +17,6 @@ const app = express();
  * Routes
  * ===================================
  */
-
 app.get('/pokemon/:name', (request, response) => {
     // send response with some data (a string)
     const file = 'pokedex.json';
@@ -30,10 +27,13 @@ app.get('/pokemon/:name', (request, response) => {
 
         const pokemon = obj.pokemon;
 
-        const selectedPokemon = pokemon.filter(pokemon => pokemon.name === pokemonName);
+        const selectedPokemon = pokemon.find(pokemon => pokemon.name === pokemonName);
 
-        if (selectedPokemon.length) {
-            const responseStr = `This is ${selectedPokemon[0].name}, it is ${selectedPokemon[0].weight} in weight.`;
+        if (selectedPokemon) {
+            const responseStr = `<h1 style="text-align:center">${selectedPokemon.name}</h1><br>\
+                                <img style="margin: 0 auto;display:block" src='${selectedPokemon.img}'/><br>\
+                                <p style="text-align:center"><strong>Weight: </strong>${selectedPokemon.weight}\
+                                <p style="text-align:center"><strong>Height: </strong>${selectedPokemon.height}`;
 
             response.send(responseStr);
         } else {
@@ -56,9 +56,11 @@ app.get('/type/:type', (request, response) => {
 
         if (selectedPokemon.length) {
             let pokemonList = "";
-            selectedPokemon.forEach(pokemon => {pokemonList += `${pokemon.name} *** `});
+            selectedPokemon.forEach(pokemon => {
+                pokemonList += `<img style="display:block;margin: 0 auto" src=${pokemon.img} /><br>\
+                                <p style="text-align:center">${pokemon.name}</p><br><hr>` });
 
-            const responseStr = `List of ${pokemonType} type Pokemon: ` + pokemonList;
+            const responseStr = `<h1 style="text-align:center">List of ${pokemonType} type Pokemon: </h1><br>` + pokemonList;
 
             response.send(responseStr);
         } else {
@@ -81,19 +83,55 @@ app.get('/weakness/:weakness', (request, response) => {
 
         if (selectedPokemon.length) {
             let pokemonList = "";
-            selectedPokemon.forEach(pokemon => {pokemonList += `${pokemon.name} *** `});
 
-            const responseStr = `List of Pokemon with weakness ${pokemonWeakness}: ` + pokemonList;
+            selectedPokemon.forEach(pokemon => {
+                pokemonList += `<img style="display:block;margin: 0 auto" src=${pokemon.img} /><br>\
+                                <p style="text-align:center">${pokemon.name}</p><br><hr>` });
+
+            const responseStr = `<h1 style="text-align:center">List of Pokemon with ${pokemonWeakness} weakness: </h1><br>` + pokemonList;
 
             response.send(responseStr);
         } else {
-            response.status(404).send(`Could not find information about Pokemon with weakness ${pokemonWeakness} - Is that a new pokemon type? Gotta catch em' all!`);
+            response.status(404).send(`Could not find information about Pokemon with weakness ${pokemonWeakness} - Is that a new pokemon weakness? Gotta catch em' all!`);
         }
     });
 });
 
+app.get('/nextevolution/:name', (request, response) => {
+    const file = 'pokedex.json';
+    const pokemonName = request.params.name;
 
-app.get('/', (req,res) => {
+    jsonfile.readFile(file, function(err, obj) {
+        console.log("nextevolution");
+        if (err) { console.error(err) }
+
+        const pokemon = obj.pokemon;
+
+        const selectedPokemon = pokemon.find(pokemon => pokemon.name === pokemonName);
+
+        if (selectedPokemon) {
+
+            if ("prev_evolution" in selectedPokemon) {
+                let prevEvolutionPokemon = "";
+
+                selectedPokemon.prev_evolution.forEach((pokemon, index, array) => {
+                    prevEvolutionPokemon += pokemon.name + (index === array.length - 1 ? "" : " & ");
+                });
+
+                const responseStr = `${pokemonName} evolves from  ${prevEvolutionPokemon}`;
+
+                response.send(responseStr);
+            } else {
+                response.send(`${pokemonName} does not evolve from another Pokemon.`);
+            }
+
+        } else {
+            response.status(404).send(`Could not find information about this Pokemon ${pokemonName} - Is that a new pokemon type? Gotta catch em' all!`);
+        }
+    });
+});
+
+app.get('/', (req, res) => {
     res.send("Welcome to the online Pokedex!");
 })
 
