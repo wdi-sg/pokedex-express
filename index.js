a051d95d9f2e323b1b7b5ea9b70412fe3f665c91
Nathanael Tan <nathanael.tan92@gmail.com>
2019-10-07 17:23:15 +0800

    First deliverable done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require("jsonfile");
 
 /**
  * ===================================
@@ -16,15 +16,44 @@ const app = express();
  * Routes
  * ===================================
  */
+const getName = (request, response) => {
+  const file = "pokedex.json"
+  
+  jsonfile.readFile(file, (err, obj) => {
+    if(err) {
+      console.log("err", err);
+    }
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+    const pokeDex = obj.pokemon;
+    for(let i = 0; i< pokeDex.length; i++) {
+      let name = pokeDex[i].name
+      let weight = pokeDex[i].weight
+      let img = pokeDex[i].img
+      let input = request.params.name
+      if(input=== name){
+       
+        response.send(` <img src=${img}> <br> The Pokemon is ${name}, his weight is ${weight}`)
+      } else if(!pokeDex.includes(request.params.name)){
+        response.send('asdasds')
+      }
+    }
+
+  })
+}
+
+app.get('/pokemon/:name', getName)
+
+  
+
+
+
+
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () =>
+  console.log("~~~ Tuning in to the waves of port 3000 ~~~")
+);
