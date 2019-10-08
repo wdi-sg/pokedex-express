1f2b28e637c6d3480e7edc2d2187a0ed11d70908
syahirah <syahirah.suradi@gmail.com>
2019-10-08 09:13:38 +0800

    completed first part. Url number fetch pokemon index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 /**
  * ===================================
@@ -10,6 +9,42 @@ const express = require('express');
 
 // Init express app
 const app = express();
+const file = 'pokedex.json';
+
+jsonfile.readFile(file, (err, obj) => {
+    //const pokemonName = obj.pokemon[i].name;
+    //const pokemonWeight = obj.pokemon[i].weight;
+
+    let pokemonName = "";
+    let pokemonWeight = "";
+    app.get('/pokemon/:x', (request, response) => {
+    //takes user input and assign it to variable i
+    let i = request.params.x;
+    pokemonName = obj.pokemon[i].name;
+    pokemonWeight = obj.pokemon[i].weight;
+
+    if( err ){
+      console.log("ERROR!!!", err );
+    }
+
+     response.send(`Pokemon Name: ${pokemonName} Weight: ${pokemonWeight}`);
+});
+});
+
+   /* app.get("/pokemon/:name", (request, response) => {
+    let pokeNum = null;
+
+     for (let i=0; i<obj.pokemon.length; i++){
+        request.params.name = obj.pokemon[i].name;
+        i = pokeNum;
+        return pokeNum;
+    }
+    pokemonWeight = obj.pokemon[pokeNum].weight;
+    response.send(`Pokemon Name: ${request.params.name} Weight:  ${pokemonWeight}`)
+});*/
+
+
+//});
 
 /**
  * ===================================
@@ -17,10 +52,7 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+
 
 /**
  * ===================================
