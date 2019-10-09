32d19fbf4863e5a305e3dab7770fbc347b5b2536
wongyijin8183 <wongyijin8183@gmail.com>
2019-10-09 10:54:44 +0800

    file creation and search by name

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+console.log(file.pokemon);
+
+
+// app.get('/', (request, response) => {
+//   response.send("Welcome to this Pokemon Search Engine");
+// });
+
+
+// app.get('/pokemon/:number', (request, response) => {
+//   console.log("Entered No. " + request.params.number);
+
+//   jsonfile.readFile(file, (err,obj) => {
+//     if( err ) {console.log("ERR", err )};
+//     const x = request.params.number; 
+//     const chosenPoke = obj.pokemon[x]["name"];
+  
+
+//     response.send ("You have found: " + chosenPoke);
+//   })
+// });
+
+//OOOOOOKKKKK
+
+app.get('/pokemon/:name', (request, response) => {
+  console.log ("Searching for: " + request.params.name);
+  
+  jsonfile.readFile(file, (err, obj) => {
+    if( err ) {console.log("ERR", err )};
+    let pokemonName = request.params.name;
+    let foundPoke;
+    for ( let i=0; i < obj["pokemon"].length; i++ ){
+      if (obj["pokemon"][i]["name"] === pokemonName);
+        foundPoke = obj["pokemon"][i];
+        
+    }
+    response.send (foundPoke);
+  })
 });
+    
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+ 
+
+
+
+
+
+
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
