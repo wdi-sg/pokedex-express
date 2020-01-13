35114eb0bd22e7ee73626ffbc06fc03b60cc26e2
Jess <jessicatanhc@gmail.com>
2020-01-13 16:19:34 +0800

    Return Pokemon and its weight based on name

--- index.js
+++ index.js
@@ -23,13 +23,20 @@ const app = express();
  * ===================================
  */
 
-app.get('/pokemon/:idOfPokemon', (request, response) => {
+app.get('/pokemon/:nameOfPokemon', (request, response) => {
   // send response with some data (a string)
     jsonfile.readFile(file, (err, obj)=>{
         if(err === null){
-            let index = parseInt(request.params.idOfPokemon)-1;
-            let pokemon = obj["pokemon"][index];
-            response.send(pokemon);
+            let name = request.params.nameOfPokemon;
+            let listOfPokemon = obj["pokemon"];
+            for(let i = 0 ; i< listOfPokemon.length; i ++){
+         if(listOfPokemon[i]["name"] == name){
+            let pokemon = listOfPokemon[i];
+            response.send(pokemon["name"] + " weighs "+ pokemon["weight"] );
+           }
+       }
+           response.send("Pokemon does not exist");
+
         }else{
             response.send(err);
         }
