fad4a9e1a1d6ed302531260cc1b0763f53279d28
joycemap <57893220+joycemap@users.noreply.github.com>
2020-01-13 17:15:38 +0800

    edits to fix malform arrow function error

--- index.js
+++ index.js
@@ -16,27 +16,27 @@ const app = express();
  * Routes
  * ===================================
  */
-const whenRequestisReceived = (request,response) => {
+const whenRequestisReceived = (request,response) => 
+{
   const file = 'pokedex.json'
 
   jsonfile.readFile(file, (err, obj) => {
-
-    if(request.path === '/pokemon/:num'){
-      let myNum = req.params[0].split("/");
-      for (myNum = 0; myNum < pokemon.length; myNum++);
+      console.log(request.path)
+    if(request.path === '/pokemon/:i'){
+      let i = req.params[0].split("/");
+      for (let i = 0; i< pokemon.length; i++);
       response.send(pokemon.weight[i]);
     }
     // else{
     //   let name = request.params.some-name;
     //   response.status(404).send("Could not find information about" + name + "- Is that a new pokemon? Gotta catch em' all!" );
     // }
-
-
+  })
 }
-app.get('/pokemon/:some-name', whenRequestisReceived) => {
-  // send response with some data (a string)
-  response.send(pokemon.weight[i]);
-});
+
+// app.get('/pokemon/:some-name', whenRequestisReceived) => {
+//   // send response with some data (a string)
+//   response.send(pokemon.weight[i]);
 
 /**
  * ===================================
