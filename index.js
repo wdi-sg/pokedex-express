f2167ca98716322d3dd468edda3137f7dd20ce0e
“hweechieh” <hweechieh@gmail.com>
2020-01-13 19:40:14 +0800

    Level One Completed

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
+//Configurations and setup
+const jsonfile = require('jsonfile');
+const file = "pokedex.json";
+
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+//Routes
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const whenPokemonRequest = (request, response) => {
+
+        jsonfile.readFile(file, (err, obj) => {
+
+        request.path === "/pokemon/:index";
+
+        for (let i = 0; i < obj['pokemon'].length; i++) {
+            if (parseInt(request.params.index) === i) {
+            response.send(obj['pokemon'][i - 1]);
+            console.log(obj['pokemon'][i - 1]);
+        }
+      };
+    });
+  };
+
+app.get("/pokemon/:index", whenPokemonRequest);
+
+//Listen to port
+app.listen(3000);
+
+
+
+
+
+// app.get('/pokemon/:index', whenPokemonRequest );
+// const whenPokemonRequest = (request, response) => {
+//   response.send(obj.['pokemon'][i]);
