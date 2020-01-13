24b305fbc73949219633d58ff2290b2ff88e2944
Robert Kolsek <kolsek.income@gmail.com>
2020-01-13 14:14:44 +0800

    MVP and first Further

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+
+const file = 'pokedex.json'
 
 /**
  * ===================================
@@ -17,9 +19,36 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
+app.get('/pokemon/:name', (request, response) => {
+
+  const input = request.params.name
+  let pokemon
+
+//readFile is async, so all code inside of here
+  jsonfile.readFile(file, (err, obj) => {
+    //check for error first
+    if (err) {
+      return console.log(err)
+    }
+    //loop through pokemon
+    for (let i = 0; i < obj.pokemon.length; i++) {
+      //check if input matches a pokemon
+      if (input.toLowerCase() === obj.pokemon[i].name.toLowerCase()) {
+        pokemon = obj.pokemon[i]
+        break
+      }
+    }
+    //if pokemon has content, send it
+    if (pokemon) {
+      response.send(pokemon)
+    } else {
+      // if pokemon has no content, error message
+      response.status(404)
+      response.send(`Could not find information about ${input} - Is that a new pokemon? Gotta catch em' all!`)
+    }
+
+  })
   // send response with some data (a string)
-  response.send(request.path);
 });
 
 /**
