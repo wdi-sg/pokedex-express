e7328f038dc7265d633e632dc3538c0b07e37e4d
benjacoblee <benjacoblee@gmail.com>
2020-01-13 08:05:38 +0800

    done till last further, second part: shows further details of pokemon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const express = require("express");
+const jsonfile = require("jsonfile");
+const file = "./pokedex.json";
 
 /**
  * ===================================
@@ -17,9 +17,42 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
+app.get("/pokemon/", (request, response) => {
+  response.send("Welcome to the online Pokedex!");
+});
+
+app.get("/pokemon/:name", (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+  // response.send(request.path);
+  let foundPokemon;
+  const name = request.params.name.toLowerCase();
+  jsonfile.readFile(file, function(err, obj) {
+    const pokemonArray = obj["pokemon"];
+    if (err) console.error(err);
+    for (let i = 0; i < pokemonArray.length; i++) {
+      const pokemonJson = pokemonArray[i];
+      const pokemonName = pokemonJson.name.toLowerCase();
+      if (name === pokemonName) {
+        foundPokemon = pokemonJson;
+      }
+    }
+    if (foundPokemon !== undefined) {
+      response.send(`<html>You searched for <b>${foundPokemon.name}</b>.
+      <p>Id: ${foundPokemon.id}</p>
+      <p>Num: ${foundPokemon.num}</p>
+      <p>Name: ${foundPokemon.name}</p>
+      <p>Image: <img src="${foundPokemon.img}"></p>
+      <p>Type: ${foundPokemon.type.join(", ")}</p>
+      <p>Height: ${foundPokemon.height}</p>
+      <p>Weight: ${foundPokemon.weight}</p>
+      </html>`);
+    } else {
+      request.statusCode = 404;
+      response.send(
+        `Error ${request.statusCode}: could not find information on ${name}. Is that a new Pokemon? Gotta catch em' all! `
+      );
+    }
+  });
 });
 
 /**
@@ -27,4 +60,6 @@ app.get('*', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () =>
+  console.log("~~~ Tuning in to the waves of port 3000 ~~~")
+);
