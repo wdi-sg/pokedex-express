660af377b65c630f35375b31aaa1a9be74bc2222
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2020-01-13 23:57:08 +0800

    In progress - type of pokemon

--- index.js
+++ index.js
@@ -15,7 +15,35 @@ const app = express();
  * Routes
  * ===================================
  */
+//Expose a new route for /type/some-type that returns a message listing the names of all pokemon that have the specified type (eg. /type/grass should show a page with names of all pokemon of grass type).
+const pokemonType = (request, response) => {
+    const file = 'pokedex.json'; //pokemon array inside object
 
+    jsonfile.readFile(file, (err, obj) => {
+
+        //loop over array to validate type
+        let userInput = request.params.*;
+
+        for (var i = 0; i < obj.pokemon.length; i++) {
+            var pokeCharacter = obj.pokemon[i].name.toLowerCase();
+                if (userInput === pokeCharacter) {
+                found = 'Y';
+                break
+                }
+            }
+            if (found === 'Y') {
+            console.log(pokeCharacter);
+            response.send(`This is ${pokeCharacter}, it is ${obj.pokemon[i].weight} in weight and ${obj.pokemon[i].height} in height. It's type is/are: ${obj.pokemon[i].type}`);
+            }
+            else {
+                response.status('404').send(`Could not find information about ${userInput} - Is that a new pokemon? Gotta catch em' all!`);
+                }
+            console.log(userInput);
+
+   })
+}
+
+app.get('/pokemon/:type/*', pokemonType);
 
 //when request path is empty, return welcome msg
 const welcome = (request, response) => {
