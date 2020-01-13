f3c8260329c7ff77a6e07fbf814917cd45e6531d
Angel Ferreros <AngelFerreros@tjs-macbook-pro.lan>
2020-01-13 22:47:29 +0800

    Display error message if pokemon not found

--- index.js
+++ index.js
@@ -16,21 +16,32 @@ const app = express();
  * ===================================
  */
 
-app.get('/pokemon/:index', (request, response) => {
+app.get('/pokemon/:name', (request, response) => {
 
-    const file = 'pokedex.json';  //pokemon array inside object
+    const file = 'pokedex.json'; //pokemon array inside object
+
     jsonfile.readFile(file, (err, obj) => {
-        if (err === null) {
-            console.log(obj.pokemon[request.params.index]);
-            response.send(`Weight of ${obj.pokemon[request.params.index].name} is ${obj.pokemon[request.params.index].weight}.`);
-        }
-        else{
-            response.status('400').send(`Not found.`);
-        }
+        //loop over array to validate name
+        let userInput = request.params.name;
+        var found = 'N';
+        for (var i = 0; i < obj.pokemon.length; i++) {
+            var pokeCharacter = obj.pokemon[i].name.toLowerCase();
+                if (userInput === pokeCharacter) {
+                found = 'Y';
+                break
+                }
+            }
+            if (found === 'Y') {
+                console.log(obj.pokemon[i].weight);
+                response.send(`Weight of ${pokeCharacter} is ${obj.pokemon[i].weight}.`);
+            }
+            else {
+                response.status('404').send(`Could not find information about ${userInput} - Is that a new pokemon? Gotta catch em' all!`);
+                }
+            console.log(userInput);
+            console.log(pokeCharacter);
    })
 
-  // send response with some data (a string)
-  // response.send(request.path);
 });
 
 /**
