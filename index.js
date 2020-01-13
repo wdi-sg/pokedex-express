95c84b8664e85924eb02512ee64e6cde8fe7a925
SamTanFL <samsite_tan@hotmail.com>
2020-01-13 19:00:21 +0800

    finished first part. now try to get the names to work

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
+const jsonfile = require('jsonfile');
 const app = express();
+const pokemonData = 'pokedex.json';
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+/*app.get('/pokemon/:index', (request, response) => {
+    let index = parseInt(request.params.index) - 1;
+    jsonfile.readFile(pokemonData, (err, obj) => {
+        let pokemon = obj.pokemon[index]
+        if (pokemon !== null) {
+            response.send("Name : " + pokemon.name + "<br>Weight : " + pokemon.weight);
+        } else {
+            response.redirect(404, "/pokemon")
+        }
+    })
+});*/
+
+app.get('/pokemon/:iden', (request, response) => {
+    if (parseInt(request.params.iden) !== NaN) {
+        let index = parseInt(request.params.iden) -1;
+        jsonfile.readFile(pokemonData, (err, obj) => {
+            let pokemon = obj.pokemon[index];
+            if (pokemon !==  undefined) {
+                response.send("Name : " + pokemon.name + "<br>Weight : " + pokemon.weight);
+            } else {
+                console.log(parseInt(request.params.iden));
+                response.redirect(404, "/pokemon");
+            }//else statement
+        })//readFile closing
+    } else {
+        response.send("This ends here for now");
+    }//else statement. this should execute if path is not a number
+})//app.get end
+
+
+
+
+
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
