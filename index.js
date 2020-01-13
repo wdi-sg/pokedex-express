d548633033434fe83e7a81460d82844899b6ece0
Stuart Myers <stuart.myers@gmail.com>
2020-01-13 16:11:24 +0800

    First deliverable completed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 /**
  * ===================================
@@ -10,6 +9,42 @@ const express = require('express');
 
 // Init express app
 const app = express();
+const dataFile = 'pokedex.json';
+
+/**
+ * ===================================
+ * Functions
+ * ===================================
+ */
+
+const displayPokemon = (request, response) => {
+  console.log('displaying pokemon ' + request.params.name);
+  jsonfile.readFile(dataFile, (err, obj) => {
+    if (err) {
+      response.status(500).send('Error: ' + err);
+      return;
+    }
+    const pokemonArray = obj.pokemon;
+
+    // using for ... of loops. Much nicer than index search for this sort of thing.
+    for (const pokemon of pokemonArray) {
+      // console.log(`name: ${pokemon.name}, weight ${pokemon.weight}`);
+      if (pokemon.name.toLowerCase() === request.params.name.toLowerCase()) {
+        console.log
+        response.send(returnPokemonByName(pokemon));
+        break;
+      }
+    }
+
+  })
+}
+
+const returnPokemonByName = (pokemon) => {
+  const name = pokemon.name;
+  const weight = pokemon.weight;
+  let responseString = `pokemon name: ${name} weight: ${weight}`
+  return responseString;
+}
 
 /**
  * ===================================
@@ -17,10 +52,14 @@ const app = express();
  * ===================================
  */
 
+app.get('/pokemon/:name', displayPokemon)
+
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+  response.send(`received something I don't understand: ${request}`);
 });
+
+// Making a change
 
 /**
  * ===================================
