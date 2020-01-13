f601ee0455b93d59b9d958b39589bf5a973038b3
Stuart Myers <stuart.myers@gmail.com>
2020-01-13 16:41:25 +0800

    Can find pokemon by number

--- index.js
+++ index.js
@@ -25,24 +25,43 @@ const displayPokemon = (request, response) => {
       return;
     }
     const pokemonArray = obj.pokemon;
+    const requestName = request.params.name
+    
+    // If input is a number, display the pokemon by its number;
+    if (parseInt(requestName)) {
+      if (requestName >= 0 && requestName < pokemonArray.length ) {
+        response.send(returnPokemon(pokemonArray[requestName-1]))
+        return;
+      }
+    }
 
     // using for ... of loops. Much nicer than index search for this sort of thing.
     for (const pokemon of pokemonArray) {
       // console.log(`name: ${pokemon.name}, weight ${pokemon.weight}`);
-      if (pokemon.name.toLowerCase() === request.params.name.toLowerCase()) {
+      if (pokemon.name.toLowerCase() === requestName.toLowerCase()) {
         console.log
-        response.send(returnPokemonByName(pokemon));
+        response.send(returnPokemon(pokemon));
         return;
       }
     }
-    response.status(404).send(`Could not find information about ${request.params.name} - Is that a new pokemon? Gotta catch em' all!`)
+    response.status(404).send(`Could not find information about ${requestName} - Is that a new pokemon? Gotta catch em' all!`)
   })
 }
 
-const returnPokemonByName = (pokemon) => {
+const returnPokemon = (pokemon) => {
   const name = pokemon.name;
   const weight = pokemon.weight;
-  let responseString = `pokemon name: ${name} weight: ${weight}`
+  const height = pokemon.height;
+  let types = "";
+  for (let i = 0; i < pokemon.type.length; i++) {
+    console.log(pokemon.type[i]);
+    if (i === pokemon.type.length - 1) {
+      types += pokemon.type[i];
+    } else {
+      types += pokemon.type[i] + ", ";  
+    }
+  }
+  let responseString = `This is ${name}, they weigh ${weight} and stand ${height} tall. They are ${types} type.`
   return responseString;
 }
 
@@ -57,8 +76,11 @@ const displayIndexPage = () => {
  * ===================================
  */
 
+
+// Display pokemon by name
 app.get('/pokemon/:name', displayPokemon)
 
+// Display index page
 app.get('/pokemon/', (request, response) => {
   console.log('sending index page');
   response.send(displayIndexPage());
