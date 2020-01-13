e9d567de7126cb7193edb3cd0cf78efb6997b3b1
rachellesg <rachelle.yeo@outlook.com>
2020-01-13 21:32:25 +0800

    updated searchable types

--- index.js
+++ index.js
@@ -28,37 +28,54 @@ app.get('/pokemon', (request, response) => {
 app.get('/pokemon/:id', (request, response, obj) => {
   // send response with some data (a string)
   jsonfile.readFile(file, (err,obj) => {
-    const checkPokemon = request.params.id;
+    const checkPokemon = request.params.id.toLowerCase();
     const listOfPokemon = obj.pokemon.length;
     if (!isNaN(checkPokemon)) {
-      response.send("This is " + obj.pokemon[checkPokemon].name + "! He is " + obj.pokemon[checkPokemon].weight + " in weight! He also " );
+      response.send("This is " + obj.pokemon[checkPokemon].name + "! He is " + obj.pokemon[checkPokemon].weight + " in weight and is also " + obj.pokemon[checkPokemon].height +  " in height." );
     } else if (isNaN(checkPokemon)) {
         //console.log(request.path + "this is " + checkPokemon + obj.pokemon.length);
-        let pokemonName = checkPokemon.toUpperCase();
+        let pokemonName = checkPokemon.toLowerCase();
         for (i = 0; i < listOfPokemon; i++) {
-        let checkPokemonName = obj.pokemon[i].name.toUpperCase();
-          if (pokemonName === checkPokemonName) {
+        let checkPokemonName = obj.pokemon[i].name.toLowerCase();
+          if (checkPokemon === checkPokemonName) {
             console.log(obj.pokemon[i].name);
             console.log(obj.pokemon[i].id);
             response.send("Name: " + obj.pokemon[i].name + "<br> Weight: " + obj.pokemon[i].weight);
           }
         }
-        response.send("You got: " + obj.pokemon.name);
+         response.status(404).send("Could not find information about " + checkPokemon + " Is that a new pokemon? Gotta catch em' all!");
       }
   });
 });
 
+app.get('/pokemon/type/:type', (request, response) => {
+  jsonfile.readFile(file, (err,obj) => {
+    const checkTypes = request.params.type.toLowerCase();
+    const listOfPokemon = obj.pokemon.length;
+    if (isNaN(checkTypes)) {
+      for (i = 0; i < listOfPokemon; i++) {
+        let checkPokemonType = obj.pokemon[i].type;
+        let listOfTypes = checkPokemonType.length;
+        //console.log(obj.pokemon[i].name + checkTypes + checkPokemonType);
+        for (j = 0; j < listOfTypes; j++) {
+          //console.log(checkPokemonType); // this prints
+          const checkNumberTypes = checkPokemonType[j].toLowerCase();
+          if (checkTypes === checkNumberTypes) {
+            console.log(obj.pokemon[i].name + obj.pokemon[i].type[j] + obj.pokemon[i].id)
+          }
+        }
+      }
+      //response.send(pokemonTypes);
+      //console.log(checkTypes);
+    }
+  });
+});
 
+          // if (checkTypes === checkNumberTypes) {
+          //   console.log(obj.pokemon[i].type[j]);
+          //   response.send(obj.pokemon[i].name + obj.pokemon[i].type[j]);
+          // }
 /**
- * ===================================
- * Read files or whatever
- * ===================================
- */
-
-
-
-/**
-
  * ===================================
  * Listen to requests on port 3000
  * ===================================
