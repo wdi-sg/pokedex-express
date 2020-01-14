e092725818716f68d2860ba45237ea5e2bd7db28
“hweechieh” <hweechieh@gmail.com>
2020-01-14 13:54:41 +0800

    Attempt on first Further

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 //Routes
-
+//When valid pokemon request sent
 const whenPokemonRequest = (request, response) => {
 
         jsonfile.readFile(file, (err, obj) => {
@@ -18,6 +18,7 @@ const whenPokemonRequest = (request, response) => {
             if (parseInt(request.params.index) === i) {
             response.send(obj['pokemon'][i - 1]);
             console.log(obj['pokemon'][i - 1]);
+            // console.log(obj['pokemon'].name)
         }
       };
     });
@@ -25,13 +26,31 @@ const whenPokemonRequest = (request, response) => {
 
 app.get("/pokemon/:index", whenPokemonRequest);
 
+
+
+//When invalid pokemon request sent
+const whenInvalidPokemonRequest = (request, response) => {
+
+        jsonfile.readFile(file, (err, obj) => {
+
+        request.path === "/pokemon/:random";
+        let randomName = request.params.random;
+
+        for (let i = 0; i < obj['pokemon'].length; i++) {
+            if (randomName !== obj['pokemon'][i].name) {
+
+            response.send("Could not find information about " + randomName + " - Is that a new pokemon? Gotta catch em' all!");
+            console.log(randomName);
+        }
+      };
+    });
+  };
+
+app.get("/pokemon/:random", whenInvalidPokemonRequest);
+
+
 //Listen to port
 app.listen(3000);
 
 
 
-
-
-// app.get('/pokemon/:index', whenPokemonRequest );
-// const whenPokemonRequest = (request, response) => {
-//   response.send(obj.['pokemon'][i]);
