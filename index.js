aade8f15672b95f11c3154a176501980843f260c
leowzhenkang <leowzk@hotmail.com>
2020-01-14 10:54:49 +0800

    completed part 1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 /**
  * ===================================
@@ -11,16 +10,39 @@ const express = require('express');
 // Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+// *
+//  * ===================================
+//  * Routes
+//  * ===================================
+const getInfo = function(request, response){
+    const file = "pokedex.json";
+    let input = request.params.name;
+    console.log("Before read");
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+    jsonfile.readFile(file,(err,obj) => {
+        const pokedex = obj.pokemon;
+        console.log("Reading file");
+
+        for (var i = 0; i < pokedex.length; i++) {
+            let name = pokedex[i].name;
+            let weight = pokedex[i].weight
+            if (name.toLowerCase() == input.toLowerCase()) {//browser will automatically set things to lower case
+                response.send("Pokemon name: " + name + " Weight: " + weight)
+            }
+        }
+    })
+}
+// app.get('*', (request, response) => {
+//   // send response with some data (a string)
+//   response.send(request.path);
+// });``
+app.get('/pokemon/:name', getInfo)
+
+
+
+
+
+
 
 /**
  * ===================================
