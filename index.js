7dcb2be183136c71a719d538bbd2f33dccf2fb3a
wickker <choydianchun@gmail.com>
2020-04-06 19:30:41 +0800

    completed further 2c

--- index.js
+++ index.js
@@ -12,16 +12,27 @@ app.get("/pokemon/:name", (request, response) => {
   jsonFile.readFile(dataFile, (err, obj) => {
     // console.log(request.params);
     let matchingPokemonObj = obj[POKEMON].find((element) => {
-      return element.name.toLowerCase() == request.params.name.toLowerCase();
+      return element.name.toLowerCase() === request.params.name.toLowerCase();
     });
     // console.log(matchingPokemonObj);
     if (matchingPokemonObj === undefined) {
       response.status(404).send("Could not find information about " + request.params.name + " - Is that a new pokemon? Gotta catch em' all!");
     } else {
       // response.send(request.params.name + " weight: " + matchingPokemonObj.weight);
-      console.log(matchingPokemonObj);
+      // console.log(matchingPokemonObj);
       response.send(matchingPokemonObj);
     }
+  });
+});
+
+app.get("/type/:sometype", (request, response) => {
+  jsonFile.readFile(dataFile, (err, obj) => {
+    let matchingPokemonArr = obj[POKEMON].filter((element) => {
+      for (let i = 0; i < element.type.length; i++) {
+        return element.type[i].toLowerCase() === request.params.sometype.toLowerCase();
+      }
+    });
+    response.send(matchingPokemonArr.map(element => element.name));
   });
 });
 
