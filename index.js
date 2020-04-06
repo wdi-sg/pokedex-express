795ba85dab456620ea5534915d6180abe20990cf
osbornechan <osbornecab@gmail.com>
2020-04-06 21:06:28 +0800

    Display information

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 /**
  * ===================================
@@ -10,16 +9,36 @@ const express = require('express');
 
 // Init express app
 const app = express();
-
+const pokedex = 'pokedex.json'
 /**
  * ===================================
  * Routes
  * ===================================
  */
+ jsonfile.readFile(pokedex, (err, obj) => {
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+    app.get('/pokemon/*', (request, response) => {
+
+        let pokemonList = obj.pokemon;
+
+        for (let i = 0; i < pokemonList.length; i++) {
+            if (request.params[0] === pokemonList[i].name.toLowerCase() || request.params[0] == pokemonList[i].id) {
+                console.log(' pokemon i: ' + pokemonList[i].name)
+                console.log('request: ' + request.params[0]);
+                response.send(`<h1>${pokemonList[i].name}</h1>
+                    <h2>${pokemonList[i].weight}</h2>
+                    <h2>${pokemonList[i].type.join(', ')}</h2>
+                    <img src='${pokemonList[i].img}'>
+                    `);
+            }
+        }
+     });
+
+    app.get('*', (request, response) => {
+      // send response with some data (a string)
+      response.send(request.path);
+    });
+
 });
 
 /**
