ac0cc17dffb2a42ac234eabcbc2d8355fb69e9b1
Dyana Wu <dyanawu@gmail.com>
2020-04-07 03:30:46 +0800

    Fix long lines

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ app.get('/list', (req, res) => {
   let results = [];
   for (let mon of pokedex) {
     let mon = pokedex[i];
-    results.push(`<a href='pokemon/${mon.name.toLowerCase()}'>${mon.num}: ${mon.name}</a>`);
+    results.push(
+      `<a href='pokemon/${mon.name.toLowerCase()}'>${mon.num}:` +
+      `${mon.name}</a>`);
   }
   res.send(results.join('<br>'));
 });
@@ -38,14 +40,18 @@ app.get('/pokemon/:name', (req, res) => {
   if (results.length > 1) {
     let results = [];
     for (let mon of results) {
-      results.push(`<a href='${mon.name.toLowerCase()}'>${mon.num}: ${mon.name}</a>`);
+      results.push(`<a href='${mon.name.toLowerCase()}'>${mon.num}: ` +
+                   `${mon.name}</a>`);
     }
     results.unshift(
       "There seems to be more than one Pokemon with a name like that!\n" +
       "Did you want one of these?");
     res.send(results.join('<br>'));
   } else if (results.length ===0) {
-    res.status(404).send(`Could not find information about ${req.params.name[0].toUpperCase()}${req.params.name.slice(1)} - is that a new Pokemon? Gotta catch 'em all!`);
+    res.status(404)
+      .send(`Could not find information about ` +
+            `${req.params.name[0].toUpperCase()}${req.params.name.slice(1)}` +
+            ` - is that a new Pokemon? Gotta catch 'em all!`);
   }
 
   let mon = results[0];
@@ -55,11 +61,17 @@ app.get('/pokemon/:name', (req, res) => {
     `<img src=${mon.img}>`,
     `This is Pokemon #${mon.num}, ${mon.name}!`,
     `They are of type(s): ${[...mon.type].join(', ')}`,
-    `They are weak against Pokemon of these type(s): ${mon.weaknesses.join(', ')}`,
-    `They are known to grow to ${mon.height} and weigh ${mon.weight} fully grown.`,
-    mon.candy === "None" ? `${mon.name} don't really like candy.` : `${mon.name} love ${mon.candy}!`,
-    mon.egg === "Not in Eggs" ? `They are not in eggs.` : `Their eggs take ${mon.egg} to hatch.`,
-    mon.next_evolution ? `They'll evolve into ${mon.next_evolution[0].name}!` : `They don't have any evolutions.`,
+    `They are weak against these type(s): ${mon.weaknesses.join(', ')}`,
+    `They'll grow to ${mon.height} and weigh ${mon.weight} fully grown.`,
+    mon.candy === "None" ?
+      `${mon.name} don't really like candy.`
+      : `${mon.name} love ${mon.candy}!`,
+    mon.egg === "Not in Eggs" ?
+      `They are not in eggs.`
+      : `Their eggs take ${mon.egg} to hatch.`,
+    mon.next_evolution ?
+      `They'll evolve into ${mon.next_evolution[0].name}!`
+      : `They don't have any evolutions.`,
   ];
   res.send(monFormat.join('<br>'));
 });
@@ -76,9 +88,11 @@ app.get('/type/:type', (req, res) => {
   }
 
   results = results.map(function (mon) {
-    return `<a href='../pokemon/${mon.name.toLowerCase()}'>${mon.num}: ${mon.name}</a>`;
+    return `<a href='../pokemon/${mon.name.toLowerCase()}'>` +
+      `${mon.num}: ${mon.name}</a>`;
   });
-  results.unshift(`These are all the Pokemon of ${req.params.type} type in the first 151!`);
+  results.unshift(`These are all the Pokemon of ${req.params.type}` +
+                  ` type in the first 151!`);
   res.send(results.join('<br>'));
 });
 
@@ -94,9 +108,11 @@ app.get('/weakness/:weak', (req, res) => {
   }
 
   results = results.map(function (mon) {
-    return `<a href='../pokemon/${mon.name.toLowerCase()}'>${mon.num}: ${mon.name}</a>`;
+    return `<a href='../pokemon/${mon.name.toLowerCase()}'>` +
+      `${mon.num}: ${mon.name}</a>`;
   });
-  results.unshift(`These are the Pokemon weak against ${req.params.weak} types!`);
+  results.unshift(`These are Pokemon weak against ` +
+                  `${req.params.weak} types!`);
   res.send(results.join('<br>'));
 });
 
@@ -109,10 +125,11 @@ app.get('/nextevolution/:end', (req, res) => {
   if (results.length > 1) {
     let results = [];
     for (let mon of results) {
-      results.push(`<a href='${mon.name.toLowerCase()}'>${mon.num}: ${mon.name}</a>`);
+      results.push(`<a href='${mon.name.toLowerCase()}'>` +
+                   `${mon.num}: ${mon.name}</a>`);
     }
     results.unshift(
-      "There seems to be more than one Pokemon with a name like that!\n" +
+      "There seems to be more than one Pokemon named like that!\n" +
       "Did you want one of these?");
     res.send(results.join('<br>'));
   }
@@ -122,15 +139,19 @@ app.get('/nextevolution/:end', (req, res) => {
   console.log(evolvesFrom);
 
   evolvesFrom = evolvesFrom.map(function (mon) {
-    return `<a href='../pokemon/${mon.name.toLowerCase()}'>${mon.num}: ${mon.name}</a>`;
+    return `<a href='../pokemon/${mon.name.toLowerCase()}'>` +
+      `${mon.num}: ${mon.name}</a>`;
   });
-  evolvesFrom.unshift(`These are the Pokemon that evolve into ${req.params.end[0].toUpperCase()}${req.params.end.slice(1)}!`);
+  evolvesFrom.unshift(
+    `These are the Pokemon that evolve into ` +
+    `${req.params.end[0].toUpperCase()}${req.params.end.slice(1)}!`);
   res.send(evolvesFrom.join('<br>'));
 });
 
 app.get('*', (req, res) => {
   // send res with some data (a string)
-  res.send("Welcome to the online Pokedex! You might want to try <a href='list'>browsing the main list</a>!");
+  res.send("Welcome to the online Pokedex! You might want to try " +
+           "<a href='list'>browsing the main list</a>!");
 });
 
 /**
@@ -138,4 +159,4 @@ app.get('*', (req, res) => {
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () => console.log('~~~ Tuning in to port 3000 ~~~'));
