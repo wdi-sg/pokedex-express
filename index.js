8e68d9254b28b012d65560676a48fcf0d3e56fa7
mdazharaw <darkaz62@hotmail.com>
2020-04-06 23:33:21 +0800

    Invalid names handled. Part 2 done.

--- index.js
+++ index.js
@@ -30,11 +30,34 @@ jsonfile.readFile(file, (err, obj) => {
 });
 // console.log(pokeName)
 
-app.get("/pokemon/:id", (request, response) => {
-  var pokeId = request.params.id;
-  pokeId = parseInt(pokeId);
-  var pokeName = pokemon[pokeId].name;
-  response.send(pokeName);
+// app.get("/pokemon/:id", (request, response) => {
+//   var pokeId = request.params.id;
+//   pokeId = parseInt(pokeId);
+//   var pokeName = pokemon[pokeId].name;
+//   response.send(pokeName);
+
+// });
+
+app.get("/pokemon/:name", (request, response) => {
+  var found = false;
+  var foundId;
+  for (id in pokemon) {
+    // console.log(pokemon[id].name)
+    if (pokemon[id].name.toLowerCase().includes(request.params.name)) {
+      found = true;
+      foundId = id;
+      break;
+    }
+    continue;
+  }
+  if (found == true) {
+    var pokeName = pokemon[foundId].name;
+    response.send(pokeName);
+  }
+  else {
+    var properName = toProperCase(request.params.name);
+    response.send(`Could not find information about ${properName} - Is that a new pokemon? Gotta catch em' all!`);
+  }
 
 });
 
@@ -44,3 +67,11 @@ app.get("/pokemon/:id", (request, response) => {
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+function toProperCase(string){
+  var firstLetter = string.substring(0,1);
+  var restOfLetter = string.substring(1,string.length);
+  firstLetter = firstLetter.toUpperCase();
+  restOfLetter = restOfLetter.toLowerCase();
+  return firstLetter+restOfLetter;
+}
