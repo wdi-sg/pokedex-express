710172dfc87c718379ad85612f43bb9ec97dc3f3
lekhweemeng <lek.hweemeng@live.com>
2020-04-06 22:57:03 +0800

    completed to request via name

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ const app = express();
  * ===================================
  */
 //this part is to set up the reading of the pokedex.json.
-    jsonfile.readFile(pokedex, (error, obj) => {
+jsonfile.readFile(pokedex, (error, obj) => {
     console.log("error of pokedex reading is: =============");
     console.log(error);
     console.log("pokedex entries: =============");
     console.log(obj["pokemon"][0]); // this line retrieves pokemon by id, id = process.argv[2] - 1
-    app.get('/pokemon/:pokedexId', (request, response) => {
-  // send response with some data (a string)
-    let pokemonId = parseInt(request.params.pokedexId);
-    response.send(obj["pokemon"][pokemonId-1]);
-    });
-    });
+    app.get('/pokemon/:pokemonName', (request, response) => {
+      // send response with some data (a string)
+        for (i=0;i<obj["pokemon"].length; i++){
+            if(obj["pokemon"][i].name === request.params.pokemonName ){
+                response.send(obj["pokemon"][i]);
+            }
+        }
+
+        });
+});
 // Init express app
 
 
@@ -39,3 +43,61 @@ const app = express();
  * ===================================
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// below is the codes for part 1
+// const express = require('express');
+// const jsonfile = require('jsonfile');
+
+// const pokedex = "pokedex.json";
+// const app = express();
+// /**
+//  * ===================================
+//  * Configurations and set up
+//  * ===================================
+//  */
+// //this part is to set up the reading of the pokedex.json.
+// jsonfile.readFile(pokedex, (error, obj) => {
+//     console.log("error of pokedex reading is: =============");
+//     console.log(error);
+//     console.log("pokedex entries: =============");
+//     console.log(obj["pokemon"][0]); // this line retrieves pokemon by id, id = process.argv[2] - 1
+//     app.get('/pokemon/:pokedexId', (request, response) => {
+//       // send response with some data (a string)
+//         let pokemonId = parseInt(request.params.pokedexId);
+//         response.send(obj["pokemon"][pokemonId-1]);
+//         });
+// });
+// // Init express app
+
+
+
+
+// /**
+//  * ===================================
+//  * Routes
+//  * ===================================
+//  */
+
+
+
+// /**
+//  * ===================================
+//  * Listen to requests on port 3000
+//  * ===================================
+//  */
+// app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
