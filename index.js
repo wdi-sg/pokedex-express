a8d81387ed7b6ebbc77a2ced0ed2cfe3f02f173b
lekhweemeng <lek.hweemeng@live.com>
2020-04-06 22:49:33 +0800

    completed part 1 to call pokedex by pokedex id

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
 
-// const jsonfile = require('jsonfile');
-
+const pokedex = "pokedex.json";
+const app = express();
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
+//this part is to set up the reading of the pokedex.json.
+    jsonfile.readFile(pokedex, (error, obj) => {
+    console.log("error of pokedex reading is: =============");
+    console.log(error);
+    console.log("pokedex entries: =============");
+    console.log(obj["pokemon"][0]); // this line retrieves pokemon by id, id = process.argv[2] - 1
+    app.get('/pokemon/:pokedexId', (request, response) => {
+  // send response with some data (a string)
+    let pokemonId = parseInt(request.params.pokedexId);
+    response.send(obj["pokemon"][pokemonId-1]);
+    });
+    });
 // Init express app
-const app = express();
+
+
+
 
 /**
  * ===================================
@@ -17,10 +31,7 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+
 
 /**
  * ===================================
