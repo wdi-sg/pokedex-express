5b21871c43c4fa673a1db9def7365ecab9aa53f2
wickker <choydianchun@gmail.com>
2020-04-06 19:04:18 +0800

    completed further 1

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ const POKEMON = "pokemon";
 
 app.get("/pokemon/:name", (request, response) => {
   jsonFile.readFile(dataFile, (err, obj) => {
-    console.log(request.params);
+    // console.log(request.params);
     let matchingPokemonObj = obj[POKEMON].find((element) => {
       return element.name.toLowerCase() == request.params.name.toLowerCase();
     });
-    console.log(matchingPokemonObj);
-    response.send(request.params.name + " weight: " + matchingPokemonObj.weight);
+    // console.log(matchingPokemonObj);
+    if (matchingPokemonObj === undefined) {
+      response.status(404).send("Could not find information about " + request.params.name + " - Is that a new pokemon? Gotta catch em' all!");
+    } else {
+      response.send(request.params.name + " weight: " + matchingPokemonObj.weight);
+    }
   });
 });
 
-app.listen(3000, () => {console.log("locating port 3000")});
+app.listen(3000, () => {
+  console.log("locating port 3000");
+});
