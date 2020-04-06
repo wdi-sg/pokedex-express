60981f824a36fde9b111cb4ce06962fd1c1f18ce
kenneththesheep <kenneththesheep@gmail.com>
2020-04-06 22:09:42 +0800

    can search via id and name

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 // const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+const file = './pokedex.json';
+
 
 /**
  * ===================================
@@ -17,14 +20,95 @@ const app = express();
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+app.get('/', (request, response) => {
+  response.send('Welcome to the online Pokdex!')
 });
+
+app.get('/pokemon', (request, response) => {
+  response.send('Welcome to the online Pokdex!')
+});
+
+app.get('/type', (request, response) => {
+  response.send('Welcome to the online Pokdex!')
+});
+
+app.get('/weaknesses', (request, response) => {
+  response.send('Welcome to the online Pokdex!')
+});
+
+app.get('/nextevolution', (request, response) => {
+  response.send('Welcome to the online Pokdex!')
+});
+
+app.get('/pokemon/:id', (request, response) => {
+  // send response with some data (a string)
+
+  console.log("test");
+  jsonfile.readFile(file, (err, obj) => {
+    let pokemonCount=0;
+    if(!isNaN(parseInt(request.params.id)))
+    {
+            let pokemonIndex = parseInt(request.params.id) - 1;
+            if(pokemonIndex>150)
+                {
+                response.send("Could not find information about pokemon number"+ request.params.id +" Is that a new pokemon? Gotta catch em' all!");
+                return;
+                }
+            else{
+                response.send("The pokemon you are searching is "+ obj["pokemon"][pokemonIndex]["name"] + "and his weight is " + obj["pokemon"][pokemonIndex]["weight"]);
+                return;
+                }
+    }
+        else
+        {
+            //response.send("some Text")
+            //console.log(obj["pokemon"][0]["name"]);
+            console.log(request.params.id);
+            console.log(obj["pokemon"][150]["name"]);
+            console.log(obj["pokemon"].length);
+
+            for(pokemonCount=0;pokemonCount<obj["pokemon"].length;pokemonCount++)
+            {
+                console.log(pokemonCount);
+                    if(request.params.id===obj["pokemon"][pokemonCount]["name"])
+                    {
+                        console.log("entered");
+                        response.send("The pokemon you are searching is "+ obj["pokemon"][pokemonCount]["name"] + " and his weight is " + obj["pokemon"][pokemonCount]["weight"]);
+                        return;
+                    }
+
+            }
+            response.send("Could not find information about " + request.params.id +"- Is that a new pokemon? Gotta catch em' all!");
+                        return;
+            }
+
+
+    });
+});
+
+
+app.get('/type/:typing', (request, response) => {
+  // send response with some data (a string)
+
+  console.log("test");
+  jsonfile.readFile(file, (err, obj) => {
+    let pokemonCount=0;
+    let typing=false;
+    let outputString=`<ol>Type: ${request.params.typing}`;
+    response.send(outputString);
+    for(pokemonCount=0;pokemonCount<obj.length;pokemonCount++)
+    {
+        console.log("something happened");
+    }
+
+
+    });
+});
+
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(3000, () => console.log('~~~ The local server 3000 is working ~~~'));
