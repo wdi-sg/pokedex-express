7ca4293927553d4ce4c56d90792c1d518c462721
kokchuantan <kokchuan1996@gmail.com>
2020-04-06 20:48:42 +0800

    first commit, able to seach with pokemon id

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
-
+const jsonfile = require('jsonfile');
+const pokedex = 'pokedex.json';
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
 // Init express app
 const app = express();
-
 /**
  * ===================================
  * Routes
  * ===================================
  */
-
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+app.get('/', (request, response) => {
+    response.send('Hello! Go to ../pokemon for more.');
 });
+
+app.get('/pokemon', (request, response) => {
+    response.send("Welcome to the Pokedex!\n Start by entering a number like so : pokemon/2");
+});
+
+app.get("/pokemon/:id", (request, response) => {
+    const editStuff = (err, obj) => {
+        var pokeId = parseInt(request.params.id) - 1;
+        var pokemon = obj.pokemon[pokeId].name;
+        response.send('Your chosen pokemon is ' + pokemon)
+        const doneReading = (err) => {
+            console.log('done editing')
+        }
+        jsonfile.writeFile(pokedex, obj, doneReading)
+    };
+
+    jsonfile.readFile(pokedex, editStuff);
+    console.log("file read called");
+});
+
+// app.get('*', (request, response) => {
+    // send response with some data (a string)
+//     response.send(request.path);
+// });
 
 /**
  * ===================================
