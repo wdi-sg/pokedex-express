f886d794adf97b5db004bb555b924b602cfe8673
Zachariah Chow <zachariahchow@gmail.com>
2020-04-06 18:09:43 +0800

    completed routes on README (w/o routes for part-two.md)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const jsonfile = require('jsonfile');
+
+const pokedexDataFile = './pokedex.json';
 
 // const jsonfile = require('jsonfile');
 
@@ -11,16 +14,225 @@ const express = require('express');
 // Init express app
 const app = express();
 
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
-});
+
+app.get('/:x', (req, res) => {
+
+    jsonfile.readFile(pokedexDataFile, (err, obj) => {
+
+        obj.pokemon.forEach(pokemon => {
+
+            if (pokemon.id == req.params.x) {
+                res.send(
+                `
+                    <img src="${pokemon.img}" width="90px"><br>
+                    This is <b>${pokemon.name}</b><br>
+                    Height: ${pokemon.height}<br>
+                    Weight: ${pokemon.weight}<br>
+                    Types: ${pokemon.type.toString()}
+                `
+                    );
+                return;
+            }
+        })
+    })
+
+})
+
+app.get('/pokemon/:x', (req, res) => {
+
+    jsonfile.readFile(pokedexDataFile, (err, obj) => {
+
+        let pokemonExists = false;
+
+        obj.pokemon.forEach(pokemon => {
+
+            if (pokemon.name.toLowerCase() == req.params.x.toLowerCase()) {
+
+                res.send(
+                `
+                <img src="${pokemon.img}" width="90px"><br>
+                This is <b>${pokemon.name}</b><br>
+                Height: ${pokemon.height}<br>
+                Weight: ${pokemon.weight}<br>
+                Types: ${pokemon.type.join(", ")}
+                `
+                    );
+
+                pokemonExists = true;
+            }
+        })
+
+        if (!pokemonExists) {
+            res.status(404).send(`<h1>Could not find information about ${req.params.x}, is that a new Pokemon?</h1>`)
+        }
+    })
+
+})
+
+app.get('/type/:x', (req, res) => {
+
+    jsonfile.readFile(pokedexDataFile, (err, obj) => {
+
+        let pokemonTypeExists = false;
+        let pokemonList = "";
+
+        obj.pokemon.forEach(pokemon => {
+
+            if (pokemon.type.map(x => x.toLowerCase()).includes(req.params.x.toLowerCase())) {
+                pokemonList +=
+                `
+                <br>
+                <img src="${pokemon.img}" width="90px"><br>
+                This is <b>${pokemon.name}</b><br>
+                Height: ${pokemon.height}<br>
+                Weight: ${pokemon.weight}<br>
+                Types: ${pokemon.type.join(", ")}
+                <br>
+                `
+
+                pokemonTypeExists = true;
+            }
+        })
+
+        if (!pokemonTypeExists) {
+            res.status(404).send(`<h1>Could not find information about ${req.params.x} type of Pokemon, is that a new Pokemon type?</h1>`)
+        } else {
+            res.send(pokemonList);
+        }
+    })
+})
+
+app.get('/weaknesses/:x', (req, res) => {
+
+    jsonfile.readFile(pokedexDataFile, (err, obj) => {
+
+        let pokemonWeaknessExists = false;
+        let pokemonList = "";
+
+        obj.pokemon.forEach(pokemon => {
+
+            if (pokemon.weaknesses.map(x => x.toLowerCase()).includes(req.params.x.toLowerCase())) {
+                pokemonList +=
+                `
+                <br>
+                <img src="${pokemon.img}" width="90px"><br>
+                This is <b>${pokemon.name}</b><br>
+                Height: ${pokemon.height}<br>
+                Weight: ${pokemon.weight}<br>
+                Types: ${pokemon.type.join(", ")}<br>
+                Weaknesses: ${pokemon.weaknesses.join(", ")}
+                <br>
+                `
+
+                pokemonWeaknessExists = true;
+            }
+        })
+
+        if (!pokemonWeaknessExists) {
+            res.status(404).send(`<h1>Could not find information about ${req.params.x} type of Pokemon, is that a new Pokemon type?</h1>`)
+        } else {
+            res.send(pokemonList);
+        }
+    })
+})
+
+app.get('/nextevolution/:x', (req, res) => {
+
+    jsonfile.readFile(pokedexDataFile, (err, obj) => {
+
+
+        let pokemonExists = false;
+        let pokemonList = "";
+
+        obj.pokemon.forEach(pokemon => {
+
+            if (pokemon.name.toLowerCase() == req.params.x.toLowerCase() && pokemon["next_evolution"][0]) {
+
+                let nextEvo = [];
+
+                pokemon["next_evolution"].forEach(pk => nextEvo.push(pk.name))
+
+
+                res.send(
+                `
+                <img src="${pokemon.img}" width="90px"><br>
+                This is <b>${pokemon.name}</b><br>
+                Height: ${pokemon.height}<br>
+                Weight: ${pokemon.weight}<br>
+                Types: ${pokemon.type.join(", ")}<br>
+                Next Evolutions: ${nextEvo.join(", ")}
+                <br>
+                `
+                );
+            }
+
+            if (pokemon.name.toLowerCase() == req.params.x.toLowerCase()) {
+                pokemonExists = true
+            }
+        })
+
+        if (!pokemonExists) {
+            res.status(404).send(`<h1>Could not find information about ${req.params.x}, is that a new Pokemon?</h1>`)
+        }
+    })
+})
+
+app.get('/prevevolution/:x', (req, res) => {
+
+    jsonfile.readFile(pokedexDataFile, (err, obj) => {
+
+
+        let pokemonExists = false;
+        let pokemonList = "";
+
+        obj.pokemon.forEach(pokemon => {
+
+            if (pokemon.name.toLowerCase() == req.params.x.toLowerCase() && pokemon["prev_evolution"][0]) {
+
+                let prevEvo = [];
+
+                pokemon["prev_evolution"].forEach(pk => prevEvo.push(pk.name))
+
+
+                res.send(
+                `
+                <img src="${pokemon.img}" width="90px"><br>
+                This is <b>${pokemon.name}</b><br>
+                Height: ${pokemon.height}<br>
+                Weight: ${pokemon.weight}<br>
+                Types: ${pokemon.type.join(", ")}<br>
+                Previous Evolutions: ${prevEvo.join(", ")}
+                <br>
+                `
+                );
+            }
+
+            if (pokemon.name.toLowerCase() == req.params.x.toLowerCase()) {
+                pokemonExists = true
+            }
+        })
+
+        if (!pokemonExists) {
+            res.status(404).send(`<h1>Could not find information about ${req.params.x}, is that a new Pokemon?</h1>`)
+        }
+    })
+})
+
+app.get("/", (req, res) => {
+    res.send('Welcome to the online Pokedex!');
+})
+
+// app.get('*', (req, res) => {
+//   // send response with some data (a string)
+//   res.send(req.path);
+// });
 
 /**
  * ===================================
