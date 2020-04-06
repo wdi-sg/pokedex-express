9b48d8ccd1c908272c5210a52489b91c69d165c9
Rachelik <rachel.ik.87@gmail.com>
2020-04-06 23:48:11 +0800

    Add to find all pokemon by type

--- index.js
+++ index.js
@@ -28,16 +28,14 @@ const app = express();
 //     }
 // });
 
-var allPokemonName = [];
-
-jsonfile.readFile(file, (err, obj) => {
-    for (let i = 0; i < obj.pokemon.length; i++) {
-        allPokemonName.push(obj.pokemon[i].name.toLowerCase());
-    };
-});
-
 app.get('/pokemon/:name', (request, response) => {
     jsonfile.readFile(file, (err, obj) => {
+        var allPokemonName = [];
+
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            allPokemonName.push(obj.pokemon[i].name.toLowerCase());
+        };
+
         if (allPokemonName.indexOf(request.params.name.toLowerCase()) === -1) {
             response.send("Status: 404. Could not find information about " +request.params.name + " - Is that a new pokemon? Gotta catch em' all!");
         } else {
@@ -56,6 +54,71 @@ app.get('/pokemon/:name', (request, response) => {
     });
 });
 
+var allPokemonType = [];
+const getAllPokemonType = () => {
+    jsonfile.readFile(file, (err, obj) => {
+        for (let i = 0; i < obj.pokemon.length; i++) {
+            for (let j = 0; j < obj.pokemon[i].type.length; j++) {
+                if (allPokemonType.indexOf(obj.pokemon[i].type[j]) === -1) {
+                    allPokemonType.push(obj.pokemon[i].type[j]);
+                };
+            };
+        };
+        console.log(allPokemonType);
+    });
+};
+
+getAllPokemonType();
+
+app.get('/type/:type', (request, response) => {
+    string = request.params.type.toLowerCase();
+    checkType = string.charAt(0).toUpperCase() + string.slice(1);
+
+    const getPokemonOfType = typeOfPokemon => {
+        var allPokemonOfType = [];
+        jsonfile.readFile(file, (err, obj) => {
+            for (let j = 0; j < obj.pokemon.length; j++) {
+                if (obj.pokemon[j].type.indexOf(typeOfPokemon) !== -1) {
+                    allPokemonOfType.push(obj.pokemon[j].name);
+                }
+            }
+            var allPokemonOfTypeList = allPokemonOfType.toString().split(",").join("<br>")
+            response.send(allPokemonOfTypeList);
+        });
+    };
+
+    if (checkType === "Grass") {
+        getPokemonOfType("Grass");
+    } else if (checkType === "Poison") {
+        getPokemonOfType("Poison");
+    } else if (checkType === "Fire") {
+        getPokemonOfType("Fire");
+    } else if (checkType === "Flying") {
+        getPokemonOfType("Flying");
+    } else if (checkType === "Water") {
+        getPokemonOfType("Water");
+    } else if (checkType === "Bug") {
+        getPokemonOfType("Bug");
+    } else if (checkType === "Normal") {
+        getPokemonOfType("Normal");
+    } else if (checkType === "Electric") {
+        getPokemonOfType("Electric");
+    } else if (checkType === "Ground") {
+        getPokemonOfType("Ground");
+    } else if (checkType === "Fighting") {
+        getPokemonOfType("Fighting");
+    } else if (checkType === "Psychic") {
+        getPokemonOfType("Psychic");
+    } else if (checkType === "Rock") {
+        getPokemonOfType("Rock");
+    } else if (checkType === "Ice") {
+        getPokemonOfType("Ice");
+    } else if (checkType === "Ghost") {
+        getPokemonOfType("Ghost");
+    } else if (checkType === "Dragon") {
+        getPokemonOfType("Dragon");
+    };
+})
 
 app.get('*', (request, response) => {
     if (request.path === "/pokemon/"+""){
