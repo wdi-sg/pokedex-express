85a46efcd592e54b1d90e43b9de9482f23b2ec3d
osbornechan <osbornecab@gmail.com>
2020-04-07 02:04:05 +0800

    Add type and weaknesses, refactor code

--- index.js
+++ index.js
@@ -15,30 +15,66 @@ const pokedex = 'pokedex.json'
  * Routes
  * ===================================
  */
- jsonfile.readFile(pokedex, (err, obj) => {
+let pokemonNames = [];
 
-    app.get('/pokemon/*', (request, response) => {
+jsonfile.readFile(pokedex, (err, obj) => {
 
-        let pokemonList = obj.pokemon;
+    let pokemonList = obj.pokemon;
 
+    app.get('/pokemon/type/:type', (request, response) => {
+        console.log(request.params.type)
         for (let i = 0; i < pokemonList.length; i++) {
-            if (request.params[0] === pokemonList[i].name.toLowerCase() || request.params[0] == pokemonList[i].id) {
-                console.log(' pokemon i: ' + pokemonList[i].name)
-                console.log('request: ' + request.params[0]);
-                response.send(`<h1>${pokemonList[i].name}</h1>
-                    <h2>${pokemonList[i].weight}</h2>
-                    <h2>${pokemonList[i].type.join(', ')}</h2>
-                    <img src='${pokemonList[i].img}'>
-                    `);
-            } else {
-                response.status(404).send(`Could not find information about ${request.params[0]} - Is that a new pokemon? Gotta catch em' all!`);
+            let pokemonTypeString = pokemonList[i].type.join(', ').toLowerCase();
+
+            if (pokemonTypeString.includes(request.params.type)) {
+                pokemonNames.push(pokemonList[i].name);
             }
         }
-     });
+        response.send(`<p><font size='4'>These are all the <strong>${request.params.type}</strong> type Pokemon:<br> ${pokemonNames.join(', ')}</font></p>`);
+    });
+
+    app.get('/pokemon/weaknesses/:weakness', (request, response) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            let pokemonWeaknessString = pokemonList[i].weaknesses.join(', ').toLowerCase();
+
+            if (pokemonWeaknessString.includes(request.params.weakness)) {
+                pokemonNames.push(pokemonList[i].name);
+            }
+        }
+        response.send(`<p><font size='4'>These are all the Pokemon which are weak against <strong>${request.params.weakness}</strong>:<br> ${pokemonNames.join(', ')}</font></p>`);
+    });
+
+    app.get('/pokemon/*', (request, response) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            if (request.params[0] == pokemonList[i].id || request.params[0] === pokemonList[i].name.toLowerCase()) {
+                response.send(`<p><font size='4'>This is <strong>${pokemonList[i].name}</strong>, it is <strong>${pokemonList[i].weight}</strong> in weight and <strong>${pokemonList[i].height}</strong> in height!<br>
+                    It is a <strong>${pokemonList[i].type.join(', ')}</strong> Pokemon, and is weak to <strong>${pokemonList[i].weaknesses.join(', ')}</strong> types.</font></p>
+                    <img src='${pokemonList[i].img}'>`);
+            }
+        }
+    });
+
+    // app.get('/pokemon/:name', (request, response) => {
+    //     for (let i = 0; i < pokemonList.length; i++) {
+    //         if (request.params.name == pokemonList[i].name) {
+    //             response.send(`<p><font size='4'>This is <strong>${pokemonList[i].name}</strong>, it is <strong>${pokemonList[i].weight}</strong> in weight and <strong>${pokemonList[i].height}</strong> in height!<br>
+    //                 It is a <strong>${pokemonList[i].type}</strong> Pokemon, and is weak to <strong>${pokemonList[i].weaknesses.join(', ')}</strong> types.</font></p>
+    //                 <img src='${pokemonList[i].img}'>`);
+    //         }
+    //     }
+    // });
+
+    // app.get('/pokemon/*', (request, response) => {
+    //     if (request.params[0] !== pokemonList[i].id && request.params[0] !== pokemonList[i].name.toLowerCase()) {
+    //         for (let i = 0; i < pokemonList.length; i++) {
+    //             response.status(404).send(`Could not find information about ${request.params[0]} - Is that a new pokemon? Gotta catch em' all!`);
+    //         }
+    //     }
+    // });
 
     app.get('*', (request, response) => {
       // send response with some data (a string)
-      response.send(request.path);
+      response.send('Welcome to the online Pokedex!');
     });
 
 });
