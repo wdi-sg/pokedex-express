7ae38f28969fb2e84799da9bd9c15b601e9076b8
Rachelik <rachel.ik.87@gmail.com>
2020-04-06 19:47:13 +0800

    Able to get pokemon by pokemon name

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
+const file = 'pokedex.json';
+
+    jsonfile.readFile(file, (err, obj) => {
+        console.log(obj.pokemon.length);
+    });
+
 
 /**
  * ===================================
@@ -11,16 +17,36 @@ const express = require('express');
 // Init express app
 const app = express();
 
+
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+
+
+
+// app.get('/pokemon/:x', (request, response) => {
+//     jsonfile.readFile(file, (err, obj) => {
+//         response.send(obj.pokemon[parseInt(request.params.x)].name);
+//     });
+// });
+
+app.get('/pokemon/:name', (request, response) => {
+    jsonfile.readFile(file, (err, obj) => {
+        for (let i = 0; i < obj.pokemon.length; i++){
+            if(obj.pokemon[i].name.toLowerCase() == request.params.name.toLowerCase()){
+                response.send("Weight: "+obj.pokemon[i].weight);
+            }
+        };
+    });
 });
+
+app.get('*', (request, response) => {
+    response.send(request.path);
+});
+
 
 /**
  * ===================================
