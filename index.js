a4a27a03b3450aeb2600d4e3865e383d9b7e4b5b
Rachelik <rachel.ik.87@gmail.com>
2020-04-06 20:52:04 +0800

    Add title with blue color font

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const jsonfile = require('jsonfile');
 const file = 'pokedex.json';
 
-    jsonfile.readFile(file, (err, obj) => {
-        console.log(obj.pokemon.length);
-    });
+    // jsonfile.readFile(file, (err, obj) => {
+    //     console.log(obj.pokemon.length);
+    // });
 
 
 /**
@@ -25,28 +25,29 @@ const app = express();
  */
 
 
-
-
 // app.get('/pokemon/:x', (request, response) => {
-//     jsonfile.readFile(file, (err, obj) => {
-//         response.send(obj.pokemon[parseInt(request.params.x)].name);
-//     });
+//     if(request.path == "/pokemon/"+request.params.x) {
+//         jsonfile.readFile(file, (err, obj) => {
+//             response.send(obj.pokemon[parseInt(request.params.x)].name);
+//         });
+//     }
 // });
 
 app.get('/pokemon/:name', (request, response) => {
     jsonfile.readFile(file, (err, obj) => {
         for (let i = 0; i < obj.pokemon.length; i++){
-            if(obj.pokemon[i].name.toLowerCase() == request.params.name.toLowerCase()){
-                response.send(
+            if (obj.pokemon[i].name.toLowerCase() === request.params.name.toLowerCase()) {
+                response.send("<h2 style='color: blue'>Pokemon Chosen</h2>" +
                     "Name: " + obj.pokemon[i].name + "<br>" +
                     "Weight: " + obj.pokemon[i].weight + "<br>" +
                     "Height: " + obj.pokemon[i].height + "<br>" +
                     "<img src='" + obj.pokemon[i].img + "'>" + "<br>" +
                     "Type: " + obj.pokemon[i].type
                     );
-            }
+            };
         };
     });
+
 });
 
 app.get('*', (request, response) => {
