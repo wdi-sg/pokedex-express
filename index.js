781e4415d04825cdac2f1e834b9abed7512f1d42
chelsejw <chelsejw@gmail.com>
2020-04-06 22:52:34 +0800

    add basic function to search for pokemon and return name, height and weight

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
+const jsonfile = require('jsonfile');
 
 /**
  * ===================================
@@ -10,16 +9,36 @@ const express = require('express');
 
 // Init express app
 const app = express();
-
+const file = 'pokedex.json'
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
 /**
  * ===================================
  * Routes
  * ===================================
  */
 
+ app.get('/pokemon/:name', (req, res) => {
+   // send response with some data (a string)
+
+   jsonfile.readFile(file, (err, obj) => {
+     const pokeArray = obj.pokemon
+     const query = capitalize(req.params.name)
+     let result = pokeArray.find((obj) => obj.name===query)
+     console.log(result)
+
+     let output = `<h1>${query}</h1> is ${result.height} tall and ${result.weight}!`
+
+     res.send(output);
+
+   })
+ });
+
 app.get('*', (request, response) => {
   // send response with some data (a string)
-  response.send(request.path);
+  response.send(`Welcome to the Pokedex!`);
 });
 
 /**
