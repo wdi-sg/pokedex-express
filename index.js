837360f061c3d301e7d4f147f4bde8290623cb3f
mdazharaw <darkaz62@hotmail.com>
2020-04-07 00:27:44 +0800

    Part 3 in progress

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/pokemon/', (request, response) => {
 app.get("/pokemon/:name", (request, response) => {
   var found = false;
   var foundId;
- 
+
   for (id in pokemon) {
     // console.log(pokemon[id].name)
     if (pokemon[id].name.toLowerCase().includes(request.params.name)) {
@@ -55,18 +55,34 @@ app.get("/pokemon/:name", (request, response) => {
     continue;
   }
   if (found == true) {
-    var pokeName = pokemon[foundId].name + "\n";
+    var pokeName = pokemon[foundId].name;
     var pokeWeight = pokemon[foundId].weight;
+    var pokeHeight = pokemon[foundId].height;
 
-    response.send(`${pokeName} weighs: ${pokeWeight}`);
+    var pokeType;
+    if (pokemon[foundId].type.length > 1) {
+      pokeType = pokemon[foundId].type[0] + "/" + pokemon[foundId].type[1];
+    } else {
+      pokeType = pokemon[foundId].type[0];
+    }
+
+
+
+    response.send(`This is ${pokeName}, a ${pokeType} type Pokemon, that weighs ${pokeWeight} and is ${pokeHeight} tall!`);
   }
   else {
     var properName = toProperCase(request.params.name);
     response.status(404);
     response.send(`Could not find information about ${properName} - Is that a new pokemon? Gotta catch em' all!`);
-    
+
   }
 
+});
+
+app.get('/type/:type', (request, response) => {
+  var targetType = request.params.type;
+  var outType = getAllPokeOfType(targetType);
+  response.send(`${outType}`);
 });
 
 /**
@@ -76,10 +92,25 @@ app.get("/pokemon/:name", (request, response) => {
  */
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
 
-function toProperCase(string){
-  var firstLetter = string.substring(0,1);
-  var restOfLetter = string.substring(1,string.length);
+function toProperCase(string) {
+  var firstLetter = string.substring(0, 1);
+  var restOfLetter = string.substring(1, string.length);
   firstLetter = firstLetter.toUpperCase();
   restOfLetter = restOfLetter.toLowerCase();
-  return firstLetter+restOfLetter;
+  return firstLetter + restOfLetter;
+}
+
+function getAllPokeOfType(type) {
+  var resultArr = [];
+  for (id in pokemon) {
+    var typeArr = pokemon[id].type;
+    for (id in typeArr){
+      typeArr[id] = typeArr[id].toLowerCase();
+    }
+    if (typeArr.includes(type)){
+     resultArr.push(pokemon[id].name);
+    }
+
+  }
+  return resultArr;
 }
