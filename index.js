e51c222106f9776daa5eb3223732e7cc85e70001
ThomasOh92 <ohcsthomas@gmail.com>
2020-04-06 18:08:41 +0800

    Finished up most of the furthers (next evo, some type, details, etc.)

--- index.js
+++ index.js
@@ -28,12 +28,52 @@ jsonfile.readFile(file, (err, obj) => {
     //     response.send("The Pokemon is " + obj["pokemon"][request.params.arrayPosition]["name"]);
     // });
 
+    app.get(`/nextevolution/:submittedPokemon/`, (req, res) => {
+        let found = false;
+        let listOfPokemon = [];
+        for (let element of obj["pokemon"]){
+            if (element["name"].toLowerCase() === req.params.submittedPokemon.toLowerCase()){
+                found = true;
+                for (let i = 0; i < element["prev_evolution"].length; i++){
+                    listOfPokemon.push(element["prev_evolution"][i]["name"])
+                }
+            }
+        }
+
+        res.send("List of Pokemon that it evolves from: " + listOfPokemon.join(" / "))
+
+        if (!found){
+            res.status(404).send("Could not find information about " + req.params.submittedPokemon + " - Is that a new pokemon? Gotta catch em' all!")
+        }
+    })
+
+
+
+    app.get(`/type/:submittedType/`, (req, res) => {
+        let found = false;
+        let listOfPokemon = [];
+        for (let element of obj["pokemon"]){
+            for (let i = 0; i < element["type"].length; i++){
+                if (element["type"][i].toLowerCase() === req.params.submittedType.toLowerCase()){
+                    found = true;
+                    listOfPokemon.push(element["name"])
+                }
+            }
+        }
+
+        res.send("List of Pokemon: " + listOfPokemon.join(" / "))
+
+        if (!found){
+            res.status(404).send("No pokemon with the type: " + req.params.submittedName + " - Is that a new type? Gotta catch em' all!")
+        }
+    })
+
     app.get(`/pokemon/:submittedName/`, (req, res) => {
         let found = false;
         for (let element of obj["pokemon"]){
             if (element["name"].toLowerCase() === req.params.submittedName.toLowerCase()){
                 found = true;
-                res.send("The Pokemon's name is " + element["name"] + ", and it weighs " + element["weight"])
+                res.send("The Pokemon's name is " + element["name"] + ", and it weighs " + element["weight"] + " , and its type is " + element["type"])
             }
         }
         if (!found){
@@ -42,7 +82,7 @@ jsonfile.readFile(file, (err, obj) => {
     })
 
     app.get('*', (request, response) => {
-        response.send("whoops");
+        response.send("Welcome to the online Pokedex!");
     });
 });
 
