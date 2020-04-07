11768b4bfc53e1eb77e09ab8007f011e2e4fd829
vigbit <vigneshb@live.com>
2020-04-07 09:59:33 +0800

    basic structure

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const express = require('express');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
 // Init express app
 const app = express();
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
+const jsonfile = require('jsonfile');
 
-app.get('*', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.path);
+const fileName = 'pokedex.json'
+
+
+app.get("/pokemon/:name", (request, response) => {
+
+    jsonfile.readFile(fileName, (err, obj) =>{
+    //console.log(obj.pokemon[1].name)
+      let pokeDetails;
+
+      let nameOfPokeAsked = request.params.name;
+          for ( i = 0; i < obj.pokemon.length; i ++){
+              if (nameOfPokeAsked === obj.pokemon[i].name){
+                pokeDetails = obj.pokemon[i];
+                //console.log(pokeDetails)
+
+              }
+              if ( nameOfPokeAsked == undefined){
+                response.send("Could not find information about" + nameOfPokeAsked + "- Is that a new pokemon? Gotta catch em' all!")
+              }
+              else{
+                response.send(pokeDetails);
+              }
+
+          }
+      })
+// send response with some data (a string)
+
 });
+//read pokedex.json
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
+
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+
+
