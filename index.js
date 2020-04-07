5f628835cc19fb03dc61c5f8f24db2de0be0c11e
vigbit <vigneshb@live.com>
2020-04-07 13:45:44 +0800

    find by name

--- index.js
+++ index.js
@@ -14,20 +14,26 @@ app.get("/pokemon/:name", (request, response) => {
       let pokeDetails;
 
       let nameOfPokeAsked = request.params.name;
-          for ( i = 0; i < obj.pokemon.length; i ++){
-              if (nameOfPokeAsked === obj.pokemon[i].name){
-                pokeDetails = obj.pokemon[i];
-                //console.log(pokeDetails)
+      console.log("what is here " + nameOfPokeAsked);
 
-              }
-              if ( nameOfPokeAsked == undefined){
-                response.send("Could not find information about" + nameOfPokeAsked + "- Is that a new pokemon? Gotta catch em' all!")
-              }
-              else{
+          for (let i = 0; i < obj.pokemon.length; i++){
+              let content = obj.pokemon[i].name.toLowerCase();
+              console.log(content);
+              if (nameOfPokeAsked === content){
+                pokeDetails = obj.pokemon[i];
+                console.log("not again!")
                 response.send(pokeDetails);
               }
-
           }
+              if( nameOfPokeAsked == undefined){
+                response.send("Could not find information about" + nameOfPokeAsked + "- Is that a new pokemon? Gotta catch em' all!")
+                console.log("go go go")
+              }
+              else{
+                console.log("Gin tonic")
+              }
+
+
       })
 // send response with some data (a string)
 
